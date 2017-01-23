3257eee34e92702c2fc797cf521b273e79402a43
Andrew Eastgaard <andrew.eastgaard@syscon.net>
2017-01-23 13:48:23 -0800

    Extended Inmate object to include optional detailed information.

--- UI/app/inmate.ts
+++ UI/app/inmate.ts
@@ -1,7 +1,39 @@
+export class PhysicalAttributes {
+  gender: string;
+  ethnicity: string;
+  heightInches: number;
+  heightMeters: number;
+  weightPounds: number;
+  weightKg: number;
+}
+
+export class PhysicalCharacteristic {
+  characteristic: string;
+  detail: string;
+}
+
+export class PhysicalMark {
+  type: string;
+  side: string;
+  bodyPart: string;
+  orientation: string;
+  comment: string;
+}
+
 export class Inmate {
-      inmateId: number;
-      bookingId: string;
-      offenderId: string;
-      firstName: string;
-      lastName: string;
+  inmateId: number;
+  bookingId: string;
+  offenderId: string;
+  firstName: string;
+  lastName: string;
+
+  middleName?: string;
+  alertCodes?: string[];
+  currentLocationId?: number;
+  assignedLivingUnitId?: number;
+  dateOfBirth?: string;
+  age?: number;
+  physicalAttributes?: PhysicalAttributes
+  physicalCharacteristics?: PhysicalCharacteristic[]
+  physicalMarks?: PhysicalMark[]
 }
