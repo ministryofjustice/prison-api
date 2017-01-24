01d75f7a04ec4eadd81b85c3ff69d8ece4f54759
Andrew Eastgaard <andrew.eastgaard@syscon.net>
2017-01-23 16:54:33 -0800

    Filled in location summary and assigned inmates. Mock data setup in SDP-WEST-SOUTH 2.

--- UI/app/agency-location-detail.component.ts
+++ UI/app/agency-location-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
+import { ActivatedRoute, Params, Router }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
 import { AgencyLocation } from './agency-location';
 import { AgencyLocationService } from './agency-location.service';
+import { Inmate } from './inmate';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -11,11 +12,71 @@ import 'rxjs/add/operator/switchMap';
   selector: 'my-agency-location-detail',
   template: `
   <div *ngIf="agencyLocation">
-  <h2>{{agencyLocation.description}} Details</h2>
+    <h2>{{agencyLocation.description}} Details</h2>
+    <div *ngIf="agencyLocation.currentOccupancy"><span><label>Current Occupancy:</label>{{agencyLocation.currentOccupancy}}</span></div>
+    <div *ngIf="agencyLocation.operationalCapacity"><span><label>Operational Capacity:</label>{{agencyLocation.operationalCapacity}}</span></div>
+    <div *ngIf="agencyLocation.assignedInmates">
+      <h3>Inmates</h3>
+      <ul class="inmates">
+    		<li *ngFor="let inmate of agencyLocation.assignedInmates" (click)="onSelect(inmate)">
+    			{{inmate.firstName}} {{inmate.lastName}} ({{inmate.bookingId}})<span class="badge">&gt;</span>
+    		</li>
+    	</ul>
+    <div>
   </div>
   <div *ngIf="!agencyLocation">Loading location...</div>
   <button (click)="goBack()">Back</button>
   `,
+  styles: [`
+    .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+    }
+    .inmates {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 30em;
+    }
+    .inmates li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+    }
+    .inmates li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+    }
+    .inmates li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+    }
+    .inmates .text {
+    position: relative;
+    top: -3px;
+    }
+    .inmates .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-left: .8em;
+    border-radius: 0 4px 4px 0;
+    float: right;
+    }
+  `],
 })
 export class AgencyLocationDetailComponent implements OnInit {
 	@Input()
@@ -24,6 +85,7 @@ export class AgencyLocationDetailComponent implements OnInit {
     constructor(
       private agencyLocationService: AgencyLocationService,
       private route: ActivatedRoute,
+      private router: Router,
       private location: Location
     ) { }
 
@@ -32,6 +94,10 @@ export class AgencyLocationDetailComponent implements OnInit {
         .switchMap((params: Params) => this.agencyLocationService.getAgencyLocation(+params['locationId']))
         .subscribe(agencyLocation => this.agencyLocation = agencyLocation);
     }
+
+  onSelect(inmate: Inmate): void {
+    this.router.navigate(['/inmates', inmate.inmateId]);
+  }
 
     goBack(): void {
       this.location.back();
--- UI/app/agency-location.ts
+++ UI/app/agency-location.ts
@@ -1,6 +1,14 @@
+import { Inmate } from './inmate';
+
 export class AgencyLocation  {
 	locationId: number;
 	agencyId: string;
 	locationType: string;
 	description: string;
+	parentLocationId?: number;
+	operationalCapacity?: number;
+	currentOccupancy: number;
+	livingUnit: boolean;
+	housingUnitType?: string;
+	assignedInmates?: Inmate[];
 }
