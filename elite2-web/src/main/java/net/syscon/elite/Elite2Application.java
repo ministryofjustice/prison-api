a521b933b307c73c4c3e23a646dde6ef194cbe81
Wellington Souza <wbsouza@gmail.com>
2017-02-03 17:01:21 -0800

    configuring bootstrap profiles and configuration dir

--- elite2-web/src/main/java/net/syscon/elite/Elite2Application.java
+++ elite2-web/src/main/java/net/syscon/elite/Elite2Application.java
@@ -1,12 +1,33 @@
 package net.syscon.elite;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
+
+import net.syscon.elite.web.config.ApplicationContextConfigs;
 
 @SpringBootApplication
 public class Elite2Application {
+	
+	private static void setUp() {
+		final File currDir = new File(".");
+		final File projectDir = currDir.getAbsolutePath().contains("elite2-web")? currDir: new File("elite2-web");
+		String activeProfile = System.getProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME);
+		if (activeProfile == null) {
+			activeProfile = projectDir.exists()? "dev": "prod";
+		}
+		final File configsDir = "prod".equals(activeProfile)? new File(currDir, "conf"): new File(projectDir, "src/main/configs");
+		final String configsPath = configsDir.getAbsolutePath().replace("\\",  "/").replace("\\.",  "");
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, activeProfile);
+		System.setProperty(ApplicationContextConfigs.CONFIGS_DIR_PROPERTY, configsPath);
+	}
+	
+	
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
+		setUp();
 		SpringApplication.run(Elite2Application.class, args);
 	}
 }
