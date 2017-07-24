3041bb27bbf0ac4dc83cc4c0718e73d792e2a43b
Michael Willis <michael.willis@syscon.net>
2017-07-24 01:18:16 +0100

    EM-102 Fix connection pool

--- mobile-web/src/main/java/net/syscon/elite/aop/OracleConnectionAspect.java
+++ mobile-web/src/main/java/net/syscon/elite/aop/OracleConnectionAspect.java
@@ -1,6 +1,7 @@
 package net.syscon.elite.aop;
 
 import net.syscon.elite.security.UserSecurityUtils;
+import net.syscon.util.SQLProvider;
 import oracle.jdbc.driver.OracleConnection;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -11,6 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.aop.MethodBeforeAdvice;
 import org.springframework.aop.aspectj.AspectJExpressionPointcutAdvisor;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,13 +25,21 @@ import java.util.Properties;
 @Aspect
 public class OracleConnectionAspect {
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	
-	private final AspectJExpressionPointcutAdvisor closeConnectionAdvisor;
-	private final String rolePassword;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	public OracleConnectionAspect(String rolePassword) {
-	    this.rolePassword = rolePassword;
+    private final AspectJExpressionPointcutAdvisor closeConnectionAdvisor;
+    private String rolePassword;
+    private final SQLProvider sqlProvider;
+    private final String jdbcUrl;
+    private final String username;
+    private final String password;
+
+    public OracleConnectionAspect(SQLProvider sqlProvider, String jdbcUrl, String username, String password) {
+        this.sqlProvider = sqlProvider;
+        this.jdbcUrl = jdbcUrl;
+        this.username = username;
+        this.password = password;
+
         closeConnectionAdvisor = new AspectJExpressionPointcutAdvisor();
         closeConnectionAdvisor.setExpression("execution (* java.sql.Connection.close(..))");
         closeConnectionAdvisor.setOrder(10);
@@ -38,29 +50,31 @@ public class OracleConnectionAspect {
                 oracleConn.close(OracleConnection.PROXY_SESSION);
             }
         });
-	}
+    }
 
     @Pointcut("execution (* com.zaxxer.hikari.HikariDataSource.getConnection())")
     private void onNewConnectionPointcut() {
     }
 
-    @Around ("onNewConnectionPointcut()")
-	public Object connectionAround(final ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("onNewConnectionPointcut()")
+    public Object connectionAround(final ProceedingJoinPoint joinPoint) throws Throwable {
         log.debug("Enter: {}.{}()", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
-		try {
-			final Connection conn = (Connection) joinPoint.proceed();
+        try {
+            final Connection conn = (Connection) joinPoint.proceed();
             final String username = UserSecurityUtils.getCurrentUsername();
-			if (!UserSecurityUtils.isAnonymousAuthentication() && username != null && rolePassword != null) {
-				final OracleConnection oracleConn = (OracleConnection) conn.unwrap(Connection.class);
+            assignRolePassword();
 
-				final Properties info = new Properties();
+            if (!UserSecurityUtils.isAnonymousAuthentication() && username != null) {
+                final OracleConnection oracleConn = (OracleConnection) conn.unwrap(Connection.class);
 
-			    info.put(OracleConnection.PROXY_USER_NAME, username);
-		        oracleConn.openProxySession(OracleConnection.PROXYTYPE_USER_NAME, info);
-		        
-		        final ProxyFactory proxyFactory = new ProxyFactory(conn);
-		        proxyFactory.addAdvisor(closeConnectionAdvisor);
-		        final Connection proxyConn = (Connection) proxyFactory.getProxy();
+                final Properties info = new Properties();
+
+                info.put(OracleConnection.PROXY_USER_NAME, username);
+                oracleConn.openProxySession(OracleConnection.PROXYTYPE_USER_NAME, info);
+
+                final ProxyFactory proxyFactory = new ProxyFactory(conn);
+                proxyFactory.addAdvisor(closeConnectionAdvisor);
+                final Connection proxyConn = (Connection) proxyFactory.getProxy();
 
                 final String startSessionSQL = "SET ROLE TAG_USER IDENTIFIED BY " + rolePassword;
                 final PreparedStatement stmt = oracleConn.prepareStatement(startSessionSQL);
@@ -68,15 +82,27 @@ public class OracleConnectionAspect {
                 stmt.close();
 
                 log.debug("Exit: {}.{}() with result = {}", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), conn);
-		        return proxyConn;
-			} else {
-				return conn;
-			}
-		} catch (final Throwable e) {
-			log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), e);
-			throw e;
-		}
-	}
+                return proxyConn;
+            } else {
+                return conn;
+            }
+        } catch (final Throwable e) {
+            log.error("Illegal argument: {} in {}.{}()", Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName(), e);
+            throw e;
+        }
+    }
+
+    private void assignRolePassword() {
+        if (rolePassword == null) {
+            final DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(jdbcUrl, username, password);
+            final NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(driverManagerDataSource);
+            final String sql = sqlProvider.get("FIND_ROLE_PASSWORD");
+            final MapSqlParameterSource params = new MapSqlParameterSource();
+            final String encryptedPassword = jdbcTemplate.queryForObject(sql, params, String.class);
+            params.addValue("password", encryptedPassword);
+            rolePassword = jdbcTemplate.queryForObject("SELECT decryption('2DECRYPTPASSWRD', :password) FROM DUAL", params, String.class);
+        }
+    }
 }
 
 
--- mobile-web/src/main/java/net/syscon/elite/web/config/AopConfigs.java
+++ mobile-web/src/main/java/net/syscon/elite/web/config/AopConfigs.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -28,14 +25,7 @@ public class AopConfigs {
                                                               @Value("${app.username}") String username,
                                                               @Value("${app.password}") String password
                                                               ) {
-        final DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(jdbcUrl, username, password);
-        final NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(driverManagerDataSource);
-        final String sql = sqlProvider.get("FIND_ROLE_PASSWORD");
-        final MapSqlParameterSource params = new MapSqlParameterSource();
-        final String encryptedPassword = jdbcTemplate.queryForObject(sql, params, String.class);
-        params.addValue("password", encryptedPassword);
-        String rolePassword = jdbcTemplate.queryForObject("SELECT decryption('2DECRYPTPASSWRD', :password) FROM DUAL", params, String.class);
-        return new OracleConnectionAspect(rolePassword);
+        return new OracleConnectionAspect(sqlProvider, jdbcUrl, username, password);
 	}
 
 }
