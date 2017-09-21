ef9145bbc41b2aa18a3d5e60c1061bd086f2da08
Michael Willis <michael.willis@syscon.net>
2017-09-21 16:55:19 +0100

    EA-160 - Fix tests

--- mobile-web/src/main/java/net/syscon/elite/v2/repository/impl/BookingRepositoryImpl.java
+++ mobile-web/src/main/java/net/syscon/elite/v2/repository/impl/BookingRepositoryImpl.java
@@ -6,6 +6,7 @@ import net.syscon.elite.persistence.mapping.FieldMapper;
 import net.syscon.elite.persistence.mapping.Row2BeanRowMapper;
 import net.syscon.elite.v2.api.model.SentenceDetail;
 import net.syscon.elite.v2.repository.BookingRepository;
+import net.syscon.util.DateFormatProvider;
 import net.syscon.util.IQueryBuilder;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
@@ -22,19 +23,19 @@ public class BookingRepositoryImpl extends RepositoryBase implements BookingRepo
     private final Map<String, FieldMapper> sentenceDetailMapping =
             new ImmutableMap.Builder<String, FieldMapper>()
                     .put("OFFENDER_BOOK_ID", new FieldMapper("bookingId"))
-                    .put("START_DATE", new FieldMapper("sentenceStartDate"))
-                    .put("END_DATE", new FieldMapper("sentenceEndDate"))
-                    .put("CONDITIONAL_RELEASE_DATE", new FieldMapper("conditionalReleaseDate"))
-                    .put("AUTOMATIC_RELEASE_DATE", new FieldMapper("automaticReleaseDate"))
-                    .put("NON_PAROLE_DATE", new FieldMapper("nonParoleDate"))
-                    .put("LICENSE_EXPIRY_DATE", new FieldMapper("licenseExpiryDate"))
-                    .put("PAROLE_ELIGIBILITY_DATE", new FieldMapper("paroleEligibilityDate"))
-                    .put("HOME_DET_CURF_ELIGIBILITY_DATE", new FieldMapper("homeDetentionCurfewEligibilityDate"))
+                    .put("START_DATE", new FieldMapper("sentenceStartDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("END_DATE", new FieldMapper("sentenceEndDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("CONDITIONAL_RELEASE_DATE", new FieldMapper("conditionalReleaseDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("AUTOMATIC_RELEASE_DATE", new FieldMapper("automaticReleaseDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("NON_PAROLE_DATE", new FieldMapper("nonParoleDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("LICENSE_EXPIRY_DATE", new FieldMapper("licenseExpiryDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("PAROLE_ELIGIBILITY_DATE", new FieldMapper("paroleEligibilityDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("HOME_DET_CURF_ELIGIBILITY_DATE", new FieldMapper("homeDetentionCurfewEligibilityDate", DateFormatProvider::toISO8601LocalDate))
                     .put("DAYS_REMAINING", new FieldMapper("daysRemaining"))
-                    .put("EARLY_TERM_DATE", new FieldMapper("earlyTermDate"))
-                    .put("MID_TERM__DATE", new FieldMapper("midTermDate"))
-                    .put("LATE_TERM_DATE", new FieldMapper("lateTermDate"))
-                    .put("RELEASE_DATE", new FieldMapper("releaseDate"))
+                    .put("EARLY_TERM_DATE", new FieldMapper("earlyTermDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("MID_TERM__DATE", new FieldMapper("midTermDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("LATE_TERM_DATE", new FieldMapper("lateTermDate", DateFormatProvider::toISO8601LocalDate))
+                    .put("RELEASE_DATE", new FieldMapper("releaseDate", DateFormatProvider::toISO8601LocalDate))
                     .build();
 
     @Override
