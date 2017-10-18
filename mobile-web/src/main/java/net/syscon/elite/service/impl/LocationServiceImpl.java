307857cb1638386fd7de38011231c23f2b1488e4
Michael Willis <michael.willis@syscon.net>
2017-10-18 10:38:12 +0100

    EA-196 Remove AgencyLocationService - merge into LocationService

--- mobile-web/src/main/java/net/syscon/elite/service/impl/LocationServiceImpl.java
+++ mobile-web/src/main/java/net/syscon/elite/service/impl/LocationServiceImpl.java
@@ -3,16 +3,27 @@ package net.syscon.elite.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import net.syscon.elite.api.model.Agency;
 import net.syscon.elite.api.model.Location;
+import net.syscon.elite.api.model.OffenderBooking;
+import net.syscon.elite.api.model.UserDetail;
+import net.syscon.elite.api.support.Order;
 import net.syscon.elite.repository.AgencyRepository;
+import net.syscon.elite.repository.InmateRepository;
 import net.syscon.elite.repository.LocationRepository;
+import net.syscon.elite.repository.UserRepository;
+import net.syscon.elite.security.UserSecurityUtils;
+import net.syscon.elite.service.EntityNotFoundException;
 import net.syscon.elite.service.LocationService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static net.syscon.elite.service.impl.InmateServiceImpl.DEFAULT_OFFENDER_SORT;
 
 /**
  * Location API service implementation.
@@ -23,16 +34,19 @@ import java.util.stream.Collectors;
 public class LocationServiceImpl implements LocationService {
     private final AgencyRepository agencyRepository;
     private final LocationRepository locationRepository;
+    private final InmateRepository inmateRepository;
+    private final UserRepository userRepository;
+    private final String locationTypeGranularity;
+    private final Integer locationDepth;
 
-    @Value("${api.users.me.locations.locationType:WING}")
-    private String locationTypeGranularity;
-
-    @Value("${api.users.me.locations.depth:1}")
-    private Integer locationDepth;
-
-    public LocationServiceImpl(AgencyRepository agencyRepository, LocationRepository locationRepository) {
-        this.agencyRepository = agencyRepository;
+    public LocationServiceImpl(AgencyRepository agencyRepository, LocationRepository locationRepository, InmateRepository inmateRepository, UserRepository userRepository,
+                               @Value("${api.users.me.locations.locationType:WING}") String locationTypeGranularity, @Value("${api.users.me.locations.depth:1}") Integer locationDepth) {
         this.locationRepository = locationRepository;
+        this.inmateRepository = inmateRepository;
+        this.userRepository = userRepository;
+        this.agencyRepository = agencyRepository;
+        this.locationTypeGranularity = locationTypeGranularity;
+        this.locationDepth = locationDepth;
     }
 
     @Override
@@ -73,6 +87,49 @@ public class LocationServiceImpl implements LocationService {
         }
 
         return locations;
+    }
+
+    @Override
+    public List<Location> getLocations(String query, String orderBy, Order order, long offset, long limit) {
+        return locationRepository.findLocations(query, orderBy, order, offset, limit);
+    }
+
+    @Override
+    public List<Location> getLocationsFromAgency(final String agencyId, final String query, final long offset, final long limit, final String orderByField, final Order order) {
+        return locationRepository.findLocationsByAgencyId(getCurrentCaseLoad(), agencyId, query, offset, limit, orderByField, order);
+    }
+
+    @Override
+    public List<OffenderBooking> getInmatesFromLocation(long locationId, String query, String orderByField, Order order, long offset, long limit) {
+        List<OffenderBooking> inmates;
+
+        Location location = getLocation(locationId, false);
+
+        String colSort = StringUtils.isNotBlank(orderByField) ? orderByField : DEFAULT_OFFENDER_SORT;
+        if ( location == null) {
+            inmates = Collections.emptyList();
+        } else {
+            inmates = inmateRepository.findInmatesByLocation(locationId, locationTypeGranularity, query, colSort, order, offset, limit);
+        }
+
+        return inmates;
+    }
+
+    @Override
+    public Location getLocation(long locationId, boolean withInmates) {
+        Location location = locationRepository.findLocation(locationId).orElseThrow(new EntityNotFoundException(String.valueOf(locationId)));
+        if (withInmates) {
+            List<OffenderBooking> inmates = inmateRepository.findInmatesByLocation(locationId, locationTypeGranularity, null, null, null, 0, 1000);
+            location.setAssignedInmates(inmates);
+        }
+        return location;
+    }
+
+    private String getCurrentCaseLoad() {
+        //  get the user data from the database
+        final String currentUsername = UserSecurityUtils.getCurrentUsername();
+        final UserDetail userDetail = userRepository.findByUsername(currentUsername).orElseThrow(new EntityNotFoundException(currentUsername));
+        return userDetail.getActiveCaseLoadId();
     }
 
     private Location convertToLocation(Agency agency) {
