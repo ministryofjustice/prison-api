44588288f59cb1ca436a0199ed0ec41a6acbb906
Matt Smith <mattonfoot@gmail.com>
2017-11-08 16:36:36 +0000

    fix database dates in migrations

--- mobile-web/src/main/resources/db/migration/data/R__offender_external_movements.sql
+++ mobile-web/src/main/resources/db/migration/data/R__offender_external_movements.sql
@@ -4,9 +4,9 @@
 
 -- 'Active-Out (TAP)'
 INSERT INTO OFFENDER_EXTERNAL_MOVEMENTS (OFFENDER_BOOK_ID, MOVEMENT_SEQ, MOVEMENT_DATE, MOVEMENT_TIME, MOVEMENT_TYPE, MOVEMENT_REASON_CODE, DIRECTION_CODE, ARREST_AGENCY_LOC_ID, TO_PROV_STAT_CODE, ESCORT_CODE, FROM_AGY_LOC_ID, TO_AGY_LOC_ID, ACTIVE_FLAG, ESCORT_TEXT, COMMENT_TEXT, REPORTING_DATE, TO_CITY, FROM_CITY, REPORTING_TIME, EVENT_ID, PARENT_EVENT_ID, TO_COUNTRY_CODE, OJ_LOCATION_CODE, APPLICATION_DATE, TO_ADDRESS_ID, FROM_ADDRESS_ID)
-    VALUES  (-17, 1, TO_DATE('2011-11-07 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2011-11-07 21:13:54', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'I', 'IN', null, null, null, 'OUT', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
-            (-17, 2, TO_DATE('2012-16-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-16-09 11:09:12', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'NOTR', 'OUT', null, null, null, 'LEI', 'BMI', 'N', null, null, TO_DATE('2012-16-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), null, null, null, null, null, null, null, null, null, null),
-            (-17, 3, TO_DATE('2013-16-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-16-09 11:10:38', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'MOTR', 'IN', null, null, null, 'BMI', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
+    VALUES  (-17, 1, TO_DATE('2011-07-11 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2011-07-11 21:13:54', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'I', 'IN', null, null, null, 'OUT', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
+            (-17, 2, TO_DATE('2012-09-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-09-16 11:09:12', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'NOTR', 'OUT', null, null, null, 'LEI', 'BMI', 'N', null, null, TO_DATE('2012-09-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), null, null, null, null, null, null, null, null, null, null),
+            (-17, 3, TO_DATE('2013-09-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-09-16 11:10:38', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'MOTR', 'IN', null, null, null, 'BMI', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
             (-17, 4, TO_DATE('2015-07-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2015-07-16 13:22:00', 'YYYY-MM-DD HH24:MI:SS'), 'CRT', 'CRT', 'OUT', null, null, null, 'LEI', 'ABDRCT', 'N', null, null, null, null, null, null, 506777, null, null, null, null, null, null),
             (-17, 5, TO_DATE('2016-05-05 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2016-05-05 17:35:28', 'YYYY-MM-DD HH24:MI:SS'), 'CRT', 'CRT', 'IN', null, null, null, 'ABDRCT', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
             (-17, 6, TO_DATE('2016-06-05 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2016-05-05 19:00:30', 'YYYY-MM-DD HH24:MI:SS'), 'TAP', 'C3', 'OUT', null, null, null, 'LEI', null, 'N', null, null, null, '17917', null, null, null, null, null, null, null, null, null),
@@ -19,8 +19,8 @@ INSERT INTO OFFENDER_EXTERNAL_MOVEMENTS (OFFENDER_BOOK_ID, MOVEMENT_SEQ, MOVEMEN
 
 -- 'In-Transit'
 INSERT INTO OFFENDER_EXTERNAL_MOVEMENTS (OFFENDER_BOOK_ID, MOVEMENT_SEQ, MOVEMENT_DATE, MOVEMENT_TIME, MOVEMENT_TYPE, MOVEMENT_REASON_CODE, DIRECTION_CODE, FROM_AGY_LOC_ID, TO_AGY_LOC_ID, ACTIVE_FLAG)
-    VALUES  (-19, 1, TO_DATE('2011-11-07 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2011-11-07 21:13:54', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'I',      'IN',   'OUT', 'LEI', 'N'),
-            (-19, 2, TO_DATE('2012-16-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-16-09 11:09:12', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'NOTR',   'OUT',  'LEI', 'BMI', 'Y');
+    VALUES  (-19, 1, TO_DATE('2011-07-11 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2011-07-11 21:13:54', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'I',      'IN',   'OUT', 'LEI', 'N'),
+            (-19, 2, TO_DATE('2012-09-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-09-16 11:09:12', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'NOTR',   'OUT',  'LEI', 'BMI', 'Y');
 
 -- 'Active-UAL'
 INSERT INTO OFFENDER_EXTERNAL_MOVEMENTS (OFFENDER_BOOK_ID, MOVEMENT_SEQ, MOVEMENT_DATE, MOVEMENT_TIME, MOVEMENT_TYPE, MOVEMENT_REASON_CODE, DIRECTION_CODE, FROM_AGY_LOC_ID, TO_AGY_LOC_ID, ACTIVE_FLAG)
@@ -46,8 +46,8 @@ INSERT INTO OFFENDER_EXTERNAL_MOVEMENTS (OFFENDER_BOOK_ID, MOVEMENT_SEQ, MOVEMEN
 
 -- 'Active-Out (CRT)'
 INSERT INTO OFFENDER_EXTERNAL_MOVEMENTS (OFFENDER_BOOK_ID, MOVEMENT_SEQ, MOVEMENT_DATE, MOVEMENT_TIME, MOVEMENT_TYPE, MOVEMENT_REASON_CODE, DIRECTION_CODE, ARREST_AGENCY_LOC_ID, TO_PROV_STAT_CODE, ESCORT_CODE, FROM_AGY_LOC_ID, TO_AGY_LOC_ID, ACTIVE_FLAG, ESCORT_TEXT, COMMENT_TEXT, REPORTING_DATE, TO_CITY, FROM_CITY, REPORTING_TIME, EVENT_ID, PARENT_EVENT_ID, TO_COUNTRY_CODE, OJ_LOCATION_CODE, APPLICATION_DATE, TO_ADDRESS_ID, FROM_ADDRESS_ID)
-    VALUES  (-25, 1, TO_DATE('2011-11-07 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2011-11-07 21:13:54', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'I', 'IN', null, null, null, 'OUT', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
-            (-25, 2, TO_DATE('2012-16-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-16-09 11:09:12', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'NOTR', 'OUT', null, null, null, 'LEI', 'BMI', 'N', null, null, TO_DATE('2012-16-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), null, null, null, null, null, null, null, null, null, null),
-            (-25, 3, TO_DATE('2012-22-09 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-16-09 11:10:38', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'INT', 'IN', null, null, null, 'LEI', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
+    VALUES  (-25, 1, TO_DATE('2011-07-11 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2011-07-11 21:13:54', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'I', 'IN', null, null, null, 'OUT', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
+            (-25, 2, TO_DATE('2012-09-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-09-16 11:09:12', 'YYYY-MM-DD HH24:MI:SS'), 'TRN', 'NOTR', 'OUT', null, null, null, 'LEI', 'BMI', 'N', null, null, TO_DATE('2012-09-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), null, null, null, null, null, null, null, null, null, null),
+            (-25, 3, TO_DATE('2012-09-22 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2012-09-16 11:10:38', 'YYYY-MM-DD HH24:MI:SS'), 'ADM', 'INT', 'IN', null, null, null, 'LEI', 'LEI', 'N', null, null, null, null, null, null, null, null, null, null, null, null, null),
             (-25, 4, TO_DATE('2015-07-16 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2015-07-16 13:22:00', 'YYYY-MM-DD HH24:MI:SS'), 'CRT', 'CRT', 'OUT', null, null, null, 'LEI', 'ABDRCT', 'Y', null, null, null, null, null, null, 506777, null, null, null, null, null, null);
 
