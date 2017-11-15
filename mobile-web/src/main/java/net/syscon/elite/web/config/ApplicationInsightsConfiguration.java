b4ef163b7754bb8dd600637ebb6d361b63f92c51
Michael Willis <michael.willis@syscon.net>
2017-11-15 19:10:35 +0000

    EA-222 only invoke filter if app insights defined.

--- mobile-web/src/main/java/net/syscon/elite/web/config/ApplicationInsightsConfiguration.java
+++ mobile-web/src/main/java/net/syscon/elite/web/config/ApplicationInsightsConfiguration.java
@@ -2,35 +2,55 @@ package net.syscon.elite.web.config;
 
 import com.microsoft.applicationinsights.TelemetryConfiguration;
 import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import javax.servlet.Filter;
 
 @Configuration
 public class ApplicationInsightsConfiguration {
 
-    @Bean
-    public String telemetryConfig(@Value("${application.insights.ikey}") String telemetryKey) {
-        if (telemetryKey != null) {
-            TelemetryConfiguration.getActive().setInstrumentationKey(telemetryKey);
-        }
-        return telemetryKey;
+    private final String telemetryKey;
+
+    @Autowired
+    public ApplicationInsightsConfiguration(@Value("${application.insights.ikey}") String telemetryKey) {
+        this.telemetryKey = telemetryKey;
     }
 
     @Bean
-    public FilterRegistrationBean aiFilterRegistration() {
+    @Conditional(AppInsightKeyPresentCondition.class)
+    public FilterRegistrationBean aiFilterRegistration(Filter webRequestTrackingFilter) {
         FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(WebRequestTrackingFilter());
+        registration.setFilter(webRequestTrackingFilter);
         registration.addUrlPatterns("/**");
         registration.setOrder(1);
         return registration;
+
     }
 
-    @Bean(name = "WebRequestTrackingFilter")
-    public Filter WebRequestTrackingFilter() {
+    @Bean(name = "webRequestTrackingFilter")
+    @Conditional(AppInsightKeyPresentCondition.class)
+    public Filter webRequestTrackingFilter() {
         return new WebRequestTrackingFilter();
+    }
+
+    @Bean
+    @Conditional(AppInsightKeyPresentCondition.class)
+    public String telemetryConfig() {
+        TelemetryConfiguration.getActive().setInstrumentationKey(telemetryKey);
+        return telemetryKey;
+    }
+
+    public static class AppInsightKeyPresentCondition implements Condition {
+
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            final String telemetryKey = context.getEnvironment().getProperty("application.insights.ikey");
+            return StringUtils.isNotBlank(telemetryKey);
+        }
     }
 }
