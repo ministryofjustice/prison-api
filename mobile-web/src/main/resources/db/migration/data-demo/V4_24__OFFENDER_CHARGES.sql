45ce961f007e9f22022f4612520848133850be6f
Michael Willis <michael.willis@syscon.net>
2017-11-29 17:40:58 +0000

    EA-233 More demo test data

---
+++ mobile-web/src/main/resources/db/migration/data-demo/V4_24__OFFENDER_CHARGES.sql
@@ -0,0 +1,10 @@
+INSERT INTO OFFENDER_CHARGES (OFFENDER_CHARGE_ID, OFFENDER_BOOK_ID, STATUTE_CODE, OFFENCE_CODE, CHARGE_STATUS, MOST_SERIOUS_FLAG, CASE_ID)
+VALUES (-1, -1, 'RV98', 'RV98011', 'A', 'Y', -1),
+(-2, -1, 'RC86', 'RC86356', 'A', 'N', -2),
+(-3, -2, 'RC86', 'RC86354', 'A', 'Y', -3),
+(-4, -3, 'RC86', 'RC86355', 'A', 'N', -4),
+(-5, -4, 'RC86', 'RC86356', 'A', 'Y', -5),
+(-6, -5, 'RC86', 'RC86359', 'A', 'Y', -6),
+(-7, -6, 'RC86', 'RC86360', 'A', 'Y', -7),
+(-8, -7, 'RC86', 'RC86360', 'A', 'Y', -8),
+(-9, -7, 'RC86', 'RC86355', 'A', 'Y', -9);
