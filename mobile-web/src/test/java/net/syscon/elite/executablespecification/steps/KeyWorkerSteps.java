a1d4e0e0fe92bbe0ef3196360baa8dcb63884d1b
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2018-01-31 07:41:39 +0000

    changed endpoint to /key-worker/${agencyId}/available

--- mobile-web/src/test/java/net/syscon/elite/executablespecification/steps/KeyWorkerSteps.java
+++ mobile-web/src/test/java/net/syscon/elite/executablespecification/steps/KeyWorkerSteps.java
@@ -2,7 +2,6 @@ package net.syscon.elite.executablespecification.steps;
 
 import net.syscon.elite.api.model.Keyworker;
 import net.syscon.elite.test.EliteClientException;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -13,8 +12,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class KeyWorkerSteps extends CommonSteps{
-    private static final String KEY_WORKER_API_URL = API_PREFIX + "key-worker/available";
-    private static final String KEY_WORKER_API_URL_WITH_AGENCY_PARAM = KEY_WORKER_API_URL + "?agencyId=%s";
+    private static final String KEY_WORKER_API_URL_WITH_AGENCY_PARAM = API_PREFIX + "key-worker/%s/available";
 
     private List<Keyworker> keyworkerList;
 
@@ -29,7 +27,7 @@ public class KeyWorkerSteps extends CommonSteps{
     private void doListApiCall(String agencyId) {
         init();
 
-        String queryUrl = StringUtils.isNotBlank(agencyId) ? String.format(KEY_WORKER_API_URL_WITH_AGENCY_PARAM, agencyId) : KEY_WORKER_API_URL;
+        String queryUrl = String.format(KEY_WORKER_API_URL_WITH_AGENCY_PARAM, agencyId);
 
         try {
             ResponseEntity<List<Keyworker>> response =
