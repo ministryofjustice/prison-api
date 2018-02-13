64c1a1fab40705425fc1c8412a395bdd688634c4
Peter Bell <peter.bell@syscon.net>
2018-02-13 09:44:44 +0000

    EA-277 'Merge Elite flyway test data and Nomis data'. PROFILE_TYPES: Add NOT NULL and PRIMARY KEY constraints to elite DDL to align it with the reference schema. Elite & NOMIS seed data are identical apart from MODIFIED_DATE being set from sysdate in NOMIS. Moved NOMIS version to db/migration/common and removed Elite version. PROFILE_CODES: Added NOT NULL and PRIMARY KEY constraints to Elite DDL to align with reference Elite schema.

--- mobile-web/src/main/resources/db/migration/elite/ddl/V1_15__profile_codes.sql
+++ mobile-web/src/main/resources/db/migration/elite/ddl/V1_15__profile_codes.sql
@@ -1,17 +1,18 @@
 CREATE TABLE PROFILE_CODES
 (
-  "PROFILE_TYPE"                  VARCHAR2(12 CHAR),
-  "PROFILE_CODE"                  VARCHAR2(12 CHAR),
+  "PROFILE_TYPE"                  VARCHAR2(12 CHAR)                       NOT NULL,
+  "PROFILE_CODE"                  VARCHAR2(12 CHAR)                       NOT NULL,
   "DESCRIPTION"                   VARCHAR2(40 CHAR),
-  "LIST_SEQ"                      NUMBER(6, 0),
-  "UPDATE_ALLOWED_FLAG"           VARCHAR2(1 CHAR)  DEFAULT 'Y',
-  "ACTIVE_FLAG"                   VARCHAR2(1 CHAR)  DEFAULT 'Y',
+  "LIST_SEQ"                      NUMBER(6, 0)                            NOT NULL,
+  "UPDATE_ALLOWED_FLAG"           VARCHAR2(1 CHAR)  DEFAULT 'Y'           NOT NULL,
+  "ACTIVE_FLAG"                   VARCHAR2(1 CHAR)  DEFAULT 'Y'           NOT NULL,
   "EXPIRY_DATE"                   DATE,
-  "USER_ID"                       VARCHAR2(32 CHAR),
-  "MODIFIED_DATE"                 DATE,
-  "CREATE_DATETIME"               TIMESTAMP(9)      DEFAULT systimestamp,
-  "CREATE_USER_ID"                VARCHAR2(32 CHAR) DEFAULT USER,
-  "MODIFY_DATETIME"               TIMESTAMP(9),
+  "USER_ID"                       VARCHAR2(32 CHAR)                       NOT NULL,
+  "MODIFIED_DATE"                 DATE                                    NOT NULL,
+  "CREATE_DATETIME"               TIMESTAMP(9)      DEFAULT systimestamp  NOT NULL,
+  "CREATE_USER_ID"                VARCHAR2(32 CHAR) DEFAULT USER          NOT NULL,
+  "MODIFY_DATETIME"               TIMESTAMP(9)      DEFAULT systimestamp,
   "MODIFY_USER_ID"                VARCHAR2(32 CHAR),
-  "SEAL_FLAG"                     VARCHAR2(1 CHAR)
+  "SEAL_FLAG"                     VARCHAR2(1 CHAR),
+  CONSTRAINT PROFILE_CODES_PK PRIMARY KEY (PROFILE_TYPE, PROFILE_CODE)
 );
