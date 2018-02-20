7138012f8e64d745ffefdb5dbef978771f3bca64
Peter Bell <peter.bell@syscon.net>
2018-02-20 14:47:37 +0000

    EA-277 'Merge Elite flyway test data and Nomis data'. OIC_HEARING_RESULTS: Elite and NOMIS data are identical.  Moved this data to db/migration/data and removed Elite version.

--- mobile-web/src/main/resources/db/migration/elite/ddl/V1_41__oic_hearing_results.sql
+++ mobile-web/src/main/resources/db/migration/elite/ddl/V1_41__oic_hearing_results.sql
@@ -1,18 +1,20 @@
-CREATE TABLE OIC_HEARING_RESULTS (
---'The results recorded during the OIC hearing.'
-    OIC_HEARING_ID NUMBER(10) NOT NULL,                          --System generated primary key for hearing.';
-    RESULT_SEQ NUMBER(6) NOT NULL,                               --Sequential number for hearing results.';
-    AGENCY_INCIDENT_ID NUMBER(10) NOT NULL,                      --System generated seqential log number for the incident.';
-    CHARGE_SEQ NUMBER(6) NOT NULL,                               --Sequential number for charge.';
-    PLEA_FINDING_CODE VARCHAR2(12) NOT NULL,                     --Reference Code ( FINDING ). The offenders plea on this charge.';
-    FINDING_CODE VARCHAR2(12) NOT NULL,                          --Reference Code ( FINDING )';
-    CREATE_DATETIME TIMESTAMP (9) DEFAULT SYSTIMESTAMP NOT NULL, --The timestamp when the record is created';
-    CREATE_USER_ID VARCHAR2(32) DEFAULT user NOT NULL,           --The user who creates the record';
-    MODIFY_DATETIME TIMESTAMP (9) DEFAULT SYSTIMESTAMP,          --The timestamp when the record is modified ';
-    MODIFY_USER_ID VARCHAR2(32),                                 --The user who modifies the record';
-    OIC_OFFENCE_ID NUMBER(10) NOT NULL, 
-    SEAL_FLAG VARCHAR2(1),
+CREATE TABLE OIC_HEARING_RESULTS
+(
+    OIC_HEARING_ID     NUMBER(10)                          NOT NULL,
+    RESULT_SEQ         NUMBER(6)                           NOT NULL,
+    AGENCY_INCIDENT_ID NUMBER(10)                          NOT NULL,
+    CHARGE_SEQ         NUMBER(6)                           NOT NULL,
+    PLEA_FINDING_CODE  VARCHAR2(12)                        NOT NULL,
+    FINDING_CODE       VARCHAR2(12)                        NOT NULL,
+    CREATE_DATETIME    TIMESTAMP (9) DEFAULT SYSTIMESTAMP  NOT NULL,
+    CREATE_USER_ID     VARCHAR2(32) DEFAULT user           NOT NULL,
+    MODIFY_DATETIME    TIMESTAMP (9) DEFAULT SYSTIMESTAMP,          
+    MODIFY_USER_ID     VARCHAR2(32),                                
+    OIC_OFFENCE_ID     NUMBER(10)                          NOT NULL,
+    SEAL_FLAG          VARCHAR2(1),
+
      CONSTRAINT OIC_HEARING_RESULTS_PK PRIMARY KEY (OIC_HEARING_ID, RESULT_SEQ),
+
      CONSTRAINT OIC_HEARING_RESULTS_UK UNIQUE (OIC_HEARING_ID, AGENCY_INCIDENT_ID, CHARGE_SEQ)
 );
 
