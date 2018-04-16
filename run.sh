947b9e4d9328892eca9cd07e028b17b9d5e94bbb
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-04-16 10:29:33 +0100

    ADd oracle J2EE 1.3 compliance back in

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 exec java ${JAVA_OPTS} \
   -Dcom.sun.management.jmxremote.local.only=false \
   -Djava.security.egd=file:/dev/./urandom \
+  -Doracle.jdbc.J2EE13Compliant=true \
   -jar /app/app.jar
