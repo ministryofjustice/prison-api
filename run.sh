6f4afbe8af5c2ced516f5aa142438634ab3579bd
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-04-16 10:27:18 +0100

    Build improvements to match keyworker service

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-NAME=${NAME:-mobile-web}
-
-JAR=$(find . -name ${NAME}*.jar|head -1)
-java ${JAVA_OPTS} -Doracle.jdbc.J2EE13Compliant=true -Dcom.sun.management.jmxremote.local.only=false -Djava.security.egd=file:/dev/./urandom -jar "${JAR}"
-
+exec java ${JAVA_OPTS} \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Djava.security.egd=file:/dev/./urandom \
+  -jar /app/app.jar
