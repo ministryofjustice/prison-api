1881db51d52743a798c6a4cbc934f2f692f081f8
Peter Bell <peter.bell@digital.justice.gov.uk>
2018-06-01 09:04:36 +0100

    NN-746 'Create New Elite V2 Endpoint for HDC/Licences' Introduce simpler, faster(?) offenderCurfews(agencyIds) method on repository.

--- mobile-web/src/test/java/net/syscon/elite/repository/OffenderCurfewRepositoryTest.java
+++ mobile-web/src/test/java/net/syscon/elite/repository/OffenderCurfewRepositoryTest.java
@@ -3,6 +3,7 @@ package net.syscon.elite.repository;
 
 import net.syscon.elite.service.support.OffenderCurfew;
 import net.syscon.elite.web.config.PersistenceConfigs;
+import org.apache.commons.collections.collection.CompositeCollection;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,8 +19,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
@@ -32,6 +32,31 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 @ContextConfiguration(classes = PersistenceConfigs.class)
 
 public class OffenderCurfewRepositoryTest {
+
+    private static final Set<OffenderCurfew> CURFEWS_LEI = new HashSet<>(Arrays.asList(
+            offenderCurfew(1, -1, "2018-01-01", null, "2018-03-01"),
+            offenderCurfew(2, -1, "2018-02-01", "APPROVED", "2018-03-01"),
+            offenderCurfew(3, -1, "2018-02-01", "REJECTED", "2018-03-01"),
+            offenderCurfew(4, -2, "2018-01-01", "APPROVED", "2018-03-01"),
+            offenderCurfew(5, -2, "2018-02-01", null, null),
+            offenderCurfew(6, -2, "2018-02-01", "APPROVED", null),
+            offenderCurfew(7, -3, "2018-01-01", "REJECTED", null),
+            offenderCurfew(8, -3, "2018-02-01", "REJECTED", null),
+            offenderCurfew(9, -3, "2018-02-01", null, null),
+            offenderCurfew(10, -4, "2018-01-01", null, null),
+            offenderCurfew(11, -5, "2018-01-01", "APPROVED", null),
+            offenderCurfew(12, -6, "2018-01-01", "APPROVED", "2018-04-01"),
+            offenderCurfew(13, -6, null, null, null),
+            offenderCurfew(14, -6, "2018-02-01", "APPROVED", null),
+            offenderCurfew(15, -7, null, "REJECTED", null),
+            offenderCurfew(16, -7, "2018-01-01", "REJECTED", null),
+            offenderCurfew(17, -7, "2018-01-01", "REJECTED", "2018-04-01"),
+            offenderCurfew(18, -7, null, null, null),
+            offenderCurfew(19, -7, "2018-02-01", "REJECTED", null)));
+
+    private static final Set<OffenderCurfew> CURFEWS_BXI = Collections.singleton(
+            offenderCurfew(30, -36, "2018-01-01", null, null)
+    );
 
     @Autowired
     private OffenderCurfewRepository repository;
@@ -55,31 +80,21 @@ public class OffenderCurfewRepositoryTest {
     }
 
     @Test
-    public void shouldReturnAllOffenderCurfewForAgency() {
-        Collection<OffenderCurfew> results = repository.offenderCurfews(Collections.singleton("LEI"));
-        assertThat(results).containsOnly(
-                offenderCurfew(1, -1, "2018-01-01", null, "2018-03-01"),
-                offenderCurfew(2, -1, "2018-02-01", "APPROVED", "2018-03-01"),
-                offenderCurfew(3, -1, "2018-02-01", "REJECTED", "2018-03-01"),
-                offenderCurfew(4, -2, "2018-01-01", "APPROVED", "2018-03-01"),
-                offenderCurfew(5, -2, "2018-02-01", null, null),
-                offenderCurfew(6, -2, "2018-02-01", "APPROVED", null),
-                offenderCurfew(7, -3, "2018-01-01", "REJECTED", null),
-                offenderCurfew(8, -3, "2018-02-01", "REJECTED", null),
-                offenderCurfew(9, -3, "2018-02-01", null, null),
-                offenderCurfew(10, -4, "2018-01-01", null, null),
-                offenderCurfew(11, -5, "2018-01-01", "APPROVED", null),
-                offenderCurfew(12, -6, "2018-01-01", "APPROVED", "2018-04-01"),
-                offenderCurfew(13, -6, null, null, null),
-                offenderCurfew(14, -6, "2018-02-01", "APPROVED", null),
-                offenderCurfew(15, -7, null, "REJECTED", null),
-                offenderCurfew(16, -7, "2018-01-01", "REJECTED", null),
-                offenderCurfew(17, -7, "2018-01-01", "REJECTED", "2018-04-01"),
-                offenderCurfew(18, -7, null, null, null),
-                offenderCurfew(19, -7, "2018-02-01", "REJECTED", null));
+    public void shouldReturnAllOffenderCurfewForAgencyLEI() {
+        assertThat(repository.offenderCurfews(agencyIds("LEI"))).containsAll(CURFEWS_LEI);
     }
 
-    private OffenderCurfew offenderCurfew(long offenderCurfewId, long offenderBookId, String assessmentDate, String approvalStatus, String ardCrdDate) {
+    @Test
+    public void shouldReturnAllOffenderCurfewForAgencyBXI() {
+        assertThat(repository.offenderCurfews(agencyIds("BXI"))).containsAll(CURFEWS_BXI);
+    }
+
+    @Test
+    public void shouldReturnAllOffenderCurfewForAgencyBXIandLEI() {
+        assertThat(repository.offenderCurfews(agencyIds("LEI", "BXI"))).containsAll(union(CURFEWS_LEI, CURFEWS_BXI));
+    }
+
+    private static OffenderCurfew offenderCurfew(long offenderCurfewId, long offenderBookId, String assessmentDate, String approvalStatus, String ardCrdDate) {
 
         return OffenderCurfew
                 .builder()
@@ -91,8 +106,21 @@ public class OffenderCurfewRepositoryTest {
                 .build();
     }
 
-    private LocalDate toLocalDate(String string) {
+    private static LocalDate toLocalDate(String string) {
         if (string == null) return null;
         return LocalDate.parse(string);
+    }
+
+    private static Set<String> agencyIds(String... agencyIds) {
+        return new HashSet<>(Arrays.asList(agencyIds));
+    }
+
+    @SafeVarargs
+    private static <T> Set<T> union(Set<T>... sets) {
+        Set<T> result = new HashSet<>();
+        for (Set<T> set : sets) {
+            result.addAll(set);
+        }
+        return result;
     }
 }
