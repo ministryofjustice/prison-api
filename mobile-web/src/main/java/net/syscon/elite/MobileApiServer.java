c2ca5c7c035ebad235f9cb7411308a230221ebf4
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-07-12 13:17:33 +0100

    Remove all senstive endpoints as gateway will expose them now!

--- mobile-web/src/main/java/net/syscon/elite/MobileApiServer.java
+++ mobile-web/src/main/java/net/syscon/elite/MobileApiServer.java
@@ -2,8 +2,12 @@ package net.syscon.elite;
 
 import net.syscon.elite.core.Constants;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.HealthEndpoint;
+import org.springframework.boot.actuate.endpoint.mvc.HealthMvcEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 import java.io.File;
@@ -20,6 +24,19 @@ public class MobileApiServer {
                 && projectDir.exists()) {
             System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, Constants.SPRING_PROFILE_DEVELOPMENT);
         }
+    }
+
+    /**
+     * Override default health endpoint (defined in EndpointWebMvcManagementContextConfiguration) to disable restricted
+     * security mode but keep security for other endpoints such as /dump etc
+     */
+    @Bean
+    public HealthMvcEndpoint healthMvcEndpoint(@Autowired(required = false) HealthEndpoint delegate) {
+        if (delegate == null) {
+            // This happens in unit test environment
+            return null;
+        }
+        return new HealthMvcEndpoint(delegate, false, null);
     }
 
     public static void main(final String[] args) throws Exception {
