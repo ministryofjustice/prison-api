3619779337545b1876561c752f1ec94060a39941
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-07-25 16:25:10 +0100

    Move Swagger to be at root of gateway so it can be rendered without proxy

--- mobile-web/src/main/java/net/syscon/elite/web/config/SwaggerWebConfig.java
+++ mobile-web/src/main/java/net/syscon/elite/web/config/SwaggerWebConfig.java
@@ -2,7 +2,6 @@ package net.syscon.elite.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,16 +10,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerWebConfig extends WebMvcConfigurerAdapter {
 
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/docs/api/swagger.json", "/api/swagger.json").setKeepQueryParams(true);
-        registry.addRedirectViewController("/docs/swagger-resources/configuration/ui","/swagger-resources/configuration/ui");
-        registry.addRedirectViewController("/docs/swagger-resources/configuration/security","/swagger-resources/configuration/security");
-        registry.addRedirectViewController("/docs/swagger-resources", "/swagger-resources");
-    }
-
-    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/docs/**")
+        registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/META-INF/resources/");
     }
 }
