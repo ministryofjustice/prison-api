2689528cf04fa40f60bb9a3c48b2d32846ad1ddb
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2018-10-02 09:12:16 +0100

    NN-1193 - access roles by username and caseload

--- mobile-web/src/test/java/net/syscon/elite/service/impl/UserServiceImplTest.java
+++ mobile-web/src/test/java/net/syscon/elite/service/impl/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package net.syscon.elite.service.impl;
 
 import com.google.common.collect.ImmutableList;
+import net.syscon.elite.api.model.CaseLoad;
 import net.syscon.elite.api.model.UserDetail;
 import net.syscon.elite.api.model.UserRole;
 import net.syscon.elite.api.support.Order;
@@ -15,6 +16,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
@@ -28,6 +30,8 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceImplTest {
+    private static final String USERNAME_GEN = "HH_GEN";
+    private static final String LEEDS_CASELOAD_ID = "LEI";
     @Mock
     private UserRepository userRepository;
 
@@ -47,42 +51,43 @@ public class UserServiceImplTest {
     @Test
     public void testGetUsersByCaseload() {
         PageRequest pr = new PageRequest("lastName", Order.ASC, 0L, 10L);  //the default if non provided
-        when(userRepository.findUsersByCaseload("LEI", "A_ROLE", "A", null)).thenReturn(pageResponse(2));
+        when(userRepository.findUsersByCaseload(LEEDS_CASELOAD_ID, "A_ROLE", "A", null)).thenReturn(pageResponse(2));
 
-        userService.getUsersByCaseload("LEI", "A", "A_ROLE", null);
+        userService.getUsersByCaseload(LEEDS_CASELOAD_ID, "A", "A_ROLE", null);
 
-        verify(userRepository, times(1)).findUsersByCaseload(eq("LEI"), eq("A_ROLE"), eq("A"), refEq(pr));
+        verify(userRepository, times(1)).findUsersByCaseload(eq(LEEDS_CASELOAD_ID), eq("A_ROLE"), eq("A"), refEq(pr));
     }
 
     @Test
     public void testGetUsersByCaseloadWithSortFieldDifferentToDefault() {
         PageRequest pr = new PageRequest("firstName", Order.ASC, 10L, 20L);
-        when(userRepository.findUsersByCaseload("LEI", "A_ROLE", "A", pr)).thenReturn(pageResponse(2));
+        when(userRepository.findUsersByCaseload(LEEDS_CASELOAD_ID, "A_ROLE", "A", pr)).thenReturn(pageResponse(2));
 
-        userService.getUsersByCaseload("LEI", "A", "A_ROLE", pr);
+        userService.getUsersByCaseload(LEEDS_CASELOAD_ID, "A", "A_ROLE", pr);
 
-        verify(userRepository, times(1)).findUsersByCaseload(eq("LEI"), eq("A_ROLE"), eq("A"), eq(pr));
+        verify(userRepository, times(1)).findUsersByCaseload(eq(LEEDS_CASELOAD_ID), eq("A_ROLE"), eq("A"), eq(pr));
     }
 
     @Test
     public void testGetRolesByUserAndCaseload() {
-        List<UserRole> list = ImmutableList.of(UserRole.builder().roleCode("TEST_CODE").roleName("Test Role").caseloadId("LEI").roleId(123L).build());  //the default if non provided
-        when(userRepository.findAccessRolesByUsernameAndCaseload("HH_GEN", "LEI")).thenReturn(list);
+        List<UserRole> list = ImmutableList.of(UserRole.builder().roleCode("TEST_CODE").roleName("Test Role").caseloadId(LEEDS_CASELOAD_ID).roleId(123L).build());  //the default if non provided
+        when(caseLoadService.getCaseLoad(Mockito.eq(LEEDS_CASELOAD_ID))).thenReturn(Optional.of(CaseLoad.builder().build()));
+        when(userRepository.findAccessRolesByUsernameAndCaseload(USERNAME_GEN, LEEDS_CASELOAD_ID)).thenReturn(list);
 
-        userService.getAccessRolesByUserAndCaseload("HH_GEN", "LEI");
+        userService.getAccessRolesByUserAndCaseload(USERNAME_GEN, LEEDS_CASELOAD_ID);
 
-        verify(userRepository, times(1)).findAccessRolesByUsernameAndCaseload(eq("HH_GEN"), eq("LEI"));
+        verify(userRepository, times(1)).findAccessRolesByUsernameAndCaseload(USERNAME_GEN, LEEDS_CASELOAD_ID);
     }
 
     @Test(expected = EntityNotFoundException.class)
     public void testGetRolesByUserAndCaseloadCaseloadDoesNotExist() {
-        when(caseLoadService.getCaseLoad("LEI")).thenReturn(Optional.empty());
-        userService.getAccessRolesByUserAndCaseload("HH_GEN", "LEI");
+        when(caseLoadService.getCaseLoad(Mockito.eq(LEEDS_CASELOAD_ID))).thenReturn(Optional.empty());
+        userService.getAccessRolesByUserAndCaseload(USERNAME_GEN, LEEDS_CASELOAD_ID);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testGetRolesByUserAndCaseloadUsernameNotProvided() {
-        userService.getAccessRolesByUserAndCaseload("", "LEI");
+        userService.getAccessRolesByUserAndCaseload("", LEEDS_CASELOAD_ID);
     }
 
     private Page<UserDetail> pageResponse(int userCount) {
