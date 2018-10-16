7f2cf9714dfe72ac8c8a31db6d85fedfd33d2747
Peter Bell <peter.bell@digital.justice.gov.uk>
2018-10-16 10:44:47 +0100

    NN-1223 'Prod Elite2 API database connection issue'. Set default pool stats reporting period to 360 sec - with environment variable override.

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ exec java ${JAVA_OPTS} \
   -Dcom.sun.management.jmxremote.local.only=false \
   -Djava.security.egd=file:/dev/./urandom \
   -Doracle.jdbc.J2EE13Compliant=true \
+  -Dcom.zaxxer.hikari.housekeeping.periodMs=${CONNECTION_POOL_STATS_PERIOD:-360000} \
   -jar /app/app.jar
