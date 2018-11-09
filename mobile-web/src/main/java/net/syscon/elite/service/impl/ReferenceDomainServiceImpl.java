374ca6810e10e198a8d96f2c5a601a32de9a2b39
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-11-09 00:29:23 +0000

    Remove Tabs

--- mobile-web/src/main/java/net/syscon/elite/service/impl/ReferenceDomainServiceImpl.java
+++ mobile-web/src/main/java/net/syscon/elite/service/impl/ReferenceDomainServiceImpl.java
@@ -21,85 +21,85 @@ import java.util.stream.Collectors;
 @Service
 @Transactional(readOnly = true)
 public class ReferenceDomainServiceImpl implements ReferenceDomainService {
-	private final ReferenceCodeRepository referenceCodeRepository;
+    private final ReferenceCodeRepository referenceCodeRepository;
 
-	public ReferenceDomainServiceImpl(ReferenceCodeRepository referenceCodeRepository) {
-		this.referenceCodeRepository = referenceCodeRepository;
-	}
+    public ReferenceDomainServiceImpl(ReferenceCodeRepository referenceCodeRepository) {
+        this.referenceCodeRepository = referenceCodeRepository;
+    }
 
-	private static String getDefaultOrderBy(String orderBy) {
-		return StringUtils.defaultIfBlank(orderBy, "code");
-	}
+    private static String getDefaultOrderBy(String orderBy) {
+        return StringUtils.defaultIfBlank(orderBy, "code");
+    }
 
-	private static Order getDefaultOrder(Order order) {
-		return Objects.isNull(order) ? Order.ASC : order;
-	}
+    private static Order getDefaultOrder(Order order) {
+        return Objects.isNull(order) ? Order.ASC : order;
+    }
 
-	@Override
-	public Page<ReferenceCode> getAlertTypes(String orderBy, Order order, long offset, long limit) {
-		return referenceCodeRepository.getReferenceCodesByDomain(
-				ReferenceDomain.ALERT.getDomain(),true,
-				getDefaultOrderBy(orderBy), getDefaultOrder(order),
-				offset, limit);
-	}
+    @Override
+    public Page<ReferenceCode> getAlertTypes(String orderBy, Order order, long offset, long limit) {
+        return referenceCodeRepository.getReferenceCodesByDomain(
+                ReferenceDomain.ALERT.getDomain(), true,
+                getDefaultOrderBy(orderBy), getDefaultOrder(order),
+                offset, limit);
+    }
 
-	@Override
-	public Page<ReferenceCode> getCaseNoteSources(String orderBy, Order order, long offset, long limit) {
-		return referenceCodeRepository.getReferenceCodesByDomain(
-				ReferenceDomain.CASE_NOTE_SOURCE.getDomain(),false,
-				getDefaultOrderBy(orderBy), getDefaultOrder(order),
-				offset, limit);
-	}
+    @Override
+    public Page<ReferenceCode> getCaseNoteSources(String orderBy, Order order, long offset, long limit) {
+        return referenceCodeRepository.getReferenceCodesByDomain(
+                ReferenceDomain.CASE_NOTE_SOURCE.getDomain(), false,
+                getDefaultOrderBy(orderBy), getDefaultOrder(order),
+                offset, limit);
+    }
 
-	@Override
-	public Page<ReferenceCode> getCaseNoteTypes(String orderBy, Order order, long offset, long limit) {
-		return referenceCodeRepository.getReferenceCodesByDomain(
-				ReferenceDomain.CASE_NOTE_TYPE.getDomain(),true,
-				getDefaultOrderBy(orderBy), getDefaultOrder(order),
-				offset, limit);
-	}
+    @Override
+    public Page<ReferenceCode> getCaseNoteTypes(String orderBy, Order order, long offset, long limit) {
+        return referenceCodeRepository.getReferenceCodesByDomain(
+                ReferenceDomain.CASE_NOTE_TYPE.getDomain(), true,
+                getDefaultOrderBy(orderBy), getDefaultOrder(order),
+                offset, limit);
+    }
 
-	@Override
-	public Page<ReferenceCode> getReferenceCodesByDomain(String domain, boolean withSubCodes, String orderBy, Order order, long offset, long limit) {
-		verifyReferenceDomain(domain);
+    @Override
+    public Page<ReferenceCode> getReferenceCodesByDomain(String domain, boolean withSubCodes, String orderBy, Order order, long offset, long limit) {
+        verifyReferenceDomain(domain);
 
-		return referenceCodeRepository.getReferenceCodesByDomain(
-				domain, withSubCodes, getDefaultOrderBy(orderBy), getDefaultOrder(order), offset, limit);
-	}
+        return referenceCodeRepository.getReferenceCodesByDomain(
+                domain, withSubCodes, getDefaultOrderBy(orderBy), getDefaultOrder(order), offset, limit);
+    }
 
-	@Override
-	public Optional<ReferenceCode> getReferenceCodeByDomainAndCode(String domain, String code, boolean withSubCodes) {
-		verifyReferenceDomain(domain);
-		verifyReferenceCode(domain, code);
+    @Override
+    public Optional<ReferenceCode> getReferenceCodeByDomainAndCode(String domain, String code, boolean withSubCodes) {
+        verifyReferenceDomain(domain);
+        verifyReferenceCode(domain, code);
 
-		return referenceCodeRepository.getReferenceCodeByDomainAndCode(domain, code, withSubCodes);
-	}
+        return referenceCodeRepository.getReferenceCodeByDomainAndCode(domain, code, withSubCodes);
+    }
 
-	private void verifyReferenceDomain(String domain) {
-		referenceCodeRepository.getReferenceDomain(domain)
-				.orElseThrow(EntityNotFoundException.withMessage("Reference domain [%s] not found.", domain));
-	}
+    private void verifyReferenceDomain(String domain) {
+        referenceCodeRepository.getReferenceDomain(domain)
+                .orElseThrow(EntityNotFoundException.withMessage("Reference domain [%s] not found.", domain));
+    }
 
-	private void verifyReferenceCode(String domain, String code) {
-		referenceCodeRepository.getReferenceCodeByDomainAndCode(domain, code, false)
-				.orElseThrow(EntityNotFoundException.withMessage("Reference code for domain [%s] and code [%s] not found.", domain, code));
-	}
+    private void verifyReferenceCode(String domain, String code) {
+        referenceCodeRepository.getReferenceCodeByDomainAndCode(domain, code, false)
+                .orElseThrow(EntityNotFoundException.withMessage("Reference code for domain [%s] and code [%s] not found.", domain, code));
+    }
 
     @Override
     public List<ReferenceCode> getScheduleReasons(String eventType) {
         verifyReferenceCode(ReferenceDomain.INTERNAL_SCHEDULE_TYPE.getDomain(), eventType);
 
-		List<ReferenceCode> scheduleReasons = referenceCodeRepository.getScheduleReasons(eventType);
-		return tidyDescriptionAndSort(scheduleReasons);
+        List<ReferenceCode> scheduleReasons = referenceCodeRepository.getScheduleReasons(eventType);
+        return tidyDescriptionAndSort(scheduleReasons);
     }
 
-	private List<ReferenceCode> tidyDescriptionAndSort(List<ReferenceCode> refCodes) {
-		return refCodes.stream()
-				.map(p -> ReferenceCode.builder()
-						.code(p.getCode())
-						.description(WordUtils.capitalizeFully(p.getDescription()))
-						.build())
-				.sorted(Comparator.comparing(ReferenceCode::getDescription))
-				.collect(Collectors.toList());
-	}
+    private List<ReferenceCode> tidyDescriptionAndSort(List<ReferenceCode> refCodes) {
+        return refCodes.stream()
+                .map(p -> ReferenceCode.builder()
+                        .code(p.getCode())
+                        .description(WordUtils.capitalizeFully(p.getDescription()))
+                        .build())
+                .sorted(Comparator.comparing(ReferenceCode::getDescription))
+                .collect(Collectors.toList());
+    }
 }
