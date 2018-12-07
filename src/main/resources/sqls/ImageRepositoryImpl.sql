1d1a01343e2f867436e1593e8ee4c4abda526313
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-12-07 12:48:14 +0000

    Simplify getting latest image for offender

--- src/main/resources/sqls/ImageRepositoryImpl.sql
+++ src/main/resources/sqls/ImageRepositoryImpl.sql
@@ -22,19 +22,12 @@ FIND_IMAGE_CONTENT_BY_OFFENDER_NO {
   FROM OFFENDER_BOOKINGS B
          JOIN OFFENDERS O ON B.OFFENDER_ID = O.OFFENDER_ID
          JOIN OFFENDER_IMAGES I ON I.OFFENDER_IMAGE_ID =
-                                   (SELECT OI.OFFENDER_IMAGE_ID
+                                   (SELECT MAX(OI.OFFENDER_IMAGE_ID)
                                     FROM OFFENDER_IMAGES OI
                                     WHERE OI.ACTIVE_FLAG = 'Y'
                                       AND IMAGE_OBJECT_TYPE = 'OFF_BKG'
                                       AND OI.OFFENDER_BOOK_ID = B.OFFENDER_BOOK_ID
                                       AND OI.IMAGE_VIEW_TYPE = 'FACE'
-                                      AND OI.ORIENTATION_TYPE = 'FRONT'
-                                      AND CREATE_DATETIME = (SELECT MAX(CREATE_DATETIME)
-                                                             FROM OFFENDER_IMAGES
-                                                             WHERE ACTIVE_FLAG = 'Y'
-                                                               AND IMAGE_OBJECT_TYPE = 'OFF_BKG'
-                                                               AND OFFENDER_BOOK_ID = B.OFFENDER_BOOK_ID
-                                                               AND IMAGE_VIEW_TYPE = 'FACE'
-                                                               AND ORIENTATION_TYPE = 'FRONT'))
+                                      AND OI.ORIENTATION_TYPE = 'FRONT')
   WHERE B.BOOKING_SEQ = 1 AND O.OFFENDER_ID_DISPLAY = :offenderNo
 }
