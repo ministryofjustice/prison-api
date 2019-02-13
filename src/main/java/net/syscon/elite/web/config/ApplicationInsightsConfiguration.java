35ad19e00d16893d60fb73bae32d5407ee71f811
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-02-13 15:38:17 +0000

    NN-1660 Invert Logic

--- src/main/java/net/syscon/elite/web/config/ApplicationInsightsConfiguration.java
+++ src/main/java/net/syscon/elite/web/config/ApplicationInsightsConfiguration.java
@@ -24,7 +24,7 @@ public class ApplicationInsightsConfiguration {
         @Override
         public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
             final var telemetryKey = context.getEnvironment().getProperty("application.insights.ikey");
-            return StringUtils.isNotBlank(telemetryKey);
+            return StringUtils.isBlank(telemetryKey);
         }
     }
 }
