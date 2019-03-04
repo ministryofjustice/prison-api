96f9085ad90d3a45894f7336f8d4d2f18732781b
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2019-03-04 15:46:46 +0000

    Added email address methods to the staff repository, staff service and staff resource classes. Added unit tests for the staff service.

--- src/main/java/net/syscon/elite/service/StaffService.java
+++ src/main/java/net/syscon/elite/service/StaffService.java
@@ -24,6 +24,8 @@ public interface StaffService {
 
     StaffDetail getStaffDetail(Long staffId);
 
+    public List<String> getStaffEmailAddresses(Long staffId);
+
     Page<StaffLocationRole> getStaffByAgencyPositionRole(GetStaffRoleRequest request, PageRequest pageRequest);
 
     StaffDetail getStaffDetailByPersonnelIdentifier(String idType, String id);
---
+++ src/test/java/net/syscon/elite/service/impl/StaffServiceImpl.java
@@ -0,0 +1,73 @@
+package net.syscon.elite.service.impl;
+
+import net.syscon.elite.repository.StaffRepository;
+import net.syscon.elite.repository.UserRepository;
+import net.syscon.elite.service.EntityNotFoundException;
+import net.syscon.elite.service.StaffService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.ws.rs.BadRequestException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StaffServiceImpl {
+
+    private static final Long ID_MULTIPLE = 8888L;
+    private static final Long ID_SINGLE = 9999L;
+    private static final Long ID_NONE = 9999L;
+    private static final Long ID_BAD = 123L;
+    private List<String> multipleAddresses = Arrays.asList(new String[] {"test1@a.com", "test2@b.com", "test3@c.com"});
+    private List<String> singleAddress = Arrays.asList(new String[] {"test1@a.com"});
+    private List<String> emptyList = new ArrayList<>();
+
+    @Mock
+    public StaffRepository staffRepository;
+
+    @Mock
+    public UserRepository userRepository;
+
+    private StaffService staffService;
+
+    @Before
+    public void init() {
+        staffService = new StaffServiceImpl(staffRepository, userRepository);
+    }
+
+    @Test
+    public void testGetStaffEmailMultipleExist() {
+        when(staffRepository.findEmailAddressesForStaffId(ID_MULTIPLE)).thenReturn(multipleAddresses);
+        List<String> addreses = staffService.getStaffEmailAddresses(ID_MULTIPLE);
+        // Assertions for list content - multiple entries
+        verify(staffRepository, times(1)).findEmailAddressesForStaffId(ID_MULTIPLE);
+    }
+
+    @Test
+    public void testGetStaffEmailSingleExists() {
+        when(staffRepository.findEmailAddressesForStaffId(ID_SINGLE)).thenReturn(singleAddress);
+        List<String> addreses = staffService.getStaffEmailAddresses(ID_SINGLE);
+        // Assertions for list content - one entry
+        verify(staffRepository, times(1)).findEmailAddressesForStaffId(ID_SINGLE);
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void testGetStaffEmailsNoneExist() {
+        when(staffRepository.findEmailAddressesForStaffId(ID_NONE)).thenThrow(new EntityNotFoundException(""));
+        List<String> addreses = staffService.getStaffEmailAddresses(ID_NONE);
+        verify(staffRepository, times(1)).findEmailAddressesForStaffId(ID_NONE);
+    }
+
+    @Test(expected = BadRequestException.class)
+    public void testGetStaffEmailInvalidId() {
+        when(staffRepository.findEmailAddressesForStaffId(ID_BAD)).thenReturn(emptyList);
+        List<String> addreses = staffService.getStaffEmailAddresses(ID_NONE);
+        verify(staffRepository, times(1)).findEmailAddressesForStaffId(ID_NONE);
+    }
+}
