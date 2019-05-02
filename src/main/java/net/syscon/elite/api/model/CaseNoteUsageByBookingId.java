d0727e5e7b6f51a3f2a287acc8477e49d8791683
Peter Phillips <peter@greenthistle.co.uk>
2019-05-02 09:02:03 +0100

    NN-1883: Improvements after code review

--- src/main/java/net/syscon/elite/api/model/CaseNoteUsageByBookingId.java
+++ src/main/java/net/syscon/elite/api/model/CaseNoteUsageByBookingId.java
@@ -5,8 +5,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @ApiModel(description = "Case Note Type Usage By Booking Id")
@@ -17,23 +15,18 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @Data
 public class CaseNoteUsageByBookingId {
-    @ApiModelProperty(required = true, value = "Booking Id", example = "123456")
-    @NotBlank
+    @ApiModelProperty(required = true, value = "Booking Id", position = 1, example = "123456")
     private Integer bookingId;
 
-    @ApiModelProperty(required = true, value = "Case Note Type", position = 1, example = "KA")
-    @NotBlank
+    @ApiModelProperty(required = true, value = "Case Note Type", position = 2, example = "KA")
     private String caseNoteType;
 
-    @ApiModelProperty(required = true, value = "Case Note Sub Type", position = 2, example = "KS")
-    @NotBlank
+    @ApiModelProperty(required = true, value = "Case Note Sub Type", position = 3, example = "KS")
     private String caseNoteSubType;
 
-    @ApiModelProperty(required = true, value = "Number of case notes of this type/subtype", position = 3, example = "5")
-    @NotNull
+    @ApiModelProperty(required = true, value = "Number of case notes of this type/subtype", position = 4, example = "5")
     private Integer numCaseNotes;
 
-    @ApiModelProperty(required = true, value = "Last case note of this type", position = 4, example = "2018-12-01T14:55:23")
-    @NotNull
+    @ApiModelProperty(required = true, value = "Last case note of this type", position = 5, example = "2018-12-01T14:55:23")
     private LocalDateTime latestCaseNote;
 }
