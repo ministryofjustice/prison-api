9d4a9f1b615c74a15fa1a6e1ac487f03083f3114
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2019-06-10 13:35:30 +0100

    NN-1677 Adding swagger examples (#69)

--- src/main/java/net/syscon/elite/api/model/Agency.java
+++ src/main/java/net/syscon/elite/api/model/Agency.java
@@ -1,101 +1,31 @@
 package net.syscon.elite.api.model;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * Agency Details
- **/
-@SuppressWarnings("unused")
 @ApiModel(description = "Agency Details")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
+@Data
 public class Agency {
-    @JsonIgnore
-    private Map<String, Object> additionalProperties;
-    
     @NotBlank
+    @ApiModelProperty(required = true, value = "Agency identifier.", example = "MDI")
     private String agencyId;
 
     @NotBlank
+    @ApiModelProperty(required = true, value = "Agency description.", example = "Moorland (HMP & YOI)")
     private String description;
 
     @NotBlank
+    @ApiModelProperty(required = true, value = "Agency type.", example = "INST")
     private String agencyType;
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return additionalProperties == null ? new HashMap<>() : additionalProperties;
-    }
-
-    @ApiModelProperty(hidden = true)
-    @JsonAnySetter
-    public void setAdditionalProperties(final Map<String, Object> additionalProperties) {
-        this.additionalProperties = additionalProperties;
-    }
-
-    /**
-      * Agency identifier.
-      */
-    @ApiModelProperty(required = true, value = "Agency identifier.")
-    @JsonProperty("agencyId")
-    public String getAgencyId() {
-        return agencyId;
-    }
-
-    public void setAgencyId(final String agencyId) {
-        this.agencyId = agencyId;
-    }
-
-    /**
-      * Agency description.
-      */
-    @ApiModelProperty(required = true, value = "Agency description.")
-    @JsonProperty("description")
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    /**
-      * Agency type.
-      */
-    @ApiModelProperty(required = true, value = "Agency type.")
-    @JsonProperty("agencyType")
-    public String getAgencyType() {
-        return agencyType;
-    }
-
-    public void setAgencyType(final String agencyType) {
-        this.agencyType = agencyType;
-    }
-
-    @Override
-    public String toString()  {
-        final var sb = new StringBuilder();
-
-        sb.append("class Agency {\n");
-        
-        sb.append("  agencyId: ").append(agencyId).append("\n");
-        sb.append("  description: ").append(description).append("\n");
-        sb.append("  agencyType: ").append(agencyType).append("\n");
-        sb.append("}\n");
-
-        return sb.toString();
-    }
 }
