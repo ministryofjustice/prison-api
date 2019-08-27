6586f8b87548448d39488575b923448ab4f80566
JamieDraperUK <draper65@googlemail.com>
2019-08-27 13:21:25 +0100

    NN-2159 improvements after review

--- src/test/java/net/syscon/elite/util/Extractors.java
+++ src/test/java/net/syscon/elite/util/Extractors.java
@@ -7,19 +7,19 @@ import java.util.Map;
 import java.util.function.Function;
 
 public class Extractors {
-    public static Function<Object, String> extractString(String field) {
+    public static Function<Object, String> extractString(final String field) {
         return m -> ((Map<String, String>) m).get(field);
     }
 
-    public static Function<Object, Integer> extractInteger(String field) {
+    public static Function<Object, Integer> extractInteger(final String field) {
         return m -> ((Map<String, BigDecimal>) m).get(field).intValue();
     }
 
-    public static Function<Object, Long> extractLong(String field) {
+    public static Function<Object, Long> extractLong(final String field) {
         return m -> ((Map<String, BigDecimal>) m).get(field).longValue();
     }
 
-    public static Function<Object, LocalDate> extractDate(String field) {
+    public static Function<Object, LocalDate> extractDate(final String field) {
         return m -> ((Map<String, Timestamp>) m).get(field).toLocalDateTime().toLocalDate();
     }
 }
