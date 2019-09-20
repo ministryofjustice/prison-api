a4b46e1e414cdabcf46717165e086c81e5a35d80
Peter Phillips <peter@greenthistle.co.uk>
2019-09-20 09:33:43 +0100

    NN-2188: Validate whereabouts patterns and fix duplicates

--- src/test/java/net/syscon/elite/service/impl/whereabouts/LocationGroupPropertiesConfigurationTest.java
+++ src/test/java/net/syscon/elite/service/impl/whereabouts/LocationGroupPropertiesConfigurationTest.java
@@ -8,8 +8,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.Properties;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,6 +41,30 @@ public class LocationGroupPropertiesConfigurationTest {
         assertThat(properties)
                 .isNotEmpty()
                 .containsKeys("MDI_Houseblock 1", "WCI_Segregation");
+    }
+
+    @Test
+    public void whereaboutsGroups_AreAllPatternsThatCompile() {
+        properties.values().forEach((e) -> {
+            final var patterns = ((String) e).split(",");
+            Arrays.stream(patterns).map(Pattern::compile).forEach(p -> {
+                //noinspection ResultOfMethodCallIgnored
+                p.matcher("some input");
+            });
+        });
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Test
+    public void whereaboutsGroups_NoDuplicateValues() {
+        final var values = properties.values();
+        // have to split as each line of properties file can contain multiple patterns
+        values.stream().flatMap(a -> Arrays.stream(((String) a).split(",")))
+                // then collect into a map which checks for duplicates in the merge function
+                .collect(Collectors.toMap((v) -> v, (v) -> v, (a, b) -> {
+                    assertThat(a).isNotEqualTo(b);
+                    return a;
+                }));
     }
 
     @Test
