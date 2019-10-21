9ecfe3861dfb627121ac5d5195ed545f95b9312a
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2019-10-21 13:07:03 +0100

    DT-318: added telemetry module to log in app insights the client ID and username of each API request

---
+++ src/test/java/net/syscon/elite/web/config/ClientTrackingTelemetryModuleTest.java
@@ -0,0 +1,92 @@
+package net.syscon.elite.web.config;
+
+import com.microsoft.applicationinsights.web.internal.RequestTelemetryContext;
+import com.microsoft.applicationinsights.web.internal.ThreadContext;
+import net.syscon.elite.util.JwtAuthenticationHelper;
+import net.syscon.elite.util.JwtParameters;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@Import({JwtAuthenticationHelper.class, ClientTrackingTelemetryModule.class})
+@ContextConfiguration(initializers = {ConfigFileApplicationContextInitializer.class})
+@ActiveProfiles("test")
+public class ClientTrackingTelemetryModuleTest {
+
+    @Autowired
+    private ClientTrackingTelemetryModule clientTrackingTelemetryModule;
+
+    @Autowired
+    private JwtAuthenticationHelper jwtAuthenticationHelper;
+
+    @Before
+    public void setup() {
+        ThreadContext.setRequestTelemetryContext(new RequestTelemetryContext(1L));
+    }
+
+    @After
+    public void tearDown() {
+        ThreadContext.remove();
+    }
+
+    @Test
+    public void shouldAddClientIdAndUserNameToInsightTelemetry() {
+
+        final var token = createJwt("bob", List.of(), 1L);
+
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        clientTrackingTelemetryModule.onBeginRequest(req, res);
+
+        final var insightTelemetry = ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry().getProperties();
+
+        assertThat(insightTelemetry).hasSize(2);
+        assertThat(insightTelemetry.get("user_name")).isEqualTo("bob");
+        assertThat(insightTelemetry.get("client_id")).isEqualTo("elite2apiclient");
+
+    }
+
+    @Test
+    public void shouldNotAddClientIdAndUserNameToInsightTelemetryAsTokenExpired() {
+
+        final var token = createJwt("Fred", List.of(), -1L);
+
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        MockHttpServletResponse res = new MockHttpServletResponse();
+
+        clientTrackingTelemetryModule.onBeginRequest(req, res);
+
+        final var insightTelemetry = ThreadContext.getRequestTelemetryContext().getHttpRequestTelemetry().getProperties();
+
+        assertThat(insightTelemetry).isEmpty();
+    }
+
+    private String createJwt(final String user, final List<String> roles, Long duration) {
+        return jwtAuthenticationHelper.createJwt(JwtParameters.builder()
+                .username(user)
+                .roles(roles)
+                .scope(List.of("read", "write"))
+                .expiryTime(Duration.ofDays(duration))
+                .build());
+    }
+
+}
