26b1e3116bf394de3365e3c01d6c6acd278bfbd2
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-11-26 16:22:41 +0000

    DT-226 Add Test

--- src/test/java/net/syscon/elite/api/resource/impl/PrisonersResourceTest.java
+++ src/test/java/net/syscon/elite/api/resource/impl/PrisonersResourceTest.java
@@ -1,17 +1,11 @@
 package net.syscon.elite.api.resource.impl;
 
-import net.syscon.elite.api.model.PrisonerDetail;
-import net.syscon.elite.api.model.v1.Events;
-import net.syscon.elite.executablespecification.steps.AuthTokenHelper;
 import net.syscon.elite.executablespecification.steps.AuthTokenHelper.AuthToken;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.json.JsonContent;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.core.ResolvableType.forType;
+import java.util.Map;
 
 public class PrisonersResourceTest extends ResourceTest {
     @Test
@@ -44,6 +38,22 @@ public class PrisonersResourceTest extends ResourceTest {
                 });
 
         assertThatJsonFileAndStatus(response, 200, "prisoners_single.json");
+    }
+
+    @Test
+    public void testCanReturnPrisonerInformationAtLocation() {
+        final var token = authTokenHelper.getToken(AuthToken.GLOBAL_SEARCH);
+
+        final var httpEntity = createHttpEntity(token, null, Map.of("Page-Limit", "100"));
+
+        final var response = testRestTemplate.exchange(
+                "/api/prisoners/at-location/LEI",
+                HttpMethod.GET,
+                httpEntity,
+                new ParameterizedTypeReference<String>() {
+                });
+
+        assertThatJsonFileAndStatus(response, 200, "prisoners_information.json");
     }
 
 }
