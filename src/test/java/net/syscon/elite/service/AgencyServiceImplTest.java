74bbd63684793f88c7177145b1a3b0ced3bce519
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-02-03 11:57:21 +0000

    DT-526 Update readme

--- src/test/java/net/syscon/elite/service/AgencyServiceImplTest.java
+++ src/test/java/net/syscon/elite/service/AgencyServiceImplTest.java
@@ -15,7 +15,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
