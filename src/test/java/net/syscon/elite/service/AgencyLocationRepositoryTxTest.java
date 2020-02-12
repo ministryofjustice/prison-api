dff8163bec63c16da00f9cdd8792714c05a38369
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-02-12 10:14:00 +0000

    DT-552 - Add extra tx test for persistence.

---
+++ src/test/java/net/syscon/elite/service/AgencyLocationRepositoryTxTest.java
@@ -0,0 +1,74 @@
+package net.syscon.elite.service;
+
+import net.syscon.elite.api.model.StaffUserRole;
+import net.syscon.elite.repository.jpa.model.ActiveFlag;
+import net.syscon.elite.repository.jpa.model.AgencyLocation;
+import net.syscon.elite.repository.jpa.repository.AgencyLocationRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.TestTransaction;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@Transactional
+public class AgencyLocationRepositoryTxTest {
+
+    public static final String TEST_AGY_ID = "TEST";
+
+    @Autowired
+    private AgencyLocationRepository repository;
+
+    @Test
+    @WithUserDetails("ITAG_USER")
+    public void testPersistingAgency() throws NoSuchFieldException, IllegalAccessException {
+        final var newAgency = AgencyLocation.builder()
+                .id(TEST_AGY_ID)
+                .description("A Test Agency")
+                .activeFlag(ActiveFlag.Y)
+                .type("NEWTYPE")
+                .build();
+
+        final var persistedEntity = repository.save(newAgency);
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+
+        Assertions.assertThat(persistedEntity.getId()).isNotNull();
+
+        TestTransaction.start();
+
+        final var retrievedAgency = repository.findById(TEST_AGY_ID).orElseThrow();
+        Assertions.assertThat(retrievedAgency).isEqualTo(persistedEntity);
+
+        // Check that the user name and timestamp is set
+        final var createUserId = ReflectionTestUtils.getField(retrievedAgency, AgencyLocation.class, "createUserId");
+        Assertions.assertThat(createUserId).isEqualTo("ITAG_USER");
+
+        final var createDatetime = ReflectionTestUtils.getField(retrievedAgency, AgencyLocation.class, "createDatetime");
+        Assertions.assertThat(createDatetime).isNotNull();
+
+
+        //Clean up
+        repository.delete(retrievedAgency);
+
+        TestTransaction.flagForCommit();
+        TestTransaction.end();
+        TestTransaction.start();
+
+        Assertions.assertThat(repository.findById(TEST_AGY_ID)).isEmpty();
+    }
+}
