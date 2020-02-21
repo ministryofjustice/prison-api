490fb9a69e0cda9a5897f7f9003bbbddd3ecb5ff
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-02-21 13:44:55 +0000

    Fleshed out the request model and renamed endpoint.

--- src/main/java/net/syscon/elite/api/resource/OffenderMovementsResource.java
+++ src/main/java/net/syscon/elite/api/resource/OffenderMovementsResource.java
@@ -7,24 +7,24 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import net.syscon.elite.api.model.CourtEvent;
 import net.syscon.elite.api.model.ErrorResponse;
-import net.syscon.elite.api.model.ScheduleCourtEvent;
+import net.syscon.elite.api.model.PrisonToCourtEvent;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Api(tags = {"/bookings"})
+@Api(tags = {"/movements"})
 public interface OffenderMovementsResource {
-    //
-    // TODO - create the model object for POST operation
-    //
-    @PostMapping("/{bookingId}/prison-to-court")
+    @PostMapping("/{bookingId}/prison-to-court-events")
+    @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Schedules a prison to court event for an offender.", notes = "Schedules a prison to court event for an offender.", nickname = "prisonToCourtEvent")
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Court event created.", response = CourtEvent.class),
             @ApiResponse(code = 400, message = "Invalid request.", response = ErrorResponse.class),
             @ApiResponse(code = 404, message = "Requested resource not found.", response = ErrorResponse.class),
             @ApiResponse(code = 500, message = "Unrecoverable error occurred whilst processing request.", response = ErrorResponse.class)})
-    ResponseEntity<CourtEvent> prisonToCourtEvent(@ApiParam(value = "The offender booking id.", required = true) @PathVariable("bookingId") Long bookingId,
-                                                  @ApiParam(value = "The court event to be scheduled.", required = true) @RequestBody ScheduleCourtEvent event);
+    ResponseEntity<CourtEvent> prisonToCourtEvent(@ApiParam(value = "The offender booking to associate the court event with.", required = true) @PathVariable("bookingId") Long bookingId,
+                                                  @ApiParam(value = "The prison to court event to be scheduled for the offender booking.", required = true) @RequestBody PrisonToCourtEvent event);
 }
--- src/main/java/net/syscon/elite/api/resource/impl/OffenderMovementsResourceImpl.java
+++ src/main/java/net/syscon/elite/api/resource/impl/OffenderMovementsResourceImpl.java
@@ -1,7 +1,7 @@
 package net.syscon.elite.api.resource.impl;
 
 import net.syscon.elite.api.model.CourtEvent;
-import net.syscon.elite.api.model.ScheduleCourtEvent;
+import net.syscon.elite.api.model.PrisonToCourtEvent;
 import net.syscon.elite.api.resource.OffenderMovementsResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("${api.base.path}/bookings")
+@RequestMapping("${api.base.path}/movements")
 @Validated
 public class OffenderMovementsResourceImpl implements OffenderMovementsResource {
     //
-    // TODO what role(s) do we need to enforce here?
+    // TODO - WIP what role(s) do we need to enforce here?
     //
     @Override
-    public ResponseEntity<CourtEvent> prisonToCourtEvent(final  Long bookingId, final ScheduleCourtEvent event) {
+    public ResponseEntity<CourtEvent> prisonToCourtEvent(final Long bookingId, final PrisonToCourtEvent event) {
         //
-        // TODO this is currently stubbed
+        // TODO - WIP this is currently a stubbed response
         //
-        return  ResponseEntity.status(HttpStatus.CREATED).body(CourtEvent.builder().build());
+        return ResponseEntity.status(HttpStatus.CREATED).body(CourtEvent.builder().commentText("This is a stubbed response.").build());
     }
 }
