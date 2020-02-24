7d801e8caf83f4bf0c3270fbb0eb6607b9b738bc
matt-crowson-moj <60104344+matt-crowson-moj@users.noreply.github.com>
2020-02-24 16:15:50 +0000

    Added optional comments field to model object and fleshed out its constraints. (#384)

--- src/main/java/net/syscon/elite/api/model/PrisonToCourtEvent.java
+++ src/main/java/net/syscon/elite/api/model/PrisonToCourtEvent.java
@@ -8,6 +8,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @ApiModel(description = "Represents the data required to schedule a prison to court event for an offender.")
@@ -19,14 +22,24 @@ import java.time.LocalDateTime;
 public class PrisonToCourtEvent {
 
     @ApiModelProperty(required = true, value = "The court case identifier to link the event to.", position = 1, example = "1")
+    @NotNull(message = "The court case identifier must be provided.")
     private Long courtCaseId;
 
-    @ApiModelProperty(required = true, value = "The prison agency code where the offender will be moved from.", position = 2, example = "LEI")
+    @ApiModelProperty(required = true, value = "The prison (agency code) where the offender will be moved from.", position = 2, example = "LEI")
+    @NotBlank(message = "The from prison location must be provided.")
+    @Size(max = 6, message = "From location must be a maximum of 6 characters.")
     private String fromPrisonLocation;
 
-    @ApiModelProperty(required = true, value = "The court agency code where the offender will moved to.", position = 3, example = "LEEDSCC")
+    @ApiModelProperty(required = true, value = "The court (agency code) where the offender will moved to.", position = 3, example = "LEEDSCC")
+    @NotBlank(message = "The court location to be moved to must be provided.")
+    @Size(max = 6, message = "To location must be a maximum of 6 characters.")
     private String toCourtLocation;
 
     @ApiModelProperty(required = true, value = "The date and time of the court event.", position = 4, example = "2020-02-28T14:40:00.000Z")
+    @NotBlank(message = "The court event date time must be provided.")
     private LocalDateTime courtEventDateTime;
+
+    @ApiModelProperty(value = "Any comments related to the court case.", position = 5, example = "Restricted access to parking level.")
+    @Size(max = 240, message = "Comment text must be a maximum of 240 characters.")
+    private String comments;
 }
