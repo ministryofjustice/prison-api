b2bc92ebbf776838a0226ae8cbdf6f104a311f64
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-02-26 14:45:55 +0000

    DT-370 New KeyPair generation not requiring Spring Security Oauth2

--- src/test/java/net/syscon/elite/util/JwtAuthenticationHelper.java
+++ src/test/java/net/syscon/elite/util/JwtAuthenticationHelper.java
@@ -5,11 +5,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+//import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.stereotype.Component;
 
-import java.security.KeyPair;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
@@ -20,11 +22,13 @@ public class JwtAuthenticationHelper {
 
     public JwtAuthenticationHelper(@Value("${jwt.signing.key.pair}") final String privateKeyPair,
                                    @Value("${jwt.keystore.password}") final String keystorePassword,
-                                   @Value("${jwt.keystore.alias:elite2api}") final String keystoreAlias) {
+                                   @Value("${jwt.keystore.alias:elite2api}") final String keystoreAlias) throws GeneralSecurityException {
 
-        final var keyStoreKeyFactory = new KeyStoreKeyFactory(new ByteArrayResource(Base64.decodeBase64(privateKeyPair)),
-                keystorePassword.toCharArray());
-        keyPair = keyStoreKeyFactory.getKeyPair(keystoreAlias);
+//        final var keyStoreKeyFactory = new KeyStoreKeyFactory(new ByteArrayResource(Base64.decodeBase64(privateKeyPair)),
+//                keystorePassword.toCharArray());
+//        keyPair = keyStoreKeyFactory.getKeyPair(keystoreAlias);
+
+        keyPair = keyPair(privateKeyPair);
     }
 
     public String createJwt(final JwtParameters parameters) {
@@ -48,4 +52,25 @@ public class JwtAuthenticationHelper {
                 .signWith(SignatureAlgorithm.RS256, keyPair.getPrivate())
                 .compact();
     }
+
+    private static PrivateKey privateKey(String key64) throws GeneralSecurityException {
+        byte[] clear = Base64.decodeBase64(key64);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(clear);
+        KeyFactory fact = KeyFactory.getInstance("DSA");
+        PrivateKey priv = fact.generatePrivate(keySpec);
+        Arrays.fill(clear, (byte) 0);
+        return priv;
+    }
+
+    private static PublicKey publicKey(String stored) throws GeneralSecurityException {
+        byte[] data = Base64.decodeBase64(stored);
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+        KeyFactory fact = KeyFactory.getInstance("DSA");
+        return fact.generatePublic(spec);
+    }
+
+    private static KeyPair keyPair(String key) throws GeneralSecurityException {
+        return new KeyPair(publicKey(key), privateKey(key));
+    }
+
 }
