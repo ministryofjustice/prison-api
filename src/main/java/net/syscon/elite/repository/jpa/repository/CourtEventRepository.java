cc8257c1c5c57571a665f6369545eaf8ab622eb5
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2020-02-26 15:31:41 +0000

    DT-576 initial JPA model to support creation of court events. (#389)
    * DT-576 initial JPA model to support creation of court events.
    
    * DT-576 adding repo and simple test for creation of court events.

---
+++ src/main/java/net/syscon/elite/repository/jpa/repository/CourtEventRepository.java
@@ -0,0 +1,7 @@
+package net.syscon.elite.repository.jpa.repository;
+
+import net.syscon.elite.repository.jpa.model.CourtEvent;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourtEventRepository extends CrudRepository<CourtEvent, Long> {
+}
