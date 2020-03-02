7eb1d19e1854c1ebf6b48f3c63d10ebecc9ff548
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-03-02 13:33:09 +0000

    DT-370 Add mock jwks server abd generate tokens with builder

--- src/test/java/net/syscon/elite/util/JwtAuthenticationHelper.java
+++ src/test/java/net/syscon/elite/util/JwtAuthenticationHelper.java
@@ -1,17 +1,21 @@
 package net.syscon.elite.util;
 
-
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.security.*;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
@@ -22,21 +26,20 @@ public class JwtAuthenticationHelper {
 
     public JwtAuthenticationHelper(@Value("${jwt.signing.key.pair}") final String privateKeyPair,
                                    @Value("${jwt.keystore.password}") final String keystorePassword,
-                                   @Value("${jwt.keystore.alias:elite2api}") final String keystoreAlias) throws GeneralSecurityException {
-
-//        final var keyStoreKeyFactory = new KeyStoreKeyFactory(new ByteArrayResource(Base64.decodeBase64(privateKeyPair)),
-//                keystorePassword.toCharArray());
-//        keyPair = keyStoreKeyFactory.getKeyPair(keystoreAlias);
-
-        keyPair = keyPair(privateKeyPair);
+                                   @Value("${jwt.keystore.alias:elite2api}") final String keystoreAlias) {
+        keyPair = getKeyPair(new ByteArrayResource(Base64.decodeBase64(privateKeyPair)), keystoreAlias, keystorePassword.toCharArray());
     }
 
     public String createJwt(final JwtParameters parameters) {
 
         final var claims = new HashMap<String, Object>();
 
-        claims.put("user_name", parameters.getUsername());
-        claims.put("client_id", "elite2apiclient");
+
+        if (parameters.getUsername() != null) {
+            claims.put("user_name", parameters.getUsername());
+        }
+        claims.put("client_id", parameters.getClientId());
+        claims.put("internalUser", parameters.isInternalUser());
 
         if (parameters.getRoles() != null && !parameters.getRoles().isEmpty())
             claims.put("authorities", parameters.getRoles());
@@ -50,27 +53,37 @@ public class JwtAuthenticationHelper {
                 .addClaims(claims)
                 .setExpiration(new Date(System.currentTimeMillis() + parameters.getExpiryTime().toMillis()))
                 .signWith(SignatureAlgorithm.RS256, keyPair.getPrivate())
+                .setIssuer("http://localhost:8080/auth/issuer")
+                .setHeaderParam("typ", "JWT")
                 .compact();
     }
 
-    private static PrivateKey privateKey(String key64) throws GeneralSecurityException {
-        byte[] clear = Base64.decodeBase64(key64);
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(clear);
-        KeyFactory fact = KeyFactory.getInstance("DSA");
-        PrivateKey priv = fact.generatePrivate(keySpec);
-        Arrays.fill(clear, (byte) 0);
-        return priv;
+    private KeyPair getKeyPair(Resource resource, String alias, char[] password) {
+        InputStream inputStream = null;
+        KeyStore store;
+        try {
+            store = KeyStore.getInstance("jks");
+            inputStream = resource.getInputStream();
+            store.load(inputStream, password);
+            final RSAPrivateCrtKey key = (RSAPrivateCrtKey) store.getKey(alias, password);
+            final RSAPublicKeySpec spec = new RSAPublicKeySpec(key.getModulus(), key.getPublicExponent());
+            final PublicKey publicKey = KeyFactory.getInstance("RSA").generatePublic(spec);
+            return new KeyPair(publicKey, key);
+        }
+        catch (Exception e) {
+            throw new IllegalStateException("Cannot load keys from store: " + resource, e);
+        }
+        finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+            catch (IOException e) {
+                throw new IllegalStateException("Cannot close open stream: ", e);
+            }
+        }
     }
 
-    private static PublicKey publicKey(String stored) throws GeneralSecurityException {
-        byte[] data = Base64.decodeBase64(stored);
-        X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
-        KeyFactory fact = KeyFactory.getInstance("DSA");
-        return fact.generatePublic(spec);
-    }
-
-    private static KeyPair keyPair(String key) throws GeneralSecurityException {
-        return new KeyPair(publicKey(key), privateKey(key));
-    }
 
 }
