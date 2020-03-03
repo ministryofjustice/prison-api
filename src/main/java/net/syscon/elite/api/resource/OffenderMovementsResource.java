eb1f61b88660e796662b5ead334fe743e0e45689
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2020-03-03 15:46:56 +0000

    DT-600 changing movement endpoint of court events to bookings endpoint for court hearings. (#400)
    Co-authored-by: Jon Brighton <brightonsbox@hotmail.com>

--- src/main/java/net/syscon/elite/api/resource/OffenderMovementsResource.java
+++ src/main/java/net/syscon/elite/api/resource/OffenderMovementsResource.java
@@ -5,9 +5,9 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import net.syscon.elite.api.model.CourtEvent;
+import net.syscon.elite.api.model.CourtHearing;
 import net.syscon.elite.api.model.ErrorResponse;
-import net.syscon.elite.api.model.PrisonToCourtEvent;
+import net.syscon.elite.api.model.PrisonToCourtHearing;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,16 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Api(tags = {"/movements"})
+@Api(tags = {"/bookings"})
 public interface OffenderMovementsResource {
-    @PostMapping("/{bookingId}/prison-to-court-events")
+    @PostMapping("/{bookingId}/prison-to-court-hearings")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "Schedules a prison to court event for an offender.", notes = "Schedules a prison to court event for an offender.", nickname = "prisonToCourtEvent")
+    @ApiOperation(value = "Schedules a prison to court hearing for an offender.", notes = "Schedules a prison to court hearing for an offender.", nickname = "prisonToCourtHearing")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Court event created.", response = CourtEvent.class),
+            @ApiResponse(code = 201, message = "Court hearing created.", response = CourtHearing.class),
             @ApiResponse(code = 400, message = "Invalid request.", response = ErrorResponse.class),
             @ApiResponse(code = 404, message = "Requested resource not found.", response = ErrorResponse.class),
             @ApiResponse(code = 500, message = "Unrecoverable error occurred whilst processing request.", response = ErrorResponse.class)})
-    ResponseEntity<CourtEvent> prisonToCourtEvent(@ApiParam(value = "The offender booking to associate the court event with.", required = true) @PathVariable("bookingId") Long bookingId,
-                                                  @ApiParam(value = "The prison to court event to be scheduled for the offender booking.", required = true) @RequestBody PrisonToCourtEvent event);
+    ResponseEntity<CourtHearing> prisonToCourtHearing(@ApiParam(value = "The offender booking to associate the court hearing with.", required = true) @PathVariable("bookingId") Long bookingId,
+                                                      @ApiParam(value = "The prison to court hearing to be scheduled for the offender booking.", required = true) @RequestBody PrisonToCourtHearing hearing);
 }
