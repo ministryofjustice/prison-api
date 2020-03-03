abf298e699582fdf15575ab5c8d472a54b70493e
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2020-03-03 14:36:28 +0000

    Dt 370 jwk server (#395)
    Add jwk set and issuer endpoints, and switch to Spring Security 5.

--- src/main/java/net/syscon/elite/web/config/ResourceServerConfiguration.java
+++ src/main/java/net/syscon/elite/web/config/ResourceServerConfiguration.java
@@ -3,26 +3,22 @@ package net.syscon.elite.web.config;
 import net.syscon.elite.security.EntryPointUnauthorizedHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.Collection;
 
 @Configuration
-@EnableResourceServer
 @EnableScheduling
 @EnableAsync(proxyTargetClass = true)
-public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
-    private final ResourceServerTokenServices tokenServices;
-
-    public ResourceServerConfiguration(final ResourceServerTokenServices tokenServices) {
-        this.tokenServices = tokenServices;
-    }
+public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public EntryPointUnauthorizedHandler unauthorizedHandler() {
@@ -37,18 +33,14 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
                 // Can't have CSRF protection as requires session
                 .and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/webjars/**", "/favicon.ico", "/csrf",
+                .authorizeRequests(auth ->
+                    auth.antMatchers("/webjars/**", "/favicon.ico", "/csrf",
                         "/health", "/info", "/ping", "/health/ping", "/h2-console/**",
                         "/v2/api-docs", "/api/swagger.json",
                         "/swagger-ui.html", "/swagger-resources", "/swagger-resources/configuration/ui",
                         "/swagger-resources/configuration/security").permitAll()
-                .anyRequest()
-                .authenticated();
-    }
-
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices);
+                    .anyRequest().authenticated()
+                ).oauth2ResourceServer()
+                    .jwt().jwtAuthenticationConverter(new AuthAwareAuthenticationConverter());
     }
 }
