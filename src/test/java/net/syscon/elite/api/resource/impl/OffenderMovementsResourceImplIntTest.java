45685a7811934cf8879df608d77262f84a37709c
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-03-11 16:00:32 +0000

    DT-604 adding more integration test cases for scheduling of court hearings.

--- src/test/java/net/syscon/elite/api/resource/impl/OffenderMovementsResourceImplIntTest.java
+++ src/test/java/net/syscon/elite/api/resource/impl/OffenderMovementsResourceImplIntTest.java
@@ -1,5 +1,6 @@
 package net.syscon.elite.api.resource.impl;
 
+import net.syscon.elite.api.model.ErrorResponse;
 import net.syscon.elite.executablespecification.steps.AuthTokenHelper;
 import org.junit.Test;
 import org.springframework.core.ParameterizedTypeReference;
@@ -7,13 +8,15 @@ import org.springframework.http.HttpMethod;
 
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OffenderMovementsResourceImplIntTest extends ResourceTest {
 
     @Test
-    public void schedule_court_hearing() {
+    public void schedules_court_hearing() {
         final var token = authTokenHelper.getToken(AuthTokenHelper.AuthToken.NORMAL_USER);
 
-        final var httpEntity = createHttpEntity(token, Map.of(
+        final var request = createHttpEntity(token, Map.of(
                 "courtCaseId", -1,
                 "fromPrisonLocation", "LEI",
                 "toCourtLocation", "COURT1",
@@ -23,10 +26,110 @@ public class OffenderMovementsResourceImplIntTest extends ResourceTest {
         final var response = testRestTemplate.exchange(
                 "/api/bookings/-1/prison-to-court-hearings",
                 HttpMethod.POST,
-                httpEntity,
+                request,
                 new ParameterizedTypeReference<String>() {
                 });
 
         assertThatJsonFileAndStatus(response, 201, "court_hearing.json");
+    }
+
+    @Test
+    public void schedule_court_hearing_fails_when_no_matching_booking() {
+        final var token = authTokenHelper.getToken(AuthTokenHelper.AuthToken.NORMAL_USER);
+
+        final var request = createHttpEntity(token, Map.of(
+                "courtCaseId", -1,
+                "fromPrisonLocation", "LEI",
+                "toCourtLocation", "COURT1",
+                "courtHearingDateTime", "2030-03-11T14:00:00.000Z"
+        ));
+
+        final var response = testRestTemplate.exchange(
+                "/api/bookings/9999999/prison-to-court-hearings",
+                HttpMethod.POST,
+                request,
+                ErrorResponse.class);
+
+        assertThat(response.getBody()).isEqualTo(
+                ErrorResponse.builder()
+                        .status(404)
+                        .userMessage("Resource with id [9999999] not found.")
+                        .developerMessage("Resource with id [9999999] not found.")
+                        .build());
+    }
+
+    @Test
+    public void schedule_court_hearing_fails_when_no_matching_prison() {
+        final var token = authTokenHelper.getToken(AuthTokenHelper.AuthToken.NORMAL_USER);
+
+        final var request = createHttpEntity(token, Map.of(
+                "courtCaseId", -1,
+                "fromPrisonLocation", "UNKNOWN_PRISON",
+                "toCourtLocation", "COURT1",
+                "courtHearingDateTime", "2030-03-11T14:00:00.000Z"
+        ));
+
+        final var response = testRestTemplate.exchange(
+                "/api/bookings/-1/prison-to-court-hearings",
+                HttpMethod.POST,
+                request,
+                ErrorResponse.class);
+
+        assertThat(response.getBody()).isEqualTo(
+                ErrorResponse.builder()
+                        .status(404)
+                        .userMessage("Resource with id [UNKNOWN_PRISON] not found.")
+                        .developerMessage("Resource with id [UNKNOWN_PRISON] not found.")
+                        .build());
+    }
+
+    @Test
+    public void schedule_court_hearing_fails_when_no_matching_court() {
+        final var token = authTokenHelper.getToken(AuthTokenHelper.AuthToken.NORMAL_USER);
+
+        final var request = createHttpEntity(token, Map.of(
+                "courtCaseId", -1,
+                "fromPrisonLocation", "LEI",
+                "toCourtLocation", "UNKNOWN_COURT",
+                "courtHearingDateTime", "2030-03-11T14:00:00.000Z"
+        ));
+
+        final var response = testRestTemplate.exchange(
+                "/api/bookings/-1/prison-to-court-hearings",
+                HttpMethod.POST,
+                request,
+                ErrorResponse.class);
+
+        assertThat(response.getBody()).isEqualTo(
+                ErrorResponse.builder()
+                        .status(404)
+                        .userMessage("Resource with id [UNKNOWN_COURT] not found.")
+                        .developerMessage("Resource with id [UNKNOWN_COURT] not found.")
+                        .build());
+    }
+
+    @Test
+    public void schedule_court_hearing_fails_when_no_matching_case_id() {
+        final var token = authTokenHelper.getToken(AuthTokenHelper.AuthToken.NORMAL_USER);
+
+        final var request = createHttpEntity(token, Map.of(
+                "courtCaseId", 8888888,
+                "fromPrisonLocation", "LEI",
+                "toCourtLocation", "COURT1",
+                "courtHearingDateTime", "2030-03-11T14:00:00.000Z"
+        ));
+
+        final var response = testRestTemplate.exchange(
+                "/api/bookings/-1/prison-to-court-hearings",
+                HttpMethod.POST,
+                request,
+                ErrorResponse.class);
+
+        assertThat(response.getBody()).isEqualTo(
+                ErrorResponse.builder()
+                        .status(404)
+                        .userMessage("Resource with id [8888888] not found.")
+                        .developerMessage("Resource with id [8888888] not found.")
+                        .build());
     }
 }
