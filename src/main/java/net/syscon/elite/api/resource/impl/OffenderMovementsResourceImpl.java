91dda108f697c60ea5158f1fba902056a6e3d7d2
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-03-12 08:13:08 +0000

    DT-604 removing unnecessary boiler plate code for endpoint.

--- src/main/java/net/syscon/elite/api/resource/impl/OffenderMovementsResourceImpl.java
+++ src/main/java/net/syscon/elite/api/resource/impl/OffenderMovementsResourceImpl.java
@@ -5,12 +5,9 @@ import net.syscon.elite.api.model.PrisonToCourtHearing;
 import net.syscon.elite.api.resource.OffenderMovementsResource;
 import net.syscon.elite.core.ProxyUser;
 import net.syscon.elite.service.CourtHearingsService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("${api.base.path}/bookings")
@@ -25,7 +22,7 @@ public class OffenderMovementsResourceImpl implements OffenderMovementsResource 
 
     @ProxyUser
     @Override
-    public ResponseEntity<CourtHearing> prisonToCourtHearing(final Long bookingId, final PrisonToCourtHearing hearing) {
-        return ResponseEntity.status(CREATED).body(courtHearingsService.scheduleHearing(bookingId, hearing));
+    public CourtHearing prisonToCourtHearing(final Long bookingId, final PrisonToCourtHearing hearing) {
+        return courtHearingsService.scheduleHearing(bookingId, hearing);
     }
 }
