880bf832e84fcf4797f4576b2ac5d5b85a98f67a
bell-pa <peter.bell@digital.justice.gov.uk>
2020-04-02 12:41:23 +0100

    DCS-198 Incidents by religion. DCS-199 Incidents by ethnic group.

--- src/main/java/net/syscon/elite/api/model/PrisonerDetail.java
+++ src/main/java/net/syscon/elite/api/model/PrisonerDetail.java
@@ -3,10 +3,7 @@ package net.syscon.elite.api.model;
 import com.fasterxml.jackson.annotation.*;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -24,73 +21,108 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Data
 public class PrisonerDetail {
     @JsonIgnore
     private Map<String, Object> additionalProperties;
 
+    @ApiModelProperty(required = true, value = "The prisoner's unique offender number (aka NOMS Number in the UK).")
     @NotBlank
     private String offenderNo;
 
+    @ApiModelProperty(value = "The prisoner's title.")
     private String title;
 
+    @ApiModelProperty(value = "The prisoner's name suffix.")
     private String suffix;
 
+    @ApiModelProperty(required = true, value = "The prisoner's first name.")
     @NotBlank
     private String firstName;
 
+    @ApiModelProperty(value = "The prisoner's middle name(s).")
     private String middleNames;
 
+    @ApiModelProperty(required = true, value = "The prisoner's last name.")
     @NotBlank
     private String lastName;
 
+    @ApiModelProperty(required = true, value = "The prisoner's date of birth (in YYYY-MM-DD format).")
     @NotNull
     private LocalDate dateOfBirth;
 
+    @ApiModelProperty(required = true, value = "The prisoner's gender.")
     @NotBlank
     private String gender;
 
+    @ApiModelProperty(required = true, value = "The prisoner's gender code.")
     @NotBlank
     private String sexCode;
 
+    @ApiModelProperty(value = "The prisoner's nationality.")
     private String nationalities;
 
+    @ApiModelProperty(required = true, value = "Flag (Y or N) to indicate if prisoner is currently in prison.")
     @NotBlank
     private String currentlyInPrison;
 
+    @ApiModelProperty(value = "ID of prisoner's latest booking.")
     private Long latestBookingId;
 
+    @ApiModelProperty(value = "Latest location ID of a prisoner (if in prison).")
     private String latestLocationId;
 
+    @ApiModelProperty(value = "Name of the prison where the prisoner resides (if in prison).")
     private String latestLocation;
 
+    @ApiModelProperty(value = "Name of the location where the prisoner resides (if in prison)")
     private String internalLocation;
 
+    @ApiModelProperty(value = "The prisoner's PNC (Police National Computer) number.")
     private String pncNumber;
 
+    @ApiModelProperty(value = "The prisoner's CRO (Criminal Records Office) number.")
     private String croNumber;
 
+    @ApiModelProperty(value = "The prisoner's ethnicity.")
     private String ethnicity;
 
+    @ApiModelProperty(value = "The prisoner's ethnicity code.")
+    private String ethnicityCode;
+
+    @ApiModelProperty(value = "The prisoner's country of birth.")
     private String birthCountry;
 
+    @ApiModelProperty(value = "The prisoner's religion.")
     private String religion;
 
+    @ApiModelProperty(value = "The prisoner's religion code.")
+    private String religionCode;
+
+    @ApiModelProperty(value = "Status code of prisoner's latest conviction.")
     private String convictedStatus;
 
+    @ApiModelProperty(value = "The prisoner's imprisonment status.")
     private String imprisonmentStatus;
 
+    @ApiModelProperty(value = "The prisoner's imprisonment status description.")
     private String imprisonmentStatusDesc;
 
+    @ApiModelProperty(value = "Date prisoner was received into the prison.")
     private LocalDate receptionDate;
 
+    @ApiModelProperty(value = "The prisoner's marital status.")
     private String maritalStatus;
 
+    @ApiModelProperty(required = true, value = "The prisoner's current working first name.")
     @NotBlank
     private String currentWorkingFirstName;
 
+    @ApiModelProperty(required = true, value = "The prisoner's current working last name.")
     @NotBlank
     private String currentWorkingLastName;
 
+    @ApiModelProperty(required = true, value = "The prisoner's current working date of birth (in YYYY-MM-DD format).")
     @NotNull
     private LocalDate currentWorkingBirthDate;
 
@@ -105,405 +137,38 @@ public class PrisonerDetail {
         this.additionalProperties = additionalProperties;
     }
 
-    /**
-     * The prisoner's unique offender number (aka NOMS Number in the UK).
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's unique offender number (aka NOMS Number in the UK).")
-    @JsonProperty("offenderNo")
-    public String getOffenderNo() {
-        return offenderNo;
-    }
-
-    public void setOffenderNo(final String offenderNo) {
-        this.offenderNo = offenderNo;
-    }
-
-    /**
-     * The prisoner's title.
-     */
-    @ApiModelProperty(value = "The prisoner's title.")
-    @JsonProperty("title")
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    /**
-     * The prisoner's name suffix.
-     */
-    @ApiModelProperty(value = "The prisoner's name suffix.")
-    @JsonProperty("suffix")
-    public String getSuffix() {
-        return suffix;
-    }
-
-    public void setSuffix(final String suffix) {
-        this.suffix = suffix;
-    }
-
-    /**
-     * The prisoner's first name.
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's first name.")
-    @JsonProperty("firstName")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(final String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * The prisoner's middle name(s).
-     */
-    @ApiModelProperty(value = "The prisoner's middle name(s).")
-    @JsonProperty("middleNames")
-    public String getMiddleNames() {
-        return middleNames;
-    }
-
-    public void setMiddleNames(final String middleNames) {
-        this.middleNames = middleNames;
-    }
-
-    /**
-     * The prisoner's last name.
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's last name.")
-    @JsonProperty("lastName")
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(final String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * The prisoner's date of birth (in YYYY-MM-DD format).
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's date of birth (in YYYY-MM-DD format).")
-    @JsonProperty("dateOfBirth")
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(final LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    /**
-     * The prisoner's sex code.
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's gender code.")
-    @JsonProperty("sexCode")
-    public String getSexCode() {
-        return sexCode;
-    }
-
-    public void setSexCode(final String sexCode) {
-        this.sexCode = sexCode;
-    }
-
-    /**
-     * The prisoner's gender.
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's gender.")
-    @JsonProperty("gender")
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(final String gender) {
-        this.gender = gender;
-    }
-
-    /**
-     * The prisoner's nationality.
-     */
-    @ApiModelProperty(value = "The prisoner's nationality.")
-    @JsonProperty("nationalities")
-    public String getNationalities() {
-        return nationalities;
-    }
-
-    public void setNationalities(final String nationalities) {
-        this.nationalities = nationalities;
-    }
-
-    /**
-     * Flag (Y or N) to indicate if prisoner is currently in prison.
-     */
-    @ApiModelProperty(required = true, value = "Flag (Y or N) to indicate if prisoner is currently in prison.")
-    @JsonProperty("currentlyInPrison")
-    public String getCurrentlyInPrison() {
-        return currentlyInPrison;
-    }
-
-    public void setCurrentlyInPrison(final String currentlyInPrison) {
-        this.currentlyInPrison = currentlyInPrison;
-    }
-
-    /**
-     * ID of prisoner's latest booking.
-     */
-    @ApiModelProperty(value = "ID of prisoner's latest booking.")
-    @JsonProperty("latestBookingId")
-    public Long getLatestBookingId() {
-        return latestBookingId;
-    }
-
-    public void setLatestBookingId(final Long latestBookingId) {
-        this.latestBookingId = latestBookingId;
-    }
-
-    /**
-     * Latest location ID of a prisoner (if in prison).
-     */
-    @ApiModelProperty(value = "Latest location ID of a prisoner (if in prison).")
-    @JsonProperty("latestLocationId")
-    public String getLatestLocationId() {
-        return latestLocationId;
-    }
-
-    public void setLatestLocationId(final String latestLocationId) {
-        this.latestLocationId = latestLocationId;
-    }
-
-    /**
-     * Name of the prison where the prisoner resides (if in prison).
-     */
-    @ApiModelProperty(value = "Name of the prison where the prisoner resides (if in prison).")
-    @JsonProperty("latestLocation")
-    public String getLatestLocation() {
-        return latestLocation;
-    }
-
-    public void setLatestLocation(final String latestLocation) {
-        this.latestLocation = latestLocation;
-    }
-
-    /**
-     * Name of the location where the prisoner resides (if in prison)
-     */
-    @ApiModelProperty(value = "Name of the location where the prisoner resides (if in prison)")
-    @JsonProperty("internalLocation")
-    public String getInternalLocation() {
-        return internalLocation;
-    }
-
-    public void setInternalLocation(final String internalLocation) {
-        this.internalLocation = internalLocation;
-    }
-
-    /**
-     * The prisoner's PNC (Police National Computer) number.
-     */
-    @ApiModelProperty(value = "The prisoner's PNC (Police National Computer) number.")
-    @JsonProperty("pncNumber")
-    public String getPncNumber() {
-        return pncNumber;
-    }
-
-    public void setPncNumber(final String pncNumber) {
-        this.pncNumber = pncNumber;
-    }
-
-    /**
-     * The prisoner's CRO (Criminal Records Office) number.
-     */
-    @ApiModelProperty(value = "The prisoner's CRO (Criminal Records Office) number.")
-    @JsonProperty("croNumber")
-    public String getCroNumber() {
-        return croNumber;
-    }
-
-    public void setCroNumber(final String croNumber) {
-        this.croNumber = croNumber;
-    }
-
-    /**
-     * The prisoner's ethnicity.
-     */
-    @ApiModelProperty(value = "The prisoner's ethnicity.")
-    @JsonProperty("ethnicity")
-    public String getEthnicity() {
-        return ethnicity;
-    }
-
-    public void setEthnicity(final String ethnicity) {
-        this.ethnicity = ethnicity;
-    }
-
-    /**
-     * The prisoner's country of birth.
-     */
-    @ApiModelProperty(value = "The prisoner's country of birth.")
-    @JsonProperty("birthCountry")
-    public String getBirthCountry() {
-        return birthCountry;
-    }
-
-    public void setBirthCountry(final String birthCountry) {
-        this.birthCountry = birthCountry;
-    }
-
-    /**
-     * The prisoner's religion.
-     */
-    @ApiModelProperty(value = "The prisoner's religion.")
-    @JsonProperty("religion")
-    public String getReligion() {
-        return religion;
-    }
-
-    public void setReligion(final String religion) {
-        this.religion = religion;
-    }
-
-    /**
-     * Status code of prisoner's latest conviction.
-     */
-    @ApiModelProperty(value = "Status code of prisoner's latest conviction.")
-    @JsonProperty("convictedStatus")
-    public String getConvictedStatus() {
-        return convictedStatus;
-    }
-
-    public void setConvictedStatus(final String convictedStatus) {
-        this.convictedStatus = convictedStatus;
-    }
-
-    /**
-     * The prisoner's imprisonment status.
-     */
-    @ApiModelProperty(value = "The prisoner's imprisonment status.")
-    @JsonProperty("imprisonmentStatus")
-    public String getImprisonmentStatus() {
-        return imprisonmentStatus;
-    }
-
-    public void setImprisonmentStatus(final String imprisonmentStatus) {
-        this.imprisonmentStatus = imprisonmentStatus;
-    }
-
-    /**
-     * The prisoner's imprisonment status description.
-     */
-    @ApiModelProperty(value = "The prisoner's imprisonment status description.")
-    @JsonProperty("imprisonmentStatusDesc")
-    public String getImprisonmentStatusDesc() {
-        return imprisonmentStatusDesc;
-    }
-
-    public void setImprisonmentStatusDesc(final String imprisonmentStatusDesc) {
-        this.imprisonmentStatusDesc = imprisonmentStatusDesc;
-    }
-
-
-    /**
-     * Date prisoner was received into the prison.
-     */
-    @ApiModelProperty(value = "Date prisoner was received into the prison.")
-    @JsonProperty("receptionDate")
-    public LocalDate getReceptionDate() {
-        return receptionDate;
-    }
-
-    public void setReceptionDate(final LocalDate receptionDate) {
-        this.receptionDate = receptionDate;
-    }
-
-    /**
-     * The prisoner's marital status.
-     */
-    @ApiModelProperty(value = "The prisoner's marital status.")
-    @JsonProperty("maritalStatus")
-    public String getMaritalStatus() {
-        return maritalStatus;
-    }
-
-    public void setMaritalStatus(final String maritalStatus) {
-        this.maritalStatus = maritalStatus;
-    }
-
-    /**
-     * The prisoner's current working first name.
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's current working first name.")
-    @JsonProperty("currentWorkingFirstName")
-    public String getCurrentWorkingFirstName() {
-        return currentWorkingFirstName;
-    }
-
-    public void setCurrentWorkingFirstName(final String currentWorkingFirstName) {
-        this.currentWorkingFirstName = currentWorkingFirstName;
-    }
-
-    /**
-     * The prisoner's current working last name.
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's current working last name.")
-    @JsonProperty("currentWorkingLastName")
-    public String getCurrentWorkingLastName() {
-        return currentWorkingLastName;
-    }
-
-    public void setCurrentWorkingLastName(final String currentWorkingLastName) {
-        this.currentWorkingLastName = currentWorkingLastName;
-    }
-
-    /**
-     * The prisoner's current working date of birth (in YYYY-MM-DD format).
-     */
-    @ApiModelProperty(required = true, value = "The prisoner's current working date of birth (in YYYY-MM-DD format).")
-    @JsonProperty("currentWorkingBirthDate")
-    public LocalDate getCurrentWorkingBirthDate() {
-        return currentWorkingBirthDate;
-    }
-
-    public void setCurrentWorkingBirthDate(final LocalDate currentWorkingBirthDate) {
-        this.currentWorkingBirthDate = currentWorkingBirthDate;
-    }
-
     @Override
     public String toString() {
-        final var sb = new StringBuilder();
 
-        sb.append("class PrisonerDetail {\n");
-
-        sb.append("  offenderNo: ").append(offenderNo).append("\n");
-        sb.append("  title: ").append(title).append("\n");
-        sb.append("  suffix: ").append(suffix).append("\n");
-        sb.append("  firstName: ").append(firstName).append("\n");
-        sb.append("  middleNames: ").append(middleNames).append("\n");
-        sb.append("  lastName: ").append(lastName).append("\n");
-        sb.append("  dateOfBirth: ").append(dateOfBirth).append("\n");
-        sb.append("  gender: ").append(gender).append("\n");
-        sb.append("  nationalities: ").append(nationalities).append("\n");
-        sb.append("  currentlyInPrison: ").append(currentlyInPrison).append("\n");
-        sb.append("  latestBookingId: ").append(latestBookingId).append("\n");
-        sb.append("  latestLocationId: ").append(latestLocationId).append("\n");
-        sb.append("  latestLocation: ").append(latestLocation).append("\n");
-        sb.append("  internalLocation: ").append(internalLocation).append("\n");
-        sb.append("  pncNumber: ").append(pncNumber).append("\n");
-        sb.append("  croNumber: ").append(croNumber).append("\n");
-        sb.append("  ethnicity: ").append(ethnicity).append("\n");
-        sb.append("  birthCountry: ").append(birthCountry).append("\n");
-        sb.append("  religion: ").append(religion).append("\n");
-        sb.append("  convictedStatus: ").append(convictedStatus).append("\n");
-        sb.append("  imprisonmentStatus: ").append(imprisonmentStatus).append("\n");
-        sb.append("  receptionDate: ").append(receptionDate).append("\n");
-        sb.append("  maritalStatus: ").append(maritalStatus).append("\n");
-        sb.append("  currentWorkingFirstName: ").append(currentWorkingFirstName).append("\n");
-        sb.append("  currentWorkingLastName: ").append(currentWorkingLastName).append("\n");
-        sb.append("  currentWorkingBirthDate: ").append(currentWorkingBirthDate).append("\n");
-        sb.append("}\n");
-
-        return sb.toString();
+        return new StringBuilder()
+                .append("class PrisonerDetail {\n")
+                .append("  offenderNo: ").append(offenderNo).append("\n")
+                .append("  title: ").append(title).append("\n")
+                .append("  suffix: ").append(suffix).append("\n")
+                .append("  firstName: ").append(firstName).append("\n")
+                .append("  middleNames: ").append(middleNames).append("\n")
+                .append("  lastName: ").append(lastName).append("\n")
+                .append("  dateOfBirth: ").append(dateOfBirth).append("\n")
+                .append("  gender: ").append(gender).append("\n")
+                .append("  nationalities: ").append(nationalities).append("\n")
+                .append("  currentlyInPrison: ").append(currentlyInPrison).append("\n")
+                .append("  latestBookingId: ").append(latestBookingId).append("\n")
+                .append("  latestLocationId: ").append(latestLocationId).append("\n")
+                .append("  latestLocation: ").append(latestLocation).append("\n")
+                .append("  internalLocation: ").append(internalLocation).append("\n")
+                .append("  pncNumber: ").append(pncNumber).append("\n")
+                .append("  croNumber: ").append(croNumber).append("\n")
+                .append("  ethnicity: ").append(ethnicity).append("\n")
+                .append("  birthCountry: ").append(birthCountry).append("\n")
+                .append("  religion: ").append(religion).append("\n")
+                .append("  convictedStatus: ").append(convictedStatus).append("\n")
+                .append("  imprisonmentStatus: ").append(imprisonmentStatus).append("\n")
+                .append("  receptionDate: ").append(receptionDate).append("\n")
+                .append("  maritalStatus: ").append(maritalStatus).append("\n")
+                .append("  currentWorkingFirstName: ").append(currentWorkingFirstName).append("\n")
+                .append("  currentWorkingLastName: ").append(currentWorkingLastName).append("\n")
+                .append("  currentWorkingBirthDate: ").append(currentWorkingBirthDate).append("\n")
+                .append("}\n")
+                .toString();
     }
 }
