82c377f019bdbe6eb6e11d4bc1bed1cde7845056
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-04-03 17:35:32 +0100

    GDPR-78 Improving integration test to prevent regression of lazy loading bug fix

--- src/test/java/uk/gov/justice/hmpps/nomis/datacompliance/controller/DataComplianceControllerTest.java
+++ src/test/java/uk/gov/justice/hmpps/nomis/datacompliance/controller/DataComplianceControllerTest.java
@@ -3,27 +3,32 @@ package uk.gov.justice.hmpps.nomis.datacompliance.controller;
 import net.syscon.elite.api.model.PendingDeletionRequest;
 import net.syscon.elite.api.resource.impl.ResourceTest;
 import org.junit.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import uk.gov.justice.hmpps.nomis.datacompliance.service.OffenderDataComplianceService;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderPendingDeletionEvent;
+import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderPendingDeletionEvent.Booking;
+import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderPendingDeletionEvent.OffenderWithBookings;
+import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderPendingDeletionReferralCompleteEvent;
+import uk.gov.justice.hmpps.nomis.datacompliance.events.publishers.OffenderPendingDeletionEventPusher;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.concurrent.CompletableFuture;
 
 import static net.syscon.elite.executablespecification.steps.AuthTokenHelper.AuthToken.ELITE2_API_USER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.jetty.http.HttpStatus.ACCEPTED_202;
 import static org.eclipse.jetty.http.HttpStatus.BAD_REQUEST_400;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
 import static org.springframework.http.HttpMethod.POST;
 
 public class DataComplianceControllerTest extends ResourceTest {
 
     private static final String REQUEST_ID = "123";
-    private static final LocalDateTime WINDOW_START = LocalDateTime.now();
-    private static final LocalDateTime WINDOW_END = WINDOW_START.plusDays(1);
+    private static final LocalDateTime WINDOW_START = LocalDateTime.of(1970, 1, 1, 0, 0);
+    private static final LocalDateTime WINDOW_END = WINDOW_START.plusYears(100);
 
-    @MockBean
-    private OffenderDataComplianceService offenderDataComplianceService;
+    @SpyBean
+    private OffenderPendingDeletionEventPusher offenderPendingDeletionEventPusher;
 
     @Test
     public void requestOffenderPendingDeletions() {
@@ -35,12 +40,24 @@ public class DataComplianceControllerTest extends ResourceTest {
                         .dueForDeletionWindowEnd(WINDOW_END)
                         .build());
 
-        when(offenderDataComplianceService.acceptOffendersPendingDeletionRequest(REQUEST_ID, WINDOW_START, WINDOW_END))
-                .thenReturn(CompletableFuture.completedFuture(null));
-
         final var response = testRestTemplate.exchange("/api/data-compliance/offenders/pending-deletions", POST, requestEntity, Void.class);
 
         assertThat(response.getStatusCodeValue()).isEqualTo(ACCEPTED_202);
+
+        verify(offenderPendingDeletionEventPusher, timeout(5000)).sendPendingDeletionEvent(
+                OffenderPendingDeletionEvent.builder()
+                        .offenderIdDisplay("Z0020ZZ")
+                        .firstName("BURT")
+                        .lastName("REYNOLDS")
+                        .birthDate(LocalDate.of(1966, 1, 1))
+                        .offender(OffenderWithBookings.builder()
+                                .offenderId(-1020L)
+                                .booking(new Booking(-20L))
+                                .build())
+                        .build());
+
+        verify(offenderPendingDeletionEventPusher, timeout(5000))
+                .sendReferralCompleteEvent(new OffenderPendingDeletionReferralCompleteEvent(REQUEST_ID));
     }
 
     @Test
