7a0d6fbc591a64b28fd9c55218527fc9c9eb9f59
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-04-06 11:34:58 +0100

    GDPR-51 Adding deletion complete event publishing (#457)

--- src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/events/publishers/OffenderDeletionEventPusher.java
+++ src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/events/publishers/OffenderDeletionEventPusher.java
@@ -1,9 +1,11 @@
 package uk.gov.justice.hmpps.nomis.datacompliance.events.publishers;
 
+import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderDeletionCompleteEvent;
 import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderPendingDeletionEvent;
 import uk.gov.justice.hmpps.nomis.datacompliance.events.dto.OffenderPendingDeletionReferralCompleteEvent;
 
-public interface OffenderPendingDeletionEventPusher {
+public interface OffenderDeletionEventPusher {
     void sendPendingDeletionEvent(OffenderPendingDeletionEvent event);
     void sendReferralCompleteEvent(OffenderPendingDeletionReferralCompleteEvent event);
+    void sendDeletionCompleteEvent(OffenderDeletionCompleteEvent event);
 }
