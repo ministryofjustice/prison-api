89ec11a5940c954bb96faa772131bbab9c2f5bda
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2020-04-30 17:24:06 +0100

    DT-792 correcting swagger docs around date time formatting. (#494)

--- src/main/java/net/syscon/elite/api/model/CourtEvent.java
+++ src/main/java/net/syscon/elite/api/model/CourtEvent.java
@@ -21,7 +21,7 @@ public class CourtEvent {
     @ApiModelProperty(required = true, value = "Offender number (NOMS ID)", example = "G3878UK")
     private String offenderNo;
 
-    @ApiModelProperty(required = true, value = "Date and time the record was created", example = "2019-12-01T13:34:00.000Z")
+    @ApiModelProperty(required = true, value = "Date and time the record was created in Europe/London (ISO 8601) format without timezone offset e.g. YYYY-MM-DDTHH:MM:SS.", example = "2019-12-01T13:34:00")
     private LocalDateTime createDateTime;
 
     @ApiModelProperty(required = true, value = "The internal event ID", example = "12343434")
@@ -42,10 +42,10 @@ public class CourtEvent {
     @ApiModelProperty(required = true, value = "The date on which the event is scheduled to occur", example = "2019-12-01")
     private LocalDate eventDate;
 
-    @ApiModelProperty(required = true, value = "The planned date and time of the start of the event", example = "2019-12-01T14:00:00.000Z")
+    @ApiModelProperty(required = true, value = "The planned date and time of the start of the event in Europe/London (ISO 8601) format without timezone offset e.g. YYYY-MM-DDTHH:MM:SS.", example = "2019-12-01T14:00:00")
     private LocalDateTime startTime;
 
-    @ApiModelProperty(required = true, value = "The planned date and time of the end of the event", example = "2019-12-01T14:40:00.000Z")
+    @ApiModelProperty(required = true, value = "The planned date and time of the end of the event in Europe/London (ISO 8601) format without timezone offset e.g. YYYY-MM-DDTHH:MM:SS.", example = "2019-12-01T14:40:00")
     private LocalDateTime endTime;
 
     @ApiModelProperty(required = true, value = "The event class (from COURT_EVENTS)", example = "EXT_MOV")
