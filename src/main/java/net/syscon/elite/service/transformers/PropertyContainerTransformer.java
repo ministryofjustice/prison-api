485840e46dc8006b9f0cb25318c170d79503d4f5
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-05-01 08:07:22 +0100

    NN-2591: Update to use location transformer

--- src/main/java/net/syscon/elite/service/transformers/PropertyContainerTransformer.java
+++ src/main/java/net/syscon/elite/service/transformers/PropertyContainerTransformer.java
@@ -1,29 +1,21 @@
 package net.syscon.elite.service.transformers;
 
-import net.syscon.elite.api.model.CourtCase;
-import net.syscon.elite.api.model.CourtHearing;
-import net.syscon.elite.api.model.Location;
 import net.syscon.elite.api.model.PropertyContainer;
-import net.syscon.elite.repository.jpa.model.CaseStatus;
-import net.syscon.elite.repository.jpa.model.LegalCaseType;
-import net.syscon.elite.repository.jpa.model.OffenderCourtCase;
 import net.syscon.elite.repository.jpa.model.OffenderPropertyContainer;
+import net.syscon.elite.repository.jpa.transform.LocationTransformer;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Simple transform object to help reduce boiler plate mapping of entity {@link OffenderCourtCase} to model object {@link CourtCase}.
+ * Simple transform object to help reduce boiler plate mapping of entity {@link OffenderPropertyContainer} to model object {@link PropertyContainer}.
  */
 public class PropertyContainerTransformer {
 
     public static PropertyContainer transform(final OffenderPropertyContainer propertyContainer) {
         return PropertyContainer.builder()
-                .location(Location.builder()
-                        .locationId(propertyContainer.getInternalLocation().getLocationId())
-                        .description(propertyContainer.getInternalLocation().getDescription())
-                        .build())
+                .location(LocationTransformer.fromAgencyInternalLocation(propertyContainer.getInternalLocation()))
                 .sealMark(propertyContainer.getSealMark())
                 .build();
     }
