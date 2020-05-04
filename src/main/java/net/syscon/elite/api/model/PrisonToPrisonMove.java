ff4cf306b90d06e9caed93329b6609574cf5899d
Matt Crowson <matt.crowson@digital.justice.gov.uk>
2020-05-04 16:33:16 +0100

    DT-781 wiring in service layer to new endpoint for scheduling prison to prison moves.

--- src/main/java/net/syscon/elite/api/model/PrisonToPrisonMove.java
+++ src/main/java/net/syscon/elite/api/model/PrisonToPrisonMove.java
@@ -25,17 +25,17 @@ public class PrisonToPrisonMove {
 
     @ApiModelProperty(required = true, value = "The prison (agency code) to be moved from.", position = 1, example = "LEI")
     @NotBlank(message = "The from prison location must be provided.")
-    @Size(max = 6, message = "From location must be a maximum of 6 characters.")
-    private String fromPrison;
+    @Size(max = 6, message = "From prison must be a maximum of 6 characters.")
+    private String fromPrisonLocation;
 
     @ApiModelProperty(required = true, value = "The prison (agency code) to be moved to.", position = 2, example = "PVI")
     @NotBlank(message = "The to prison location must be provided.")
-    @Size(max = 6, message = "To location must be a maximum of 6 characters.")
-    private String toPrison;
+    @Size(max = 6, message = "To prison must be a maximum of 6 characters.")
+    private String toPrisonLocation;
 
     @ApiModelProperty(required = true, value = "The escort type of the move.", position = 3, example = "PECS")
     @NotBlank(message = "The escort type must be provided.")
-    @Size(max = 12, message = "To escort type must be a maximum of 12 characters.")
+    @Size(max = 12, message = "Escort type must be a maximum of 12 characters.")
     private String escortType;
 
     @ApiModelProperty(required = true, value = "The date and time of the move in Europe/London (ISO 8601) format without timezone offset e.g. YYYY-MM-DDTHH:MM:SS.", position = 4, example = "2020-02-28T14:40:00")
