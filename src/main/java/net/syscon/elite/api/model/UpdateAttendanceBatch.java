54f619a1b711dfba84a8ae998d5f0d1ddacd845f
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2020-05-19 08:02:52 +0100

    DCS-488 Upgrading dependencies (#523)
    * DCS-488 Upgrading dependencies
    
    * DCS-488 Upgrading gradle and removing pinning of spring security
    
    * DCS-488 Moving away from hibernate specific validation annotations
    
    * DCS-488 revert serentity version bump, move to all dist for gradle and remove explicit hibernate dependency
    
    * DCS-488 Adding in spring validation
    
    * DCS-488 Fix validation message

--- src/main/java/net/syscon/elite/api/model/UpdateAttendanceBatch.java
+++ src/main/java/net/syscon/elite/api/model/UpdateAttendanceBatch.java
@@ -3,10 +3,15 @@ package net.syscon.elite.api.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
-import org.hibernate.validator.constraints.Length;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @ApiModel(description = "Attendance details.  This is used to update the attendance details of multiple bookings")
@@ -20,16 +25,16 @@ import java.util.Set;
 public class UpdateAttendanceBatch {
 
     @ApiModelProperty(required = true, position = 0, value = "Attendance outcome, possible values are the codes in the 'PS_PA_OC' reference domain.", example = "ATT", allowableValues = "ABS,ACCAB,ATT,CANC,NREQ,SUS,UNACAB,REST")
-    @Length(max = 12)
+    @Size(max = 12)
     @NotBlank
     private String eventOutcome;
 
     @ApiModelProperty(value = "Possible values are the codes in the 'PERFORMANCE' reference domain, mandatory for eventOutcome 'ATT'.", position = 1, example = "ACCEPT", allowableValues = "ACCEPT,GOOD,POOR,STANDARD,UNACCEPT")
-    @Length(max = 12)
+    @Size(max = 12)
     private String performance;
 
     @ApiModelProperty(value = "Free text comment, maximum length 240 characters.", position = 2, example = "Turned up very late")
-    @Length(max = 240)
+    @Size(max = 240)
     private String outcomeComment;
 
     @ApiModelProperty(required = true, value = "set of booking and activity ids")
