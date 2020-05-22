9cf7945f627b721d6dc001d6e2d53e074ae40d3d
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-05-22 18:36:42 +0100

    GDPR-113 Providing ability to limit number of offender deletion referrals in a batch

--- src/main/java/net/syscon/elite/api/model/PendingDeletionRequest.java
+++ src/main/java/net/syscon/elite/api/model/PendingDeletionRequest.java
@@ -9,13 +9,11 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 import static java.time.LocalDate.EPOCH;
-import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 
 @Getter
 @Builder
@@ -26,19 +24,22 @@ import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 @ApiModel(description = "PendingDeletionRequest")
 public class PendingDeletionRequest {
 
-    @ApiModelProperty(value = "An ID uniquely identifying the deletion request batch")
+    @NotNull
+    @ApiModelProperty(value = "An ID uniquely identifying the deletion request batch", example = "123", required = true)
     @JsonProperty("batchId")
     private Long batchId;
 
     @Builder.Default
-    @DateTimeFormat(iso = DATE)
-    @ApiModelProperty(value = "The start of the deletion-due-date window (defaults to epoch)")
+    @ApiModelProperty(value = "The start of the deletion-due-date window in Europe/London (ISO 8601) format without timezone offset e.g. YYYY-MM-DDTHH:MM:SS (defaults to epoch)", example = "2020-02-28T14:40:00")
     @JsonProperty("dueForDeletionWindowStart")
     private LocalDateTime dueForDeletionWindowStart = EPOCH.atStartOfDay();
 
     @NotNull
-    @DateTimeFormat(iso = DATE)
-    @ApiModelProperty(value = "The end of the deletion-due-date window")
+    @ApiModelProperty(value = "The end of the deletion-due-date window in Europe/London (ISO 8601) format without timezone offset e.g. YYYY-MM-DDTHH:MM:SS", example = "2020-02-28T14:40:00", required = true)
     @JsonProperty("dueForDeletionWindowEnd")
     private LocalDateTime dueForDeletionWindowEnd;
+
+    @ApiModelProperty(value = "The limit of the number of offenders retrieved and referred for deletion", example = "10")
+    @JsonProperty("limit")
+    private Integer limit;
 }
--- src/test/java/uk/gov/justice/hmpps/nomis/datacompliance/repository/jpa/repository/OffenderPendingDeletionRepositoryTest.java
+++ src/test/java/uk/gov/justice/hmpps/nomis/datacompliance/repository/jpa/repository/OffenderPendingDeletionRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.model.OffenderPendingDeletion;
@@ -22,14 +24,30 @@ class OffenderPendingDeletionRepositoryTest {
 
     private static final LocalDate SENTENCE_END_DATE = LocalDate.of(2020, 3, 24);
     private static final LocalDate DELETION_DUE_DATE = SENTENCE_END_DATE.plusYears(7);
+    private static final PageRequest PAGE_REQUEST = PageRequest.of(0, 1);
 
     @Autowired
     private OffenderPendingDeletionRepository repository;
 
     @Test
-    void getOffendersDueForDeletion() {
+    void getOffendersDueForDeletionWithPaging() {
 
-        var offenders = repository.getOffendersDueForDeletionBetween(DELETION_DUE_DATE.minusDays(1), DELETION_DUE_DATE.plusDays(1));
+        var offenders = repository.getOffendersDueForDeletionBetween(
+                DELETION_DUE_DATE.minusDays(1),
+                DELETION_DUE_DATE.plusDays(1),
+                PAGE_REQUEST);
+
+        assertThat(offenders).hasSize(1);
+        assertThat(offenders).extracting(OffenderPendingDeletion::getOffenderNumber).containsOnly("Z0020ZZ");
+    }
+
+    @Test
+    void getOffendersDueForDeletionUnpaged() {
+
+        var offenders = repository.getOffendersDueForDeletionBetween(
+                DELETION_DUE_DATE.minusDays(1),
+                DELETION_DUE_DATE.plusDays(1),
+                Pageable.unpaged());
 
         assertThat(offenders).hasSize(1);
         assertThat(offenders).extracting(OffenderPendingDeletion::getOffenderNumber).containsOnly("Z0020ZZ");
@@ -38,7 +56,10 @@ class OffenderPendingDeletionRepositoryTest {
     @Test
     void getOffendersDueForDeletionUsesDatesInclusively() {
 
-        var offenders = repository.getOffendersDueForDeletionBetween(DELETION_DUE_DATE, DELETION_DUE_DATE);
+        var offenders = repository.getOffendersDueForDeletionBetween(
+                DELETION_DUE_DATE,
+                DELETION_DUE_DATE,
+                PAGE_REQUEST);
 
         assertThat(offenders).hasSize(1);
         assertThat(offenders).extracting(OffenderPendingDeletion::getOffenderNumber).containsOnly("Z0020ZZ");
@@ -46,11 +67,19 @@ class OffenderPendingDeletionRepositoryTest {
 
     @Test
     void getOffendersDueForDeletionReturnsEmpty() {
-        assertThat(repository.getOffendersDueForDeletionBetween(LocalDate.of(1970, 1, 1), LocalDate.of(1970, 1, 1))).isEmpty();
+        assertThat(repository.getOffendersDueForDeletionBetween(
+                LocalDate.of(1970, 1, 1),
+                LocalDate.of(1970, 1, 1),
+                PAGE_REQUEST))
+                .isEmpty();
     }
 
     @Test
     void getOffendersDueForDeletionReturnsEmptyForDatesWrongWayRound() {
-        assertThat(repository.getOffendersDueForDeletionBetween(DELETION_DUE_DATE.plusDays(1), DELETION_DUE_DATE.minusDays(1))).isEmpty();
+        assertThat(repository.getOffendersDueForDeletionBetween(
+                DELETION_DUE_DATE.plusDays(1),
+                DELETION_DUE_DATE.minusDays(1),
+                PAGE_REQUEST))
+                .isEmpty();
     }
 }
