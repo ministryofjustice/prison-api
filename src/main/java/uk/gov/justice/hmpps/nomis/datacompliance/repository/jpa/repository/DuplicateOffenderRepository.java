eada39745362f67b285263371d8e19beeb9ed2dd
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-05-28 14:31:47 +0100

    GDPR-110 Hooking up PNC duplicate search to service
    Also improving the search to account for leading
    zeros.

--- src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/repository/jpa/repository/DuplicateOffenderRepository.java
+++ src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/repository/jpa/repository/DuplicateOffenderRepository.java
@@ -2,14 +2,13 @@ package uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.repository;
 
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.model.DuplicateOffender;
 
 import java.util.List;
 
 @Repository
-public interface DuplicateOffenderRepository extends CrudRepository<DuplicateOffender, String> {
+public interface DuplicateOffenderRepository extends org.springframework.data.repository.Repository<DuplicateOffender, String> {
 
     /**
      * The following query finds offenders that share the same
@@ -18,12 +17,12 @@ public interface DuplicateOffenderRepository extends CrudRepository<DuplicateOff
      * the first part (year) of the number.
      */
     @Query(value =
-                    "SELECT O1.OFFENDER_ID_DISPLAY FROM OFFENDERS O1 " +
+                    "SELECT DISTINCT(O1.OFFENDER_ID_DISPLAY) FROM OFFENDERS O1 " +
                     "INNER JOIN OFFENDER_IDENTIFIERS OI1 " +
                     "ON O1.OFFENDER_ID = OI1.OFFENDER_ID " +
-                    "AND OI1.IDENTIFIER_TYPE = 'PNC'" +
-                    "AND REGEXP_SUBSTR(UPPER(OI1.IDENTIFIER), '[0-9]{2,2}/[0-9]+[A-Z]+') IN (" +
-                    "    SELECT REGEXP_SUBSTR(UPPER(OI2.IDENTIFIER), '[0-9]{2,2}/[0-9]+[A-Z]+') FROM OFFENDERS O2" +
+                    "AND OI1.IDENTIFIER_TYPE = 'PNC' " +
+                    "AND CONCAT(REGEXP_SUBSTR(TRIM(UPPER(OI1.IDENTIFIER)), '[0-9]{2,2}/'), REGEXP_SUBSTR(TRIM(UPPER(OI1.IDENTIFIER)), '[1-9][0-9]*[A-Z]+$')) IN (" +
+                    "    SELECT CONCAT(REGEXP_SUBSTR(TRIM(UPPER(OI2.IDENTIFIER)), '[0-9]{2,2}/'), REGEXP_SUBSTR(TRIM(UPPER(OI2.IDENTIFIER)), '[1-9][0-9]*[A-Z]+$')) FROM OFFENDERS O2" +
                     "    INNER JOIN OFFENDER_IDENTIFIERS OI2" +
                     "    ON O2.OFFENDER_ID = OI2.OFFENDER_ID" +
                     "    AND OI2.IDENTIFIER_TYPE = 'PNC'" +
--- src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/service/DataDuplicateService.java
+++ src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/service/DataDuplicateService.java
@@ -1,25 +1,54 @@
 package uk.gov.justice.hmpps.nomis.datacompliance.service;
 
+import com.google.common.collect.ImmutableSet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.hmpps.nomis.datacompliance.events.publishers.DataComplianceEventPusher;
 import uk.gov.justice.hmpps.nomis.datacompliance.events.publishers.dto.DataDuplicateResult;
+import uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.model.DuplicateOffender;
+import uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.repository.DuplicateOffenderRepository;
+
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DataDuplicateService {
 
+    private final DuplicateOffenderRepository duplicateOffenderRepository;
     private final DataComplianceEventPusher dataComplianceEventPusher;
 
     public void checkForDataDuplicates(final String offenderNo, final Long retentionCheckId) {
 
-        // TODO GDPR-110 Implement data duplicate check
+        final var duplicateOffenders = ImmutableSet.<String>builder()
+                .addAll(getOffendersWithMatchingPncNumber(offenderNo))
+                .build();
+
+        // TODO GDPR-110 duplicate checks including:
+        //  * CRO
+        //  * LIDS Number
+        //  * Personal data
 
         dataComplianceEventPusher.send(DataDuplicateResult.builder()
                 .offenderIdDisplay(offenderNo)
                 .retentionCheckId(retentionCheckId)
+                .duplicateOffenders(duplicateOffenders)
                 .build());
+    }
+
+    private Set<String> getOffendersWithMatchingPncNumber(final String offenderNo) {
+
+        final var duplicates = duplicateOffenderRepository.getOffendersWithMatchingPncNumber(offenderNo).stream()
+                .map(DuplicateOffender::getOffenderNumber)
+                .collect(toSet());
+
+        if (!duplicates.isEmpty()) {
+            log.info("Found offender(s) ({}) with matching PNCs for offender '{}'", duplicates, offenderNo);
+        }
+
+        return duplicates;
     }
 }
