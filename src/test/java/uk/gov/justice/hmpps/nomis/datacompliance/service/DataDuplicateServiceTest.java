284210323b7db691dd3534531333ba5cd044009f
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-05-29 16:53:26 +0100

    GDPR-110 Hooking up PNC duplicate search to service (#542)
    Also improving the search to account for leading
    zeros.

--- src/test/java/uk/gov/justice/hmpps/nomis/datacompliance/service/DataDuplicateServiceTest.java
+++ src/test/java/uk/gov/justice/hmpps/nomis/datacompliance/service/DataDuplicateServiceTest.java
@@ -7,11 +7,24 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.justice.hmpps.nomis.datacompliance.events.publishers.DataComplianceEventPusher;
 import uk.gov.justice.hmpps.nomis.datacompliance.events.publishers.dto.DataDuplicateResult;
+import uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.model.DuplicateOffender;
+import uk.gov.justice.hmpps.nomis.datacompliance.repository.jpa.repository.DuplicateOffenderRepository;
 
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DataDuplicateServiceTest {
+
+    private static final String OFFENDER_NO = "A1234AA";
+    private static final String DUPLICATE_OFFENDER_1 = "B1234BB";
+    private static final long RETENTION_CHECK_ID = 123;
+
+    @Mock
+    private DuplicateOffenderRepository duplicateOffenderRepository;
 
     @Mock
     private DataComplianceEventPusher dataComplianceEventPusher;
@@ -20,16 +33,34 @@ class DataDuplicateServiceTest {
 
     @BeforeEach
     void setUp() {
-        dataDuplicateService = new DataDuplicateService(dataComplianceEventPusher);
+        dataDuplicateService = new DataDuplicateService(duplicateOffenderRepository, dataComplianceEventPusher);
     }
 
     @Test
     void checkForDataDuplicates() {
-        dataDuplicateService.checkForDataDuplicates("A1234AA", 123L);
+
+        when(duplicateOffenderRepository.getOffendersWithMatchingPncNumber(OFFENDER_NO))
+                .thenReturn(List.of(new DuplicateOffender(DUPLICATE_OFFENDER_1)));
+
+        dataDuplicateService.checkForDataDuplicates(OFFENDER_NO, RETENTION_CHECK_ID);
 
         verify(dataComplianceEventPusher).send(DataDuplicateResult.builder()
-                .offenderIdDisplay("A1234AA")
-                .retentionCheckId(123L)
+                .offenderIdDisplay(OFFENDER_NO)
+                .retentionCheckId(RETENTION_CHECK_ID)
+                .duplicateOffender(DUPLICATE_OFFENDER_1)
+                .build());
+    }
+
+    @Test
+    void checkForDataDuplicatesReturnsEmpty() {
+
+        when(duplicateOffenderRepository.getOffendersWithMatchingPncNumber(OFFENDER_NO)).thenReturn(emptyList());
+
+        dataDuplicateService.checkForDataDuplicates(OFFENDER_NO, RETENTION_CHECK_ID);
+
+        verify(dataComplianceEventPusher).send(DataDuplicateResult.builder()
+                .offenderIdDisplay(OFFENDER_NO)
+                .retentionCheckId(RETENTION_CHECK_ID)
                 .build());
     }
 }
