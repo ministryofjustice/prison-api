427f493438c9e02e4158157c3108c096305d8083
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-06-04 08:49:04 +0100

    NN-2663: Add DPRRD, TERSED and effective sentence end date to sentence detail

--- src/main/java/net/syscon/elite/api/model/SentenceDetail.java
+++ src/main/java/net/syscon/elite/api/model/SentenceDetail.java
@@ -37,7 +37,9 @@ public class SentenceDetail extends BaseSentenceDetail {
     private LocalDate nonParoleOverrideDate;
     @ApiModelProperty(value = "PRRD (override) - post-recall release override date for offender.", position = 44, example = "2020-04-01")
     private LocalDate postRecallReleaseOverrideDate;
-    @ApiModelProperty(value = "Release date for non-DTO sentence (if applicable). This will be based on one of ARD, CRD, NPD or PRRD.", position = 45, example = "2020-04-01")
+    @ApiModelProperty(value = "DPRRD (override) - detention training order post-recall release override date for offender", position = 45, example = "2020-04-01")
+    private LocalDate dtoPostRecallReleaseDateOverride;
+    @ApiModelProperty(value = "Release date for non-DTO sentence (if applicable). This will be based on one of ARD, CRD, NPD or PRRD.", position = 46, example = "2020-04-01")
     private LocalDate nonDtoReleaseDate;
     @ApiModelProperty(value = "Indicates which type of non-DTO release date is the effective release date. One of 'ARD', 'CRD', 'NPD' or 'PRRD'.", position = 33, example = "CRD", allowableValues = "ARD,CRD,NPD,PRRD", required = true)
     private NonDtoReleaseDateType nonDtoReleaseDateType;
@@ -48,8 +50,8 @@ public class SentenceDetail extends BaseSentenceDetail {
     private LocalDate releaseDate;
 
     @Builder(builderMethodName = "sentenceDetailBuilder")
-    public SentenceDetail(final LocalDate sentenceExpiryDate, final LocalDate automaticReleaseDate, final LocalDate conditionalReleaseDate, final LocalDate nonParoleDate, final LocalDate postRecallReleaseDate, final LocalDate licenceExpiryDate, final LocalDate homeDetentionCurfewEligibilityDate, final LocalDate paroleEligibilityDate, final LocalDate homeDetentionCurfewActualDate, final LocalDate actualParoleDate, final LocalDate releaseOnTemporaryLicenceDate, final LocalDate earlyRemovalSchemeEligibilityDate, final LocalDate earlyTermDate, final LocalDate midTermDate, final LocalDate lateTermDate, final LocalDate topupSupervisionExpiryDate, final LocalDate tariffDate, @NotNull final Long bookingId, final LocalDate sentenceStartDate, final Integer additionalDaysAwarded, final LocalDate automaticReleaseOverrideDate, final LocalDate conditionalReleaseOverrideDate, final LocalDate nonParoleOverrideDate, final LocalDate postRecallReleaseOverrideDate, final LocalDate nonDtoReleaseDate, final NonDtoReleaseDateType nonDtoReleaseDateType, final LocalDate confirmedReleaseDate, final LocalDate releaseDate) {
-        super(sentenceExpiryDate, automaticReleaseDate, conditionalReleaseDate, nonParoleDate, postRecallReleaseDate, licenceExpiryDate, homeDetentionCurfewEligibilityDate, paroleEligibilityDate, homeDetentionCurfewActualDate, actualParoleDate, releaseOnTemporaryLicenceDate, earlyRemovalSchemeEligibilityDate, earlyTermDate, midTermDate, lateTermDate, topupSupervisionExpiryDate, tariffDate);
+    public SentenceDetail(final LocalDate sentenceExpiryDate, final LocalDate automaticReleaseDate, final LocalDate conditionalReleaseDate, final LocalDate nonParoleDate, final LocalDate postRecallReleaseDate, final LocalDate licenceExpiryDate, final LocalDate homeDetentionCurfewEligibilityDate, final LocalDate paroleEligibilityDate, final LocalDate homeDetentionCurfewActualDate, final LocalDate actualParoleDate, final LocalDate releaseOnTemporaryLicenceDate, final LocalDate earlyRemovalSchemeEligibilityDate, final LocalDate earlyTermDate, final LocalDate midTermDate, final LocalDate lateTermDate, final LocalDate dtoPostRecallReleaseDate, final LocalDate tariffEarlyRemovalSchemeEligibilityDate, final LocalDate effectiveSentenceEndDate, final LocalDate topupSupervisionExpiryDate, final LocalDate tariffDate, @NotNull final Long bookingId, final LocalDate sentenceStartDate, final Integer additionalDaysAwarded, final LocalDate automaticReleaseOverrideDate, final LocalDate conditionalReleaseOverrideDate, final LocalDate nonParoleOverrideDate, final LocalDate postRecallReleaseOverrideDate, final LocalDate dtoPostRecallReleaseDateOverride, final LocalDate nonDtoReleaseDate, final NonDtoReleaseDateType nonDtoReleaseDateType, final LocalDate confirmedReleaseDate, final LocalDate releaseDate) {
+        super(sentenceExpiryDate, automaticReleaseDate, conditionalReleaseDate, nonParoleDate, postRecallReleaseDate, licenceExpiryDate, homeDetentionCurfewEligibilityDate, paroleEligibilityDate, homeDetentionCurfewActualDate, actualParoleDate, releaseOnTemporaryLicenceDate, earlyRemovalSchemeEligibilityDate, earlyTermDate, midTermDate, lateTermDate, topupSupervisionExpiryDate, tariffDate, dtoPostRecallReleaseDate, tariffEarlyRemovalSchemeEligibilityDate, effectiveSentenceEndDate);
         this.bookingId = bookingId;
         this.sentenceStartDate = sentenceStartDate;
         this.additionalDaysAwarded = additionalDaysAwarded;
@@ -57,6 +59,7 @@ public class SentenceDetail extends BaseSentenceDetail {
         this.conditionalReleaseOverrideDate = conditionalReleaseOverrideDate;
         this.nonParoleOverrideDate = nonParoleOverrideDate;
         this.postRecallReleaseOverrideDate = postRecallReleaseOverrideDate;
+        this.dtoPostRecallReleaseDateOverride = dtoPostRecallReleaseDateOverride;
         this.nonDtoReleaseDate = nonDtoReleaseDate;
         this.nonDtoReleaseDateType = nonDtoReleaseDateType;
         this.confirmedReleaseDate = confirmedReleaseDate;
