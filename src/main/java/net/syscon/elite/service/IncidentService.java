185b926d5729158f22eb685e82870ed4aa4bf81d
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-06-05 07:56:22 +0100

    DT-892 Give booking id and seq an optional wrapped object to make sure null is handled. (#561)
    * DT-892 Give booking id and seq an optional wrapped object to make sure null is handled.
    
    * DT-892 Give booking id and seq an optional wrapped object to make sure null is handled.

--- src/main/java/net/syscon/elite/service/IncidentService.java
+++ src/main/java/net/syscon/elite/service/IncidentService.java
@@ -38,10 +38,7 @@ public class IncidentService {
 
     @PreAuthorize("hasAnyRole('SYSTEM_READ_ONLY','SYSTEM_USER')")
     public List<IncidentCase> getIncidentCasesByOffenderNo(@NotNull final String offenderNo, final List<String> incidentTypes, final List<String> participationRoles) {
-        final var identifiers = bookingService.getOffenderIdentifiers(offenderNo);
-        if (identifiers.getBookingId() == null) {
-            throw EntityNotFoundException.withMessage("No booking found for offender {}", offenderNo);
-        }
+        bookingService.getOffenderIdentifiers(offenderNo);
         return repository.getIncidentCasesByOffenderNo(offenderNo, incidentTypes, participationRoles);
     }
 
