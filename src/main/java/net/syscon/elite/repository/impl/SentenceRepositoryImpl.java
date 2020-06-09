f9aabe10e90aecd26c860ec9314d4d2ea854a31a
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2020-06-09 10:58:40 +0100

    PAT-933 Add outcome and court date information to OffenceHistoryDetail New request parameter to allow offences that did not result in a conviction to be returned

--- src/main/java/net/syscon/elite/repository/impl/SentenceRepositoryImpl.java
+++ src/main/java/net/syscon/elite/repository/impl/SentenceRepositoryImpl.java
@@ -9,7 +9,11 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Repository
 public class SentenceRepositoryImpl extends RepositoryBase implements SentenceRepository {
