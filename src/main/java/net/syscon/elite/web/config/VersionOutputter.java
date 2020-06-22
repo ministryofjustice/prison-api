d21158d38d249afed5d7cfa47f60fc146fe19ea3
JamieDraperUK <draper65@googlemail.com>
2020-06-22 09:25:00 +0100

    NN-2459 add VersionOutputter back in

--- src/main/java/net/syscon/elite/web/config/VersionOutputter.java
+++ src/main/java/net/syscon/elite/web/config/VersionOutputter.java
@@ -1,20 +1,29 @@
 package net.syscon.elite.web.config;
 
 
-//@Slf4j
-//@Configuration
-//@AllArgsConstructor
-//public class VersionOutputter {
-//
-//    private final BuildProperties buildProperties;
-//
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void logVersionOnStartup() {
-//        log.info("Version {} started", buildProperties.getVersion());
-//    }
-//
-//    @Bean
-//    public ContextInitializer versionContextInitializer() {
-//        return telemetryContext -> telemetryContext.getComponent().setVersion(buildProperties.getVersion());
-//    }
-//}
+import com.microsoft.applicationinsights.extensibility.ContextInitializer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@Configuration
+@AllArgsConstructor
+public class VersionOutputter {
+
+    private final BuildProperties buildProperties;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logVersionOnStartup() {
+        log.info("Version {} started", buildProperties.getVersion());
+    }
+
+    @Bean
+    public ContextInitializer versionContextInitializer() {
+        return telemetryContext -> telemetryContext.getComponent().setVersion(buildProperties.getVersion());
+    }
+}
