013e1b1141fbdabe5145456519e7ced8a3e4d5ba
Nevelina Aleksandrova <nevelina@diginaga.online>
2020-06-22 09:04:47 +0100

    NN-2415: Fix imports

--- src/main/java/net/syscon/elite/api/model/Visit.java
+++ src/main/java/net/syscon/elite/api/model/Visit.java
@@ -1,9 +1,14 @@
 package net.syscon.elite.api.model;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
--- src/main/java/net/syscon/elite/api/model/VisitWithVisitors.java
+++ src/main/java/net/syscon/elite/api/model/VisitWithVisitors.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
