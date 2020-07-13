3a0651b47544ee6a92207cdb1b916165e0b07838
Michael Willis <michael.willis@digital.justice.gov.uk>
2020-07-13 19:44:00 +0100

    DT-1014 Revert elite2api redirect (#648)

--- src/main/java/uk/gov/justice/hmpps/prison/web/config/ResourceServerConfiguration.java
+++ src/main/java/uk/gov/justice/hmpps/prison/web/config/ResourceServerConfiguration.java
@@ -28,7 +28,7 @@ public class ResourceServerConfiguration extends WebSecurityConfigurerAdapter {
                 // Can't have CSRF protection as requires session
                 .and().csrf().disable()
                 .authorizeRequests(auth ->
-                    auth.antMatchers("/webjars/**", "/favicon.ico", "/csrf", "/elite2api/**",
+                    auth.antMatchers("/webjars/**", "/favicon.ico", "/csrf",
                         "/health/**", "/info", "/ping", "/h2-console/**",
                         "/v2/api-docs", "/api/swagger.json",
                         "/swagger-ui.html", "/swagger-resources", "/swagger-resources/configuration/ui",
