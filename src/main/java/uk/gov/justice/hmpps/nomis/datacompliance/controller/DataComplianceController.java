4331feb690b23fffa70e51feea1db50ca5ef84e3
Jon Brighton <jonathan.brighton@digital.justice.gov.uk>
2020-07-31 15:37:51 +0100

    GDPR-199 Adding API documentation

--- src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/controller/DataComplianceController.java
+++ src/main/java/uk/gov/justice/hmpps/nomis/datacompliance/controller/DataComplianceController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -37,11 +38,11 @@ public class DataComplianceController {
     private final OffenderImageUpdateService offenderImageUpdateService;
 
     @GetMapping("/offenders-with-images")
-    @ApiOperation(value = "Get offenders with images captured in provided range")
+    @PreAuthorize("hasRole('SYSTEM_USER')")
+    @ApiOperation(value = "Get offenders with images captured in provided range", authorizations = { @Authorization("SYSTEM_USER") } )
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Invalid request.", response = ErrorResponse.class, responseContainer = "List"),
             @ApiResponse(code = 500, message = "Unrecoverable error occurred whilst processing request.", response = ErrorResponse.class, responseContainer = "List")})
-    @PreAuthorize("hasRole('SYSTEM_USER')")
     public Page<OffenderNumber> getOffendersWithImagesCapturedInRange(
             @ApiParam(value = "fromDateTime", required = true) @DateTimeFormat(iso = DATE_TIME) @RequestParam("fromDateTime") final LocalDateTime fromDate,
             @PageableDefault(direction = ASC, sort = "offender_id_display") final Pageable pageable) {
