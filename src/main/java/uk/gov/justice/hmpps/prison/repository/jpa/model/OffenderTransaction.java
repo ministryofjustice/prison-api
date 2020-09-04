179bcab54df625e611a77064769118610becac63
Peter Phillips <peter@greenthistle.co.uk>
2020-09-04 11:45:05 +0100

    DT-1023: ✨⬆️ Add in offender transaction and sequence

---
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/OffenderTransaction.java
@@ -0,0 +1,43 @@
+package uk.gov.justice.hmpps.prison.repository.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@IdClass(OffenderTransaction.Pk.class)
+@Table(name = "OFFENDER_TRANSACTIONS")
+public class OffenderTransaction {
+
+    @Id
+    @Column(name = "TXN_ID", nullable = false, insertable = false, updatable = false)
+    private Long transactionId;
+
+    @Id
+    @Column(name = "TXN_ENTRY_SEQ", nullable = false, insertable = false, updatable = false)
+    private Long transactionEntrySequence;
+
+    @Column(name = "CASELOAD_ID", nullable = false)
+    private String prisonId;
+
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Pk implements Serializable {
+        private Long transactionId;
+        private Long transactionEntrySequence;
+    }
+}
