b959e3b72993efee48f01bd8f77fe90eb81c17c5
petergphillips <peter@greenthistle.co.uk>
2020-09-04 12:14:08 +0100

    DT-1023: ✨⬆️ Add in offender transaction and sequence (#707)

---
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderTransactionRepository.java
@@ -0,0 +1,12 @@
+package uk.gov.justice.hmpps.prison.repository.jpa.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderTransaction;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderTransaction.Pk;
+
+public interface OffenderTransactionRepository extends CrudRepository<OffenderTransaction, Pk> {
+
+    @Query(value = "SELECT TXN_ID.nextval FROM dual", nativeQuery = true)
+    Long getNextTransactionId();
+}
