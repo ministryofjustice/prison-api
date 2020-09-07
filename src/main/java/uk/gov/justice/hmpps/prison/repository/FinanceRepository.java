2d5dbf80dd27501916bfd0f6d6683e552952f513
Peter Phillips <peter@greenthistle.co.uk>
2020-09-07 14:40:49 +0100

    DT-1023: 🎨 Improvement after code review

--- src/main/java/uk/gov/justice/hmpps/prison/repository/FinanceRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/FinanceRepository.java
@@ -1,6 +1,5 @@
 package uk.gov.justice.hmpps.prison.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import uk.gov.justice.hmpps.prison.api.model.Account;
 
 import java.math.BigDecimal;
@@ -8,14 +7,14 @@ import java.util.Date;
 
 public interface FinanceRepository {
 
-    Account getBalances(long bookingId, final String agencyId);
+    Account getBalances(long bookingId, String agencyId);
 
-    void insertIntoOffenderTrans(final String prisonId, final long offId, final long offBookId,
-                                 final String subActType, final long transPostType, final long transNumber,
-                                 final long transSeq, final BigDecimal transAmount, final String transDesc,
-                                 final Date transDate);
+    void insertIntoOffenderTrans(String prisonId, long offId, long offBookId,
+                                 String subActType, long transPostType, long transNumber,
+                                 long transSeq, BigDecimal transAmount, String transDesc,
+                                 Date transDate);
 
-    void processGlTransNew(final String prisonId, final long offId, final long offBookId, final Object subActTypeDr,
-                           final long subActTypeCr, final long transNumber, final long transSeq,
-                           final BigDecimal transAmount, final String transDesc, final Date transDate);
+    void processGlTransNew(String prisonId, long offId, long offBookId, Object subActTypeDr,
+                           long subActTypeCr, long transNumber, long transSeq,
+                           BigDecimal transAmount, String transDesc, Date transDate);
 }
