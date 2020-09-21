4012cec189731dca4abea011b18732f03ec4ca85
= <=>
2020-09-21 14:09:34 +0100

    NN-2896 fix flaky tests

--- src/main/java/uk/gov/justice/hmpps/prison/web/config/VersionOutputter.java
+++ src/main/java/uk/gov/justice/hmpps/prison/web/config/VersionOutputter.java
@@ -10,21 +10,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
-//
-//@Slf4j
-//@Configuration
-//@AllArgsConstructor
-//public class VersionOutputter {
-//
-//    private final BuildProperties buildProperties;
-//
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void logVersionOnStartup() {
-//        log.info("Version {} started", buildProperties.getVersion());
-//    }
-//
-//    @Bean
-//    public ContextInitializer versionContextInitializer() {
-//        return telemetryContext -> telemetryContext.getComponent().setVersion(buildProperties.getVersion());
-//    }
-//}
+
+@Slf4j
+@Configuration
+@AllArgsConstructor
+public class VersionOutputter {
+
+    private final BuildProperties buildProperties;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logVersionOnStartup() {
+        log.info("Version {} started", buildProperties.getVersion());
+    }
+
+    @Bean
+    public ContextInitializer versionContextInitializer() {
+        return telemetryContext -> telemetryContext.getComponent().setVersion(buildProperties.getVersion());
+    }
+}
