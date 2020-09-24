559149e7728aaa1c0ac266de5b922bad01146550
petergphillips <peter@greenthistle.co.uk>
2020-09-24 13:01:35 +0100

    DT-1155: ✨ Add in treatment description to reasonable adjustments (#733)

--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/BookingResourceImplIntTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/BookingResourceImplIntTest.java
@@ -145,8 +145,8 @@ public class BookingResourceImplIntTest extends ResourceTest {
         final var treatmentCodes = List.of("WHEELCHR_ACC", "PEEP");
         when(inmateRepository.findReasonableAdjustments(bookingId, treatmentCodes)).thenReturn(
                 List.of(
-                        new ReasonableAdjustment("WHEELCHR_ACC", "abcd", LocalDate.of(2010, 6, 21), null, "LEI"),
-                        new ReasonableAdjustment("PEEP", "efgh", LocalDate.of(2010, 6, 21), null, "LEI"))
+                        new ReasonableAdjustment("WHEELCHR_ACC", "abcd", LocalDate.of(2010, 6, 21), null, "LEI", "Wheelchair accessibility"),
+                        new ReasonableAdjustment("PEEP", "efgh", LocalDate.of(2010, 6, 21), null, "LEI", "Some other description"))
         );
 
         final var requestEntity = createHttpEntityWithBearerAuthorisation("ITAG_USER", List.of(), Map.of());
