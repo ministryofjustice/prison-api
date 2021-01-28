82219c6dbeb828a1141e0bd942518acdf660d1d8
Peter Phillips <peter@greenthistle.co.uk>
2021-01-28 12:29:29 +0000

    DT-1557: ⬆️ Application insights v3

--- src/main/java/uk/gov/justice/hmpps/prison/web/config/VersionOutputter.java
+++ src/main/java/uk/gov/justice/hmpps/prison/web/config/VersionOutputter.java
@@ -22,9 +22,4 @@ public class VersionOutputter {
     public void logVersionOnStartup() {
         log.info("Version {} started", buildProperties.getVersion());
     }
-
-    @Bean
-    public ContextInitializer versionContextInitializer() {
-        return telemetryContext -> telemetryContext.getComponent().setVersion(buildProperties.getVersion());
-    }
 }
