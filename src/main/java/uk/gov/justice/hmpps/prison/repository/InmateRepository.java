edd77ecabe66b238a3ba52856154908d21ef8995
petergphillips <peter@greenthistle.co.uk>
2021-02-05 15:44:57 +0000

    Revert "CAT-677: Use second latest assessment if latest is category U (#825)" (#840)
    This reverts commit 0b10d1c0245f01bbf6d3403cb1691ca5c87683e1.

--- src/main/java/uk/gov/justice/hmpps/prison/repository/InmateRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/InmateRepository.java
@@ -576,7 +576,8 @@ public class InmateRepository extends RepositoryBase {
                 OFFENDER_CATEGORY_MAPPER);
 
         return applyCutoffDateForActiveCategorisations(
-                removeNonStandardCategoryRecords(removeEarlierCategorisations(removeInvalidLatestCategorisations(rawData))), cutoffDate);
+                removeNonStandardCategoryRecords(removeEarlierCategorisations(rawData)),
+                cutoffDate);
     }
 
 
@@ -616,11 +617,6 @@ public class InmateRepository extends RepositoryBase {
         return bookingIdMap.values().stream()
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
-    }
-
-    private List<OffenderCategorise> removeInvalidLatestCategorisations(final List<OffenderCategorise> catList) {
-        final var invalidCategoryCodes = Set.of("U");
-        return catList.stream().filter(cat -> cat.getCategory() != null && !invalidCategoryCodes.contains(cat.getCategory())).collect(Collectors.toList());
     }
 
     private List<OffenderCategorise> cleanDuplicateRecordsUsingAssessmentSeq(final List<OffenderCategorise> individualCatList) {
