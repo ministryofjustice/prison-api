41aaf90be22d6338c3997d60d0343e4d8465fd1d
Jamie Draper <draper65@googlemail.com>
2021-02-08 15:09:17 +0000

    Revert "Revert "NN-3102: cellsWithCapacity returns cells with zero operational capacity (WIP) (#833)" (#841)"
    This reverts commit 299f9ad83480abb60a267ef8a3e321481b8a0ea6.

--- src/main/java/uk/gov/justice/hmpps/prison/service/MovementUpdateService.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/MovementUpdateService.java
@@ -58,7 +58,7 @@ public class MovementUpdateService {
         if (offenderBooking.getAssignedLivingUnitId().equals(internalLocation.getLocationId()))
             return transformToCellSwapResult(offenderBooking);
 
-        if (!internalLocation.isActiveCellWithSpace())
+        if (!internalLocation.isActiveCellWithSpace(false))
             throw new IllegalArgumentException(String.format("Location %s is either not a cell, active or is at maximum capacity", internalLocation.getDescription()));
 
         return saveAndReturnCellMoveResult(bookingId, reasonCode, movementDateTime, internalLocation);
