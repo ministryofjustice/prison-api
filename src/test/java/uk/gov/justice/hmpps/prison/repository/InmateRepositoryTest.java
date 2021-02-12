6d760881220806b1942a6117fcc2c069253e2f43
sp-watson <steve.watson@digital,justice.gov.uk>
2021-02-12 09:48:07 +0000

    NN-3161: Fix new SQL used for getOffendersAtLocation

--- src/test/java/uk/gov/justice/hmpps/prison/repository/InmateRepositoryTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/repository/InmateRepositoryTest.java
@@ -1328,7 +1328,7 @@ public class InmateRepositoryTest {
     }
 
     @Test
-    public void testSearchForInmatesByLocation() {
+    public void testSearchForInmatesByWingLocation() {
         final var expectedInfo = List.of(
             OffenderBooking.builder()
                 .bookingId(-40L)
@@ -1342,7 +1342,28 @@ public class InmateRepositoryTest {
                 .assignedLivingUnitId(-204L)
                 .build());
 
-        final var results = repository.findInmatesByLocation(-200L, "CELL", "SYI", null, "lastName,firstName,offenderNo", Order.DESC, 0, 10);
+        final var results = repository.findInmatesByLocation(-200L, "WING", "SYI", null, "lastName,firstName,offenderNo", Order.DESC, 0, 10);
+
+        assertThat(results.getItems()).hasSize(1);
+        assertThat(results.getItems().get(0)).isEqualTo(expectedInfo.get(0));
+    }
+
+    @Test
+    public void testSearchForInmatesByCellLocation() {
+        final var expectedInfo = List.of(
+            OffenderBooking.builder()
+                .bookingId(-40L)
+                .bookingNo("SAME_NO")
+                .offenderNo("A1184JR")
+                .firstName("JOE")
+                .lastName("ROOT")
+                .dateOfBirth(LocalDate.of(1990, Month.DECEMBER, 30))
+                .age(30)
+                .agencyId("SYI")
+                .assignedLivingUnitId(-204L)
+                .build());
+
+        final var results = repository.findInmatesByLocation(-204L, "CELL", "SYI", null, "lastName,firstName,offenderNo", Order.DESC, 0, 10);
 
         assertThat(results.getItems()).hasSize(1);
         assertThat(results.getItems().get(0)).isEqualTo(expectedInfo.get(0));
