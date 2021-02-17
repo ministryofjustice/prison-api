2ef3d5bc5d09cdd11357697627a1e3f25c7c56df
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2021-02-17 09:49:08 +0000

    CAT-677: Use second latest assessment for re-categorisations when latest is category U (#852)

--- src/main/java/uk/gov/justice/hmpps/prison/repository/InmateRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/InmateRepository.java
@@ -571,13 +571,11 @@ public class InmateRepository extends RepositoryBase {
         final var rawData = jdbcTemplate.query(
                 InmateRepositorySql.GET_RECATEGORISE.getSql(),
                 createParams("agencyId", agencyId,
-                        "assessStatus", Set.of("A", "P"),
                         "assessmentId", getCategoryAssessmentTypeId()),
                 OFFENDER_CATEGORY_MAPPER);
 
         return applyCutoffDateForActiveCategorisations(
-                removeNonStandardCategoryRecords(removeEarlierCategorisations(rawData)),
-                cutoffDate);
+                removeNonStandardCategoryRecords(removeEarlierCategorisations(removeInvalidLatestCategorisations(rawData))), cutoffDate);
     }
 
 
@@ -617,6 +615,11 @@ public class InmateRepository extends RepositoryBase {
         return bookingIdMap.values().stream()
                 .flatMap(List::stream)
                 .collect(Collectors.toList());
+    }
+
+    private List<OffenderCategorise> removeInvalidLatestCategorisations(final List<OffenderCategorise> catList) {
+        final var invalidCategoryCodes = Set.of("U");
+        return catList.stream().filter(cat -> cat.getCategory() != null && !invalidCategoryCodes.contains(cat.getCategory())).collect(Collectors.toList());
     }
 
     private List<OffenderCategorise> cleanDuplicateRecordsUsingAssessmentSeq(final List<OffenderCategorise> individualCatList) {
