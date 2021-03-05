1a7c7e02e32eb0c9dfe40db133edad0fde70c47e
sp-watson <steve.watson@digital,justice.gov.uk>
2021-03-05 11:35:07 +0000

    NN-3202: Assessment parent is ManyToOne association

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/AssessmentEntry.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/AssessmentEntry.java
@@ -1,21 +1,11 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.model;
 
 import lombok.*;
-import lombok.Builder.Default;
-import org.hibernate.annotations.*;
 
 import javax.persistence.Entity;
 import javax.persistence.*;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.time.LocalDate;
-
-import static org.hibernate.annotations.NotFoundAction.IGNORE;
-import static uk.gov.justice.hmpps.prison.repository.jpa.model.DisciplinaryAction.MLTY_DISCP;
-import static uk.gov.justice.hmpps.prison.repository.jpa.model.MilitaryBranch.MLTY_BRANCH;
-import static uk.gov.justice.hmpps.prison.repository.jpa.model.MilitaryDischarge.MLTY_DSCHRG;
-import static uk.gov.justice.hmpps.prison.repository.jpa.model.MilitaryRank.MLTY_RANK;
-import static uk.gov.justice.hmpps.prison.repository.jpa.model.WarZone.MLTY_WZONE;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
@@ -31,7 +21,7 @@ public class AssessmentEntry extends AuditableEntity implements Serializable {
     @Column(name = "ASSESSMENT_ID")
     private Long assessmentId;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="PARENT_ASSESSMENT_ID")
     private AssessmentEntry parentAssessment;
 
