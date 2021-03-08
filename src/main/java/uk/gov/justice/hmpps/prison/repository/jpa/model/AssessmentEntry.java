12064625480a7d0d2a253e6a4c39b4d550bacf21
sp-watson <steve.watson@digital,justice.gov.uk>
2021-03-08 11:58:14 +0000

    NN-3202: Removing wildcard imports

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/AssessmentEntry.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/AssessmentEntry.java
@@ -1,9 +1,18 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.*;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.io.Serializable;
 
