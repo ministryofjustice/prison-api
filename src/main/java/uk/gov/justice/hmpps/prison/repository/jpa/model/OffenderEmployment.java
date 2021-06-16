8619a5c445468c2bdb78d921e9f91cb60d203b0e
akeemfaulkner <contact@ake3m.com>
2021-06-16 16:37:31 +0100

    creates OffenderEmploymentAddress entity and updates composite keys OffenderEmployment to use @EmbeddedId

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/OffenderEmployment.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/OffenderEmployment.java
@@ -1,27 +1,33 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 import uk.gov.justice.hmpps.prison.repository.converter.YesNoToBooleanConverter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Convert;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "OFFENDER_EMPLOYMENTS")
 public class OffenderEmployment {
 
-    @Id
-    @Column(name = "OFFENDER_BOOK_ID")
-    private Long bookingId;
-
-    @Id
-    @Column(name = "EMPLOY_SEQ")
-    private Long employSeq;
+    @EmbeddedId
+    private PK id;
 
     @Column(name = "EMPLOYMENT_DATE")
     private LocalDate employmentDate;
@@ -113,5 +119,21 @@ public class OffenderEmployment {
 
     @Column(name = "CERTIFICATION")
     private String certification;
+
+    @Where(clause = "OWNER_CLASS = '" + OffenderEmploymentAddress.ADDR_TYPE + "'")
+    @OneToMany(mappedBy = "employment", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OffenderEmploymentAddress> addresses = new ArrayList<>();
+
+    @Embeddable
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PK implements Serializable {
+        @Column(name = "OFFENDER_BOOK_ID")
+        private Long bookingId;
+
+        @Column(name = "EMPLOY_SEQ")
+        private Long employSeq;
+    }
 }
 
---
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/OffenderEmploymentAddress.java
@@ -0,0 +1,33 @@
+package uk.gov.justice.hmpps.prison.repository.jpa.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+
+@Data
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = "employment", callSuper = true)
+@ToString(of = {"employment"}, callSuper = true)
+@DiscriminatorValue(OffenderAddress.ADDR_TYPE)
+public class OffenderEmploymentAddress extends Address {
+
+    static final String ADDR_TYPE = "OFF_EMP";
+
+    @JoinColumns({
+        @JoinColumn(name = "OWNER_ID", referencedColumnName = "OFFENDER_BOOK_ID"),
+        @JoinColumn(name = "OWNER_SEQ", referencedColumnName = "EMPLOY_SEQ")
+    })
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private OffenderEmployment employment;
+}
