8cb6ab57356b8b798469e0263601c809793eefdf
akeemfaulkner <contact@ake3m.com>
2021-06-17 16:39:36 +0100

    DT-2162: Creates endpoint to surface Employment details

--- src/main/java/uk/gov/justice/hmpps/prison/service/transformers/OffenderEmploymentTransformer.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/transformers/OffenderEmploymentTransformer.java
@@ -5,7 +5,14 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.hmpps.prison.api.model.OffenderEmploymentResponse;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderEmployment;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderEmployment.EmploymentPostType;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderEmployment.PayPeriodType;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderEmployment.ScheduleType;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.ReferenceCode;
 import uk.gov.justice.hmpps.prison.service.AddressTransformer;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @Component
 public class OffenderEmploymentTransformer implements Converter<OffenderEmployment, OffenderEmploymentResponse> {
@@ -15,20 +22,24 @@ public class OffenderEmploymentTransformer implements Converter<OffenderEmployme
             offenderEmployment.getId().getBookingId(),
             offenderEmployment.getStartDate(),
             offenderEmployment.getEndDate(),
-            offenderEmployment.getPostType().getDescription(),
+            getOrNull(offenderEmployment.getPostType(), EmploymentPostType::getDescription),
             offenderEmployment.getEmployerName(),
             offenderEmployment.getSupervisorName(),
             offenderEmployment.getPosition(),
             offenderEmployment.getTerminationReason(),
             offenderEmployment.getWage(),
-            offenderEmployment.getWagePeriod().getDescription(),
-            offenderEmployment.getOccupation().getDescription(),
+            getOrNull(offenderEmployment.getWagePeriod(), PayPeriodType::getDescription),
+            getOrNull(offenderEmployment.getOccupation(), ReferenceCode::getDescription),
             offenderEmployment.getComment(),
-            offenderEmployment.getScheduleType().getDescription(),
+            getOrNull(offenderEmployment.getScheduleType(), ScheduleType::getDescription),
             offenderEmployment.getHoursWeek(),
             offenderEmployment.getIsEmployerAware(),
             offenderEmployment.getIsEmployerContactable(),
             AddressTransformer.translate(offenderEmployment.getAddresses())
         );
+    }
+
+    private <T> String getOrNull(T data, Function<T, String> map) {
+        return Optional.ofNullable(data).map(map).orElse(null);
     }
 }
--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/EmploymentResourceTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/EmploymentResourceTest.java
@@ -2,26 +2,17 @@ package uk.gov.justice.hmpps.prison.api.resource.impl;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import uk.gov.justice.hmpps.prison.executablespecification.steps.AuthTokenHelper;
 import uk.gov.justice.hmpps.prison.executablespecification.steps.AuthTokenHelper.AuthToken;
-import uk.gov.justice.hmpps.prison.service.OffenderEmploymentService;
 
-import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.GET;
 
 
 public class EmploymentResourceTest extends ResourceTest {
 
-    private final String OFFENDER_NUMBER = "A1234AB";
-
-    @MockBean
-    private OffenderEmploymentService offenderEmploymentService;
+    private final String OFFENDER_NUMBER = "G8346GA";
 
     @Test
     public void testShouldNotBeAbleToAccessInformation() {
@@ -41,8 +32,6 @@ public class EmploymentResourceTest extends ResourceTest {
     @Test
     public void testShouldBeAbleToAccessInformationAsASystemUser() {
 
-        when(offenderEmploymentService.getOffenderEmployments(OFFENDER_NUMBER, PageRequest.of(0, 10))).thenReturn(new PageImpl<>(emptyList()));
-
         final var token = authTokenHelper.getToken(AuthToken.SYSTEM_USER_READ_WRITE);
         final var httpEntity = createHttpEntity(token, null);
 
@@ -58,8 +47,6 @@ public class EmploymentResourceTest extends ResourceTest {
 
     @Test
     public void testShouldBeAbleToAccessInformationAsAGlobalSearchUser() {
-
-        when(offenderEmploymentService.getOffenderEmployments(OFFENDER_NUMBER, PageRequest.of(0, 10))).thenReturn(new PageImpl<>(emptyList()));
 
         final var token = authTokenHelper.getToken(AuthToken.GLOBAL_SEARCH);
         final var httpEntity = createHttpEntity(token, null);
