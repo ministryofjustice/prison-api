f5265bbcf47c6552727a6e6a9381c20205803b1a
akeemfaulkner <contact@ake3m.com>
2021-06-17 13:45:20 +0100

    Created OffenderEmploymentResponse dto

---
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/OffenderEmploymentResponse.java
@@ -0,0 +1,79 @@
+package uk.gov.justice.hmpps.prison.api.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.Occupation;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderEmployment.PayPeriodType;
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderEmployment.ScheduleType;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(description = "Offender Employment")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OffenderEmploymentResponse {
+
+    @ApiModelProperty(value = "Offender booking id.", example = "14")
+    private Long bookingId;
+
+    @ApiModelProperty(value = "Start date of employment")
+    private LocalDate startDate;
+
+    @ApiModelProperty(value = "End date of employment")
+    private LocalDate endDate;
+
+    @ApiModelProperty(value = "The employment post type condition")
+    private String postType;
+
+    @ApiModelProperty(value = "The name of the employer")
+    private String employerName;
+
+    @ApiModelProperty(value = "Supervisor's name for employment period")
+    private String supervisorName;
+
+    @ApiModelProperty(value = "Position held on job")
+    private String position;
+
+    @ApiModelProperty(value = "The reason for leaving job")
+    private String terminationReason;
+
+    @ApiModelProperty(value = "Amount the offender was earning")
+    private Double wage;
+
+    @ApiModelProperty(value = "The frequency of wage payments")
+    private String wagePeriod;
+
+    @ApiModelProperty(value = "The occupation name of the offender")
+    private String occupation;
+
+    @ApiModelProperty(value = "A comment about the employment")
+    private String comment;
+
+    @ApiModelProperty(value = "The employment schedule")
+    private String schedule;
+
+    @ApiModelProperty(value = "The hours worked per week")
+    private Integer hoursWeek;
+
+    @ApiModelProperty(value = "Whether the employer is aware of the offender's charges")
+    private Boolean isEmployerAware;
+
+    @ApiModelProperty(value = "Whether the employer can be contacted or not")
+    private Boolean isEmployerContactable;
+
+    @NotNull
+    @Builder.Default
+    @ApiModelProperty(value = "A list of addresses associated with the employment")
+    private List<AddressDto> addresses = new ArrayList<>();
+}
