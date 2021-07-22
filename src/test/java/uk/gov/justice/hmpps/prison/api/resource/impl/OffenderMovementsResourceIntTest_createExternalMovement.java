1cb3fcb65cf7e5b4d3921e5b2dbfde83ec667945
JamieDraper <65b174d7066dfab51fad25c9c8774bf6392b1610>
2021-07-22 09:59:27 +0100

    NN-3554: - add test for verify booking access

--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderMovementsResourceIntTest_createExternalMovement.java
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderMovementsResourceIntTest_createExternalMovement.java
@@ -7,26 +7,15 @@ import uk.gov.justice.hmpps.prison.executablespecification.steps.AuthTokenHelper
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.http.HttpMethod.POST;
 
 public class OffenderMovementsResourceIntTest_createExternalMovement extends ResourceTest {
 
     @Test
     public void createExternalMovement() {
-        final var now = LocalDateTime.now();
-        final var bookingId = -1L;
 
-        final var body = Map.of(
-            "bookingId", bookingId,
-            "fromAgencyId", "HAZLWD",
-            "toAgencyId", "OUT",
-            "movementTime", now,
-            "movementType", "TRN",
-            "movementReason", "SEC",
-            "directionCode", "OUT"
-        );
-
-        final var entity = createHttpEntity(authTokenHelper.getToken(AuthToken.NORMAL_USER), body);
+        final var entity = createHttpEntity(authTokenHelper.getToken(AuthToken.NORMAL_USER), getBody());
 
         final var response = testRestTemplate.exchange(
             "/api/movements",
@@ -37,6 +26,32 @@ public class OffenderMovementsResourceIntTest_createExternalMovement extends Res
 
 
         assertThatJsonFileAndStatus(response, 201, "create_external_movement.json");
+    }
+
+    @Test
+    public void returnNotFound_whenThePrisonerIsOutsideOfOurCaseload() {
+       final var entity = createHttpEntity(authTokenHelper.getToken(AuthToken.NO_CASELOAD_USER), getBody());
+
+        final var response = testRestTemplate.exchange(
+            "/api/movements",
+            POST,
+            entity,
+            new ParameterizedTypeReference<String>() {
+            });
+
+        assertThat(response.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    private Map<String,Object> getBody() {
+        return  Map.of(
+            "bookingId", -1,
+            "fromAgencyId", "HAZLWD",
+            "toAgencyId", "OUT",
+            "movementTime", LocalDateTime.now(),
+            "movementType", "TRN",
+            "movementReason", "SEC",
+            "directionCode", "OUT"
+        );
     }
 }
 
