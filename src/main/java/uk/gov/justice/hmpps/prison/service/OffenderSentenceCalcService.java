5fd009fa162e4b65170cc17fba0c2403ac541b2e
Michael Willis <michael.willis@digital.justice.gov.uk>
2021-08-04 15:00:02 +0100

    DT-2408 Sentence Calc WIP

---
+++ src/main/java/uk/gov/justice/hmpps/prison/service/OffenderSentenceCalcService.java
@@ -0,0 +1,11 @@
+package uk.gov.justice.hmpps.prison.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.justice.hmpps.prison.api.model.OffenderSentenceCalc;
+
+@Service
+public class OffenderSentenceCalcService {
+    public OffenderSentenceCalc addCalculatedDates(final String offenderNo, final OffenderSentenceCalc offenderSentenceCalc) {
+        return null;
+    }
+}
