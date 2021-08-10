7d0c9f83afc2ded23188aba18eaea817f4f9d109
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-08-10 15:01:14 +0100

    DT-2329 Added /api/bookings/{bookingId}/alerts/v2 as new contract (but still vulnerable)

--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/BookingResourceIntTest_getOffenderAlerts.java
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/BookingResourceIntTest_getOffenderAlerts.java
@@ -291,4 +291,284 @@ public class BookingResourceIntTest_getOffenderAlerts extends ResourceTest {
             assertThat(jsonContent).extractingJsonPathBooleanValue("[0].active").isFalse();
         }
     }
+    @Nested
+    @DisplayName("GET /api/bookings/{bookingId}/alert/v2")
+    class NewUnSafeEndpoint {
+        @Test
+        @DisplayName("will return a page of alerts sorted by DESC by dateExpires,dateCreated which is ALERT_DATE not CREATE_DATETIME with no filters or sorting")
+        void willReturnAllActiveAlertsWhenNoFilter() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+            assertThat(jsonContent).extractingJsonPathArrayValue("content").hasSize(10);
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].dateExpires").isEqualTo("2021-10-03");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[1].dateExpires").isEqualTo("2021-10-02");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[1].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[2].dateExpires").isEqualTo("2021-10-01");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[2].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[3].dateExpires").isEqualTo("2021-09-03");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[3].dateCreated").isEqualTo("2021-08-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[4].dateExpires").isEqualTo("2021-09-03");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[4].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[5].dateExpires").isEqualTo("2021-09-03");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[5].dateCreated").isEqualTo("2021-06-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[6].dateExpires").isEqualTo("2021-09-02");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[6].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[7].dateExpires").isEqualTo("2021-09-01");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[7].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[8].dateExpires").isEqualTo("2021-08-04");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[8].dateCreated").isEqualTo("2021-07-19");
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[9].dateExpires").isEqualTo("2021-08-03");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[9].dateCreated").isEqualTo("2021-07-19");
+        }
+        @Test
+        @DisplayName("can control sort order")
+        void canControlSortOrder() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?sort={sort}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "sort", "dateCreated,desc"));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+            assertThat(jsonContent).extractingJsonPathArrayValue("content").hasSize(10);
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-08-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[1].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[2].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[3].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[4].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[5].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[6].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[7].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[8].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[9].dateCreated").isEqualTo("2021-07-19");
+        }
+
+        @Test
+        @DisplayName("page totals are returned in the response body")
+        void pageTotalsAreReturnedInTheResponseHeaders() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+            assertThat(jsonContent).extractingJsonPathNumberValue("totalElements").isEqualTo(12);
+            assertThat(jsonContent).extractingJsonPathNumberValue("numberOfElements").isEqualTo(10);
+            assertThat(jsonContent).extractingJsonPathNumberValue("number").isEqualTo(0);
+            assertThat(jsonContent).extractingJsonPathNumberValue("totalPages").isEqualTo(2);
+            assertThat(jsonContent).extractingJsonPathNumberValue("size").isEqualTo(10);
+        }
+
+        @Test
+        @DisplayName("can request second page of results")
+        void canRequestSecondPageOfResults() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?page={page}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "page", 1));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+            assertThat(jsonContent).extractingJsonPathArrayValue("content").hasSize(2);
+            assertThat(jsonContent).extractingJsonPathNumberValue("numberOfElements").isEqualTo(2);
+            assertThat(jsonContent).extractingJsonPathNumberValue("totalElements").isEqualTo(12);
+            assertThat(jsonContent).extractingJsonPathNumberValue("number").isEqualTo(1);
+            assertThat(jsonContent).extractingJsonPathNumberValue("totalPages").isEqualTo(2);
+            assertThat(jsonContent).extractingJsonPathNumberValue("size").isEqualTo(10);
+        }
+
+        @Test
+        @DisplayName("can filter by alert type")
+        void canFilterByAlertType() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?alertType={alertType}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "alertType", "X"));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+            assertThat(jsonContent).extractingJsonPathArrayValue("content").hasSize(3);
+
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].alertType").isEqualTo("X");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].alertCode").isEqualTo("XCU");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[1].alertType").isEqualTo("X");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[1].alertCode").isEqualTo("XCU");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[2].alertType").isEqualTo("X");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[2].alertCode").isEqualTo("XTACT");
+        }
+
+        @Test
+        @DisplayName("can filter by active state")
+        void canFilterByActiveState() {
+            final var activeResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?alertStatus={alertStatus}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "alertStatus", "ACTIVE"));
+
+            assertThat(getBodyAsJsonContent(activeResponse)).extractingJsonPathArrayValue("content").hasSize(9);
+
+
+            final var inactiveResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?alertStatus={alertStatus}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "alertStatus", "INACTIVE"));
+
+            assertThat(getBodyAsJsonContent(inactiveResponse)).extractingJsonPathArrayValue("content").hasSize(3);
+        }
+        @Test
+        @DisplayName("can filter by before createDate (alert date)")
+        void canFilterByBeforeCreateDate() {
+            final var oldestAlertResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?to={to}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "to", "2021-06-19"));
+
+            assertThat(getBodyAsJsonContent(oldestAlertResponse)).extractingJsonPathArrayValue("content").hasSize(1);
+            assertThat(getBodyAsJsonContent(oldestAlertResponse)).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-06-19");
+
+            final var justBeforeSecondOldestAlertResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?to={to}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "to", "2021-07-18"));
+
+            assertThat(getBodyAsJsonContent(justBeforeSecondOldestAlertResponse)).extractingJsonPathArrayValue("content").hasSize(1);
+            assertThat(getBodyAsJsonContent(justBeforeSecondOldestAlertResponse)).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-06-19");
+        }
+        @Test
+        @DisplayName("can filter by after createDate (alert date)")
+        void canFilterByAfterCreateDate() {
+            final var youngestAlertResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?from={from}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "from", "2021-08-19"));
+
+            assertThat(getBodyAsJsonContent(youngestAlertResponse)).extractingJsonPathArrayValue("content").hasSize(1);
+            assertThat(getBodyAsJsonContent(youngestAlertResponse)).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-08-19");
+
+            final var justAfterSecondYoungestAlertResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?from={from}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "from", "2021-07-20"));
+
+            assertThat(getBodyAsJsonContent(justAfterSecondYoungestAlertResponse)).extractingJsonPathArrayValue("content").hasSize(1);
+            assertThat(getBodyAsJsonContent(justAfterSecondYoungestAlertResponse)).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-08-19");
+        }
+
+        @Test
+        @DisplayName("can filter between inclusive two dates")
+        void canFilterBetweenInclusiveTwoDates() {
+            final var middleDatesResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?from={from}&to={to}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "from", "2021-07-19", "to", "2021-07-19"));
+
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathArrayValue("content").hasSize(10);
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathNumberValue("totalElements").isEqualTo(10);
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[1].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[2].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[3].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[4].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[5].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[6].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[7].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[8].dateCreated").isEqualTo("2021-07-19");
+            assertThat(getBodyAsJsonContent(middleDatesResponse)).extractingJsonPathStringValue("content[9].dateCreated").isEqualTo("2021-07-19");
+
+            final var justBeforeAndAfterMiddleDatesResponse = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?from={from}&to={to}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "from", "2021-07-18", "to", "2021-07-20"));
+
+            assertThat(getBodyAsJsonContent(justBeforeAndAfterMiddleDatesResponse)).extractingJsonPathArrayValue("content").hasSize(10);
+            assertThat(getBodyAsJsonContent(justBeforeAndAfterMiddleDatesResponse)).extractingJsonPathNumberValue("totalElements").isEqualTo(10);
+        }
+
+        @Test
+        @DisplayName("can filter be dateCreated and active state and alert type")
+        void canFilterBeDateCreatedAndActiveStateAndAlertType() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?alertType={alertType}&from={from}&to={to}&alertStatus={alertStatus}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "from", "2021-07-19", "to", "2021-07-19", "alertType", "X", "alertStatus", "INACTIVE"));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+
+            assertThat(jsonContent).extractingJsonPathArrayValue("content").hasSize(1);
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].dateCreated").isEqualTo("2021-07-19");
+            assertThat(jsonContent).extractingJsonPathStringValue("content[0].alertType").isEqualTo("X");
+            assertThat(jsonContent).extractingJsonPathBooleanValue("content[0].active").isFalse();
+        }
+
+        @Test
+        @DisplayName("empty filter parameters are ignored")
+        void emptyFilterParametersAreIgnored() {
+            final var response = testRestTemplate.exchange(
+                "/api/bookings/{bookingId}/alerts/v2?alertType={alertType}&from={from}&to={to}&alertStatus={alertStatus}",
+                GET,
+                createEmptyHttpEntity(AuthToken.VIEW_PRISONER_DATA, Map.of()),
+                new ParameterizedTypeReference<String>() {
+                },
+                Map.of("bookingId", -56, "from", "", "to", "", "alertType", "", "alertStatus", ""));
+
+            final var jsonContent = getBodyAsJsonContent(response);
+            assertThat(jsonContent).extractingJsonPathArrayValue("content").hasSize(10);
+            assertThat(jsonContent).extractingJsonPathNumberValue("totalElements").isEqualTo(12);
+        }
+    }
 }
