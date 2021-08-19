b921021faa1e1be3a3eaa7c476ad36cc2eac3c33
sp-watson <steve.watson@digital,justice.gov.uk>
2021-08-19 09:27:10 +0100

    ESWE-67: Do not expose API docs yet

--- src/main/java/uk/gov/justice/hmpps/prison/api/resource/OffenderActivitiesResource.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/resource/OffenderActivitiesResource.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import uk.gov.justice.hmpps.prison.api.model.ErrorResponse;
 import uk.gov.justice.hmpps.prison.api.model.OffenderActivities;
 import uk.gov.justice.hmpps.prison.service.OffenderActivitiesService;
 
+@Hidden // This is WIP
 @RestController
 @Api(tags = {"offender-relationships"})
 @Validated
