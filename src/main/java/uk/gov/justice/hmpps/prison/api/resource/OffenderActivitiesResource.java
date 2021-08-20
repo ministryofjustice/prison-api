eb37e80940e33a7a3ed3c0e1efd423209b9b14f1
sp-watson <steve.watson@digital,justice.gov.uk>
2021-08-20 15:12:34 +0100

    ESWE-72: Add resource entry for new endpoint

--- src/main/java/uk/gov/justice/hmpps/prison/api/resource/OffenderActivitiesResource.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/resource/OffenderActivitiesResource.java
@@ -1,19 +1,28 @@
 package uk.gov.justice.hmpps.prison.api.resource;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.justice.hmpps.prison.api.model.ErrorResponse;
 import uk.gov.justice.hmpps.prison.api.model.OffenderActivities;
 import uk.gov.justice.hmpps.prison.service.OffenderActivitiesService;
+
+import java.time.LocalDate;
 
 @Hidden // This is WIP
 @RestController
@@ -35,5 +44,21 @@ public class OffenderActivitiesResource {
     @GetMapping("/{offenderNo}/current-work")
     public OffenderActivities getCurrentWorkActivities(@PathVariable("offenderNo") @ApiParam(value = "The offenderNo of the prisoner", required = true) final String offenderNo) {
         return activitesService.getCurrentWorkActivities(offenderNo);
+    }
+
+    @ApiResponses({
+        @ApiResponse(code = 500, message = "Unrecoverable error occurred whilst processing request.", response = ErrorResponse.class, responseContainer = "List")})
+    @ApiOperation(value = "The work activities that this offender has been allocated to", notes = "This includes suspended activities", nickname = "getRecentStartedWorkActivities")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "page", dataType = "java.lang.Integer", paramType = "query",
+            value = "Results page you want to retrieve (0..N). Default 0, e.g. the first page", example = "0"),
+        @ApiImplicitParam(name = "size", dataType = "java.lang.Integer", paramType = "query",
+            value = "Number of records per page. Default 10")
+    })
+    @GetMapping("/{offenderNo}/work-history")
+    public OffenderActivities getRecentStartedWorkActivities(@PathVariable("offenderNo") @ApiParam(value = "The offenderNo of the prisoner", required = true) final String offenderNo,
+            @RequestParam(value = "earliestEndDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "Only include activities that have not ended or have an end date after the given date", example = "1970-01-02") final LocalDate earliestEndDate,
+            @PageableDefault final Pageable pageable) {
+        return activitesService.getStartedWorkActivities(offenderNo, earliestEndDate, pageable);
     }
 }
