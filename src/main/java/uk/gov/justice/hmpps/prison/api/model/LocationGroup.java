bcee4983b63fa1d7775624d2eadee258f0fe5cf0
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-10-07 13:29:52 +0100

    NN-3625 rename of prisonStaffHub

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/LocationGroup.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/LocationGroup.java
@@ -14,10 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Cell Locations are grouped for unlock lists as a 2 level tree. The two levels are referred to as Location and Sub-Location in the prisonstaffhub UI. Each (location/sub-location) group has a name that is understood by prison officers and also serves as a key to retrieve the corresponding Cell Locations and information about their occupants.
+ * Cell Locations are grouped for unlock lists as a 2 level tree. The two levels are referred to as Location and Sub-Location in the digital prison services UI. Each (location/sub-location) group has a name that is understood by prison officers and also serves as a key to retrieve the corresponding Cell Locations and information about their occupants.
  **/
 @SuppressWarnings("unused")
-@ApiModel(description = "Cell Locations are grouped for unlock lists as a 2 level tree. The two levels are referred to as Location and Sub-Location in the prisonstaffhub UI. Each (location/sub-location) group has a name that is understood by prison officers and also serves as a key to retrieve the corresponding Cell Locations and information about their occupants.")
+@ApiModel(description = "Cell Locations are grouped for unlock lists as a 2 level tree. The two levels are referred to as Location and Sub-Location in the digital prison services UI. Each (location/sub-location) group has a name that is understood by prison officers and also serves as a key to retrieve the corresponding Cell Locations and information about their occupants.")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @Builder(toBuilder = true)
