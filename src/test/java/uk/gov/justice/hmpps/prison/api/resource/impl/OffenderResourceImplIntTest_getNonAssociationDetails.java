2529c37ac651c1363c26332128964aebbcb95159
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2021-11-03 09:39:04 +0000

    VB-307 add endpoint to retrieve non-associations by offender no

---
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderResourceImplIntTest_getNonAssociationDetails.java
@@ -0,0 +1,75 @@
+package uk.gov.justice.hmpps.prison.api.resource.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import uk.gov.justice.hmpps.prison.api.model.ErrorResponse;
+import uk.gov.justice.hmpps.prison.executablespecification.steps.AuthTokenHelper.AuthToken;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OffenderResourceImplIntTest_getNonAssociationDetails extends ResourceTest {
+
+    @Test
+    public void shouldReturnListOfContacts() {
+        final var token = authTokenHelper.getToken(AuthToken.NORMAL_USER);
+
+        final var request = createHttpEntity(token, null);
+
+        final var response = testRestTemplate.exchange(
+                "/api/offenders/A1234AA/non-association-details",
+                HttpMethod.GET,
+                request,
+                new ParameterizedTypeReference<String>() {
+                });
+
+        final var json = getBodyAsJsonContent(response);
+        assertThat(json).extractingJsonPathStringValue("offenderNo").isEqualTo("A1234AA");
+        assertThat(json).extractingJsonPathStringValue("firstName").isEqualTo("ARTHUR");
+        assertThat(json).extractingJsonPathStringValue("lastName").isEqualTo("ANDERSON");
+        assertThat(json).extractingJsonPathArrayValue("nonAssociations").hasSize(2);
+        assertThat(json).extractingJsonPathStringValue("nonAssociations[0].reasonCode").isEqualTo("VIC");
+        assertThat(json).extractingJsonPathStringValue("nonAssociations[0].offenderNonAssociation.offenderNo").isEqualTo("A1234AB");
+        assertThat(json).extractingJsonPathStringValue("nonAssociations[0].offenderNonAssociation.firstName").isEqualTo("GILLIAN");
+        assertThat(json).extractingJsonPathStringValue("nonAssociations[1].offenderNonAssociation.offenderNo").isEqualTo("A1234AC");
+        assertThat(json).extractingJsonPathStringValue("nonAssociations[1].offenderNonAssociation.firstName").isEqualTo("NORMAN");
+    }
+
+
+    @Test
+    public void shouldReturn404WhenOffenderNotFound() {
+        final var token = authTokenHelper.getToken(AuthToken.NORMAL_USER);
+
+        final var request = createHttpEntity(token, null);
+
+        final var response = testRestTemplate.exchange(
+                "/api/offenders/AAA444/non-association-details",
+                HttpMethod.GET,
+                request,
+                ErrorResponse.class);
+
+        assertThat(response.getBody()).isEqualTo(
+                ErrorResponse.builder()
+                        .status(404)
+                        .userMessage("Resource with id [AAA444] not found.")
+                        .developerMessage("Resource with id [AAA444] not found.")
+                        .build());
+    }
+
+    @Test
+    public void shouldReturn404IfNotAuthorised() {
+        final var token = authTokenHelper.getToken(AuthToken.GLOBAL_SEARCH);
+
+        final var request = createHttpEntity(token, null);
+
+        final var response = testRestTemplate.exchange(
+                "/api/offenders/A1234AA/non-association-details",
+                HttpMethod.GET,
+                request,
+                ErrorResponse.class);
+
+        assertThat(Objects.requireNonNull(response.getBody()).getStatus()).isEqualTo(404);
+    }
+}
