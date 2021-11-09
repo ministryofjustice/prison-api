2689ba1eb471afe3761a4c761883cf1742a7e207
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2021-11-09 10:44:39 +0000

    VB-309 offender endpoint to receive visit restrictions by offender no

---
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/OffenderRestriction.java
@@ -0,0 +1,45 @@
+package uk.gov.justice.hmpps.prison.api.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
+@ApiModel(description = "Offender restriction")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class OffenderRestriction {
+
+    @ApiModelProperty(required = true, value = "restriction id")
+    private Long restrictionId;
+
+    @ApiModelProperty(value = "Restriction comment text")
+    private String comment;
+
+    @ApiModelProperty(required = true, value = "code of restriction type")
+    private String restrictionType;
+
+    @ApiModelProperty(required = true, value = "description of restriction type")
+    @NotBlank
+    private String restrictionTypeDescription;
+
+    @ApiModelProperty(required = true, value = "Date from which the restrictions applies", example="1980-01-01")
+    private LocalDate startDate;
+
+    @ApiModelProperty(value = "Date restriction applies to, or indefinitely if null", example="1980-01-01")
+    private LocalDate expiryDate;
+
+    @ApiModelProperty(required = true, value = "true if restriction is within the start date and optional expiry date range")
+    private boolean active;
+}
