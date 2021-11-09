99b9283965426ae07822e08a8f6e86ad4a762163
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2021-11-09 11:27:47 +0000

    DT-2556: make test transactional to stop it breaking other tests

--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderDatesResourceTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderDatesResourceTest.java
@@ -3,6 +3,7 @@ package uk.gov.justice.hmpps.prison.api.resource.impl;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.justice.hmpps.prison.api.model.ErrorResponse;
 import uk.gov.justice.hmpps.prison.api.model.RequestToUpdateOffenderDates;
 import uk.gov.justice.hmpps.prison.executablespecification.steps.AuthTokenHelper.AuthToken;
@@ -13,41 +14,52 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// Make these tests transactional because inserting into the database broke other tests, such as OffendersResourceTest
+// because they depend on seed data from R__4_19__OFFENDER_SENT_CALCULATIONS.sql
+@Transactional
 public class OffenderDatesResourceTest extends ResourceTest {
 
+    private static final long BOOKING_ID = -2;
     private static final LocalDate NOV_11_2021 = LocalDate.of(2021, 11, 8);
 
     @Test
     public void testCanUpdateOffenderDates() {
+        // Given
         final var token = authTokenHelper.getToken(AuthToken.CRD_USER);
         final var body = RequestToUpdateOffenderDates.builder()
             .keyDates(OffenderDatesServiceTest.createOffenderKeyDates(NOV_11_2021, NOV_11_2021, NOV_11_2021))
             .build();
         final var request = createHttpEntity(token, body);
 
+        // When
         final var responseEntity = testRestTemplate.exchange(
             "/api/offender-dates/{bookingId}",
             HttpMethod.POST,
             request,
             new ParameterizedTypeReference<String>() {
             },
-            -2);
+            Map.of("bookingId", BOOKING_ID));
 
+        // Then
         assertThatJsonFileAndStatus(responseEntity, 201, "offenderkeydatesupdated.json");
     }
 
     @Test
     public void testCantUpdateOffenderDatesWithInvalidBookingId() {
+        // Given
         final var token = authTokenHelper.getToken(AuthToken.CRD_USER);
-        final var body = Map.of("some key", "some value");
+        final var body = RequestToUpdateOffenderDates.builder().build();
         final var request = createHttpEntity(token, body);
 
+        // When
         final var response = testRestTemplate.exchange(
             "/api/offender-dates/{bookingId}",
             HttpMethod.POST,
             request,
-            ErrorResponse.class,0,-11);
+            ErrorResponse.class,
+            Map.of("bookingId", 0));
 
+        // Then
         assertThat(response.getBody()).isEqualTo(
             ErrorResponse.builder()
                 .status(404)
@@ -58,16 +70,20 @@ public class OffenderDatesResourceTest extends ResourceTest {
 
     @Test
     public void testCantUpdateOffenderDatesWithIncorrectRole() {
+        // Given
         final var token = authTokenHelper.getToken(AuthToken.NORMAL_USER);
         final var body = Map.of("some key", "some value");
         final var request = createHttpEntity(token, body);
 
+        // When
         final var response = testRestTemplate.exchange(
             "/api/offender-dates/{bookingId}",
             HttpMethod.POST,
             request,
-            ErrorResponse.class,0,-11);
+            ErrorResponse.class,
+            Map.of("bookingId", BOOKING_ID));
 
+        // Then
         assertThat(response.getBody()).isEqualTo(
             ErrorResponse.builder()
                 .status(403)
