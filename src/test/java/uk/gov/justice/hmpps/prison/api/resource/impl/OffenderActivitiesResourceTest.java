d43d00bd7cff5480fad2761c56aafab94b42606a
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2021-11-11 16:21:47 +0000

    ESWE-214 prison-api endpoints should not have role protection (#1083)
    The new education endpoints should be accessible to all

--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderActivitiesResourceTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderActivitiesResourceTest.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,7 +22,7 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
     class GetActivitiesHistory {
         @Test
         public void successfulRequest_returnsCorrectData() {
-            final var entity = createHttpEntity(validToken(List.of("ROLE_SYSTEM_USER")), null);
+            final var entity = createHttpEntity(validToken(), null);
 
             final var response = testRestTemplate.exchange(
                 "/api/offender-activities/A1234AC/activities-history?earliestEndDate=2021-01-01",
@@ -40,7 +39,7 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
 
         @Test
         public void badRequest_NoEndDateParameter() {
-            final var entity = createHttpEntity(validToken(List.of("ROLE_SYSTEM_USER")), null);
+            final var entity = createHttpEntity(validToken(), null);
 
             final var response = testRestTemplate.exchange(
                 "/api/offender-activities/1234/activities-history",
@@ -54,7 +53,7 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
 
         @Test
         public void successfulRequest_page() {
-            final var entity = createHttpEntity(validToken(List.of("ROLE_SYSTEM_USER")), null);
+            final var entity = createHttpEntity(validToken(), null);
 
             final var response = testRestTemplate.exchange(
                 "/api/offender-activities/A1234AC/activities-history?earliestEndDate=2021-01-01&page=1&size=2",
@@ -69,26 +68,13 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
             assertThat(jsonContent).extractingJsonPathNumberValue("$.totalPages").isEqualTo(3);
             assertThat(jsonContent).extractingJsonPathNumberValue("$.totalElements").isEqualTo(5);
         }
-
-        @Test
-        public void noRole() {
-            final var entity = createHttpEntity(validToken(List.of()), null);
-
-            final var response = testRestTemplate.exchange(
-                "/api/offender-activities/1234/activities-history?earliestEndDate=2021-01-01",
-                HttpMethod.GET,
-                entity,
-                String.class);
-
-            assertThatStatus(response, HttpStatus.FORBIDDEN.value());
-        }
     }
 
     @Nested
     class GetHistoricalAttendances {
         @Test
         public void successfulRequest_returnsCorrectData() {
-            final var entity = createHttpEntity(validToken(List.of("ROLE_SYSTEM_USER")), null);
+            final var entity = createHttpEntity(validToken(), null);
             final var today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
 
             final var response = testRestTemplate.exchange(
@@ -102,7 +88,7 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
 
         @Test
         public void badRequest_NoFromDateParameter() {
-            final var entity = createHttpEntity(validToken(List.of("ROLE_SYSTEM_USER")), null);
+            final var entity = createHttpEntity(validToken(), null);
 
             final var response = testRestTemplate.exchange(
                 "/api/offender-activities/1234/attendance-history?toDate=2017-01-01",
@@ -116,7 +102,7 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
 
         @Test
         public void badRequest_NoToDateParameter() {
-            final var entity = createHttpEntity(validToken(List.of("ROLE_SYSTEM_USER")), null);
+            final var entity = createHttpEntity(validToken(), null);
 
             final var response = testRestTemplate.exchange(
                 "/api/offender-activities/1234/attendance-history?fromDate=2017-01-01",
@@ -126,19 +112,6 @@ public class OffenderActivitiesResourceTest extends ResourceTest {
 
             assertThatStatus(response, HttpStatus.BAD_REQUEST.value());
             assertThatJson(response.getBody()).node("userMessage").asString().isEqualTo("Required request parameter 'toDate' for method parameter type LocalDate is not present");
-        }
-
-        @Test
-        public void noRole() {
-            final var entity = createHttpEntity(validToken(List.of()), null);
-
-            final var response = testRestTemplate.exchange(
-                "/api/offender-activities/1234/attendance-history?fromDate=2017-01-01&toDate=2017-01-02",
-                HttpMethod.GET,
-                entity,
-                String.class);
-
-            assertThatStatus(response, HttpStatus.FORBIDDEN.value());
         }
     }
 }
