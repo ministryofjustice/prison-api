5a6e76bff23b5679d50658600c3a277bd4216957
Richard Popple <richard.popple@digital.justice.gov.uk>
2021-11-15 13:46:10 +0000

    NN-3723 fixing swagger docs for page parameters

--- src/main/java/uk/gov/justice/hmpps/prison/api/resource/AdjudicationsResource.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/resource/AdjudicationsResource.java
@@ -1,6 +1,8 @@
 package uk.gov.justice.hmpps.prison.api.resource;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 import uk.gov.justice.hmpps.prison.api.model.AdjudicationDetail;
 import uk.gov.justice.hmpps.prison.api.model.AdjudicationSearchRequest;
 import uk.gov.justice.hmpps.prison.api.model.ErrorResponse;
@@ -79,10 +82,18 @@ public class AdjudicationsResource {
     }
 
     @ApiOperation(value = "Search of adjudications", notes = "Requires MAINTAIN_ADJUDICATIONS access")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "page", dataType = "java.lang.Integer", paramType = "query",
+            value = "Results page you want to retrieve (0..N). Default 0, e.g. the first page", example = "0"),
+        @ApiImplicitParam(name = "size", dataType = "java.lang.Integer", paramType = "query",
+            value = "Number of records per page. Default 10"),
+        @ApiImplicitParam(name = "sort", dataType = "java.lang.String", paramType = "query",
+            value = "Sort as combined comma separated property and uppercase direction. Multiple sort params allowed to sort by multiple properties. Default to incidentDate,incidentTime ASC")})
     @PostMapping("/search")
     @PreAuthorize("hasRole('MAINTAIN_ADJUDICATIONS')")
     public Page<AdjudicationDetail> getAdjudications(@ApiParam(value = "The adjudication search request", required = true) @RequestBody final AdjudicationSearchRequest searchRequest,
-                                                     @PageableDefault(sort = {"incidentDate", "incidentTime"}, direction = Sort.Direction.DESC, size = 20) final Pageable pageable) {
+                                                     @PageableDefault(sort = {"incidentDate", "incidentTime"}, direction = Sort.Direction.DESC, size = 20)
+                                                     @ApiIgnore final Pageable pageable) {
         return adjudicationsService.getAdjudications(searchRequest, pageable);
     }
 }
