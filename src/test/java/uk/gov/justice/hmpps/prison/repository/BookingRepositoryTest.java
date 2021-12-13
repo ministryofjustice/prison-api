b188be2e710d3086f91c4dcb3e09f245adeb3c50
lbennett-moj <lucy.bennett@digital.justice.gov.uk>
2021-12-13 15:17:17 +0000

    VB-316 add iep last updated dates for visit order and privilege visit order balances.

--- src/test/java/uk/gov/justice/hmpps/prison/repository/BookingRepositoryTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/repository/BookingRepositoryTest.java
@@ -168,7 +168,8 @@ public class BookingRepositoryTest {
     public void findBalancesForVisitOrdersAndPrivilageVisitOrders() {
         final var visitBalances = repository.getBookingVisitBalances(-1L);
 
-        assertThat(visitBalances).get().isEqualToIgnoringGivenFields(new VisitBalances(25, 2, LocalDate.now(), LocalDate.now()));
+        assertThat(visitBalances).get().isEqualToIgnoringGivenFields(
+                VisitBalances.builder().remainingVo(25).remainingPvo(2).latestIepAdjustmentDate(LocalDate.parse("2021-09-22")).latestPrivilegeIepAdjustmentDate(LocalDate.parse("2021-10-22")).build());
     }
 
     @Test
