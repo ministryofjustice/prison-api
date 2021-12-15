569f0966ff01e834fecc315435890a6b0619de36
Peter Phillips <peter@greenthistle.co.uk>
2021-12-15 12:08:37 +0000

    DT-2990: 🐛 Add missing conversion to local date time

--- src/test/java/uk/gov/justice/hmpps/prison/executablespecification/ParameterTypes.java
+++ src/test/java/uk/gov/justice/hmpps/prison/executablespecification/ParameterTypes.java
@@ -2,6 +2,7 @@ package uk.gov.justice.hmpps.prison.executablespecification;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.cucumber.java.DefaultDataTableCellTransformer;
 import io.cucumber.java.DefaultDataTableEntryTransformer;
 import io.cucumber.java.DefaultParameterTransformer;
@@ -14,6 +15,7 @@ public class ParameterTypes {
 
     public ParameterTypes() {
         objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        objectMapper.registerModule(new JavaTimeModule());
     }
 
     @DefaultParameterTransformer
