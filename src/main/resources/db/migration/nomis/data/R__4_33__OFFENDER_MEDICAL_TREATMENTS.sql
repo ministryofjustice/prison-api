06236a3c8f8a7456e71aaed169052a5269b7e791
petergphillips <peter@greenthistle.co.uk>
2022-02-14 15:23:47 +0000

    SDI-50: 🔧 Initial stab at h2 migration (#1163)
    * SDI-50: 🔧 Initial stab at h2 migration
    
    * SDI-50: 🔧 Correct test data due to correcting date format insert

--- src/main/resources/db/migration/nomis/data/R__4_33__OFFENDER_MEDICAL_TREATMENTS.sql
+++ src/main/resources/db/migration/nomis/data/R__4_33__OFFENDER_MEDICAL_TREATMENTS.sql
@@ -1,8 +1,8 @@
 Insert into OFFENDER_MEDICAL_TREATMENTS (OFFENDER_MEDICAL_TREATMENT_ID, OFFENDER_HEALTH_PROBLEM_ID, START_DATE,
                                          CASELOAD_TYPE, END_DATE, TREATMENT_CODE, COMMENT_TEXT, DESCRIPTION,
                                          TREATMENT_PROVIDER_CODE)
-values (-1001, -206, to_date('21-06-2010', 'DD-MM-YYYY'), 'INST', null, 'WHEELCHR_ACC', 'Some Comment Text', 'Some Description Text', null);
+values (-1001, -206, to_date('21-06-2010', 'DD-MM-YYYY'), 'INST', null, 'WHEELCHR_ACC', 'Some Comment Text', 'Some Description Text', 'DEN');
 Insert into OFFENDER_MEDICAL_TREATMENTS (OFFENDER_MEDICAL_TREATMENT_ID, OFFENDER_HEALTH_PROBLEM_ID, START_DATE,
                                          CASELOAD_TYPE, END_DATE, TREATMENT_CODE, COMMENT_TEXT, DESCRIPTION,
                                          TREATMENT_PROVIDER_CODE)
-values (-1002, -206, to_date('21-06-2010', 'DD-MM-YYYY'), 'INST', null, 'COMP SOFT', 'EFGH', null, null);
+values (-1002, -206, to_date('21-06-2010', 'DD-MM-YYYY'), 'INST', null, 'COMP SOFT', 'EFGH', null, 'DR');
