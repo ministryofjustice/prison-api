6a09fbb1ab8a78fe36f580356711937d93a9029c
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-03-04 12:42:21 +0000

    CRS-784 removed unused imports.

--- src/main/java/uk/gov/justice/hmpps/prison/service/OffenderFixedTermRecallService.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/OffenderFixedTermRecallService.java
@@ -1,34 +1,11 @@
 package uk.gov.justice.hmpps.prison.service;
 
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import uk.gov.justice.hmpps.prison.api.model.BedAssignment;
 import uk.gov.justice.hmpps.prison.api.model.ReturnToCustodyDate;
-import uk.gov.justice.hmpps.prison.core.HasWriteScope;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.AgencyInternalLocation;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.BedAssignmentHistory;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.BedAssignmentHistory.BedAssignmentHistoryPK;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderBooking;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderFixedTermRecall;
-import uk.gov.justice.hmpps.prison.repository.jpa.repository.AgencyInternalLocationRepository;
-import uk.gov.justice.hmpps.prison.repository.jpa.repository.BedAssignmentHistoriesRepository;
 import uk.gov.justice.hmpps.prison.repository.jpa.repository.OffenderFixedTermRecallRepository;
-import uk.gov.justice.hmpps.prison.security.VerifyAgencyAccess;
-import uk.gov.justice.hmpps.prison.security.VerifyBookingAccess;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
