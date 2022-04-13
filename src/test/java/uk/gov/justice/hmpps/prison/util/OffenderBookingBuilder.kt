ebe5f2012475382575d2038e53499083a249b206
Andy Marke <andy.marke@digital.justice.gov.uk>
2022-04-13 15:36:35 +0100

    SDI-152 Added additional transfer integration tests

--- src/test/java/uk/gov/justice/hmpps/prison/util/OffenderBookingBuilder.kt
+++ src/test/java/uk/gov/justice/hmpps/prison/util/OffenderBookingBuilder.kt
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.returnResult
 import org.springframework.web.reactive.function.BodyInserters
+import uk.gov.justice.hmpps.prison.api.model.IepLevelAndComment
 import uk.gov.justice.hmpps.prison.api.model.InmateDetail
 import uk.gov.justice.hmpps.prison.api.model.RequestForNewBooking
 import java.time.LocalDateTime
@@ -15,13 +16,22 @@ class OffenderBookingBuilder(
   var movementReasonCode: String = "N",
   var youthOffender: Boolean = false,
   var cellLocation: String? = null,
-  var imprisonmentStatus: String = "SENT03"
+  var imprisonmentStatus: String = "SENT03",
+  var iepLevel: String? = null,
+  var iepLevelComment: String = "iep level comment"
 ) : WebClientEntityBuilder() {
+
+  fun withIEPLevel(iepLevel: String): OffenderBookingBuilder {
+    this.iepLevel = iepLevel
+    return this
+  }
+
   fun save(
     webTestClient: WebTestClient,
     jwtAuthenticationHelper: JwtAuthenticationHelper,
     offenderNo: String
   ): InmateDetail {
+
     val request =
       RequestForNewBooking.builder().bookingInTime(bookingInTime).cellLocation(cellLocation)
         .fromLocationId(fromLocationId).imprisonmentStatus(imprisonmentStatus).movementReasonCode(movementReasonCode)
@@ -42,6 +52,24 @@ class OffenderBookingBuilder(
       )
       .exchange()
       .expectStatus().isOk
-      .returnResult<InmateDetail>().responseBody.blockFirst()!!
+      .returnResult<InmateDetail>().responseBody.blockFirst()!!.also {
+      this.iepLevel?.run {
+        webTestClient.post()
+          .uri("/api/bookings/{bookingId}/iepLevels", it.bookingId)
+          .headers(
+            setAuthorisation(
+              jwtAuthenticationHelper = jwtAuthenticationHelper,
+              roles = listOf("ROLE_MAINTAIN_IEP")
+            )
+          )
+          .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+          .accept(MediaType.APPLICATION_JSON)
+          .body(
+            BodyInserters.fromValue(IepLevelAndComment.builder().iepLevel(iepLevel).comment(iepLevelComment).build())
+          )
+          .exchange()
+          .expectStatus().is2xxSuccessful
+      }
+    }
   }
 }
