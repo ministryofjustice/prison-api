5a05c36c4a475d5559cf1fc66d0b8296d294512b
Peter Phillips <peter@greenthistle.co.uk>
2022-04-29 12:34:17 +0100

    SDI-171: 🔒️ Suppress sarif4k ktlint vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta-3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.6"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
   kotlin("plugin.lombok") version "1.6.21"
