6a9ea1548cc603e33f51ae02b052d84e6be43426
yasin <yasin.mustafa@gmail.com>
2022-05-12 12:25:09 +0100

    OFFENCES-68 Add endpoint to create statute record, protected by OFFENCE_MAINTAINER role

---
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/StatuteRepository.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.hmpps.prison.repository.jpa.repository
+
+import org.springframework.data.repository.CrudRepository
+import uk.gov.justice.hmpps.prison.repository.jpa.model.Statute
+
+interface StatuteRepository : CrudRepository<Statute, String>
