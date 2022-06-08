b4abbe6d511c11ab4f371374cdf7d637fc5987e6
Peter Phillips <peter@greenthistle.co.uk>
2022-06-08 12:14:53 +0100

    SDI-195: 🐛 Exclude events where the offender program end date is before the schedule date

--- src/test/java/uk/gov/justice/hmpps/prison/service/PrisonerActivityImpl.kt
+++ src/test/java/uk/gov/justice/hmpps/prison/service/PrisonerActivityImpl.kt
@@ -1,10 +1,18 @@
 package uk.gov.justice.hmpps.prison.service
 
 import uk.gov.justice.hmpps.prison.repository.jpa.repository.PrisonerActivity
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class PrisonerActivityImpl(
   override val bookingId: Long,
   override val suspended: String?,
-  override val startTime: LocalDateTime,
-) : PrisonerActivity
+  override val programStatus: String?,
+  val programEnd: String?,
+  val schedule: String?,
+  val start: String,
+) : PrisonerActivity {
+  override val programEndDate: LocalDate? = programEnd?.let { LocalDate.parse(programEnd) }
+  override val scheduleDate: LocalDate? = schedule?.let { LocalDate.parse(schedule) }
+  override val startTime: LocalDateTime = start.let { LocalDateTime.parse(start) }
+}
