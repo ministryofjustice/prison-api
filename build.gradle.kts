5171dfd3493da5141391d8ac66693d5d326672b1
petergphillips <peter@greenthistle.co.uk>
2022-06-24 08:00:12 +0100

    SDI-252: ⬆️  spring boot 2.7.1 (#1340)
    * SDI-252: ⬆️  spring boot 2.7.1
    
    * SDI-252: 🐛 Change message as exclamation mark now removed from validation message

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0-beta-1"
   kotlin("plugin.spring") version "1.7.0"
   kotlin("plugin.jpa") version "1.7.0"
   kotlin("plugin.lombok") version "1.7.0"
