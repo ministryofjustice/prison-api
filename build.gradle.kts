9fefd8d8e2102f1a1d36b6aaf6fb3803ffba6da6
petergphillips <peter@greenthistle.co.uk>
2022-07-01 15:44:22 +0100

    SDI-257: 🔒️ Suppress false positive for log4j (#1350)
    * SDI-261: 💩 Force pinning of versions so that upgrade versions ignores it each time
    
    * SDI-257: 🔒️ Suppress false positive for log4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.2"
   kotlin("plugin.spring") version "1.7.0"
   kotlin("plugin.jpa") version "1.7.0"
   kotlin("plugin.lombok") version "1.7.0"
