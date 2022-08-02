aec019bf356c90d9695b4bc289ce541193137b4b
michalnawrockidj <92578842+michalnawrockidj@users.noreply.github.com>
2022-08-02 17:13:22 +0100

    DCS-1759 count offender health problems only on the newest booking (#1373)

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderHealthProblemRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderHealthProblemRepository.java
@@ -6,8 +6,11 @@ import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderHealthProblem;
 
 import java.time.LocalDate;
 import java.util.List;
+
 @Repository
 public interface OffenderHealthProblemRepository extends CrudRepository<OffenderHealthProblem, Long> {
 
-    List<OffenderHealthProblem> findAllByOffenderBookingOffenderNomsIdInAndProblemTypeCodeAndStartDateAfterAndStartDateBefore(List<String> nomisId, String problemTypeCode, LocalDate fromStartDate, LocalDate toStartDate);
+    List<OffenderHealthProblem> findAllByOffenderBookingOffenderNomsIdInAndOffenderBookingBookingSequenceAndProblemTypeCodeAndStartDateAfterAndStartDateBefore(List<String> nomisId, Integer bookingSequence, String problemTypeCode, LocalDate fromStartDate, LocalDate toStartDate);
+
+
 }
--- src/test/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderHealthProblemRepositoryTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderHealthProblemRepositoryTest.java
@@ -21,9 +21,9 @@ public class OffenderHealthProblemRepositoryTest {
     private OffenderHealthProblemRepository repository;
 
     @Test
-    void findAllByOffenderBookingOffenderNomsIdInAndProblemTypeCodeAndStartDateAfterAndStartDateBefore() {
-        final var list = repository.findAllByOffenderBookingOffenderNomsIdInAndProblemTypeCodeAndStartDateAfterAndStartDateBefore(List.of("A1234AD"),"DISAB", LocalDate.of(2010,06,23), LocalDate.of(2010,06,26));
-        assertThat(list).extracting(o->o.getOffenderBooking().getOffender().getNomsId(), o-> o.getProblemType().getCode()).containsOnly(tuple("A1234AD","DISAB"));
+    void findAllByOffenderBookingOffenderNomsIdInAndOffenderBookingBookingSequenceAndProblemTypeCodeAndStartDateAfterAndStartDateBefore() {
+        final var list = repository.findAllByOffenderBookingOffenderNomsIdInAndOffenderBookingBookingSequenceAndProblemTypeCodeAndStartDateAfterAndStartDateBefore( List.of("A1234AD"),1, "DISAB", LocalDate.of(2010, 06, 23), LocalDate.of(2010, 06, 26));
+        assertThat(list).extracting(o -> o.getOffenderBooking().getOffender().getNomsId(), o -> o.getProblemType().getCode()).containsOnly(tuple("A1234AD", "DISAB"));
 
     }
 
