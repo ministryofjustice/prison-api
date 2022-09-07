2044e3e19f7955e70c1e30a06b5198a370a85c12
Peter Phillips <peter@greenthistle.co.uk>
2022-09-07 13:01:36 +0100

    SDI-365: ⬆️  Upgrade plugin to latest to suppress snakeyaml vuln and upgrade app insights

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.4.3-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.0-beta"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
   kotlin("plugin.lombok") version "1.7.10"
