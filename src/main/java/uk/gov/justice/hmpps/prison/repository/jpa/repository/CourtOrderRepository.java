4237d1cfc337ac5e44609498ad71f1190b5e990a
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-10-24 15:24:05 +0100

    MDWFP-23 fixing error.

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/CourtOrderRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/CourtOrderRepository.java
@@ -5,5 +5,5 @@ import uk.gov.justice.hmpps.prison.repository.jpa.model.CourtOrder;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.SentenceTerm;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.SentenceTerm.PK;
 
-public interface CourtOrderRepository extends CrudRepository<CourtOrder, PK> {
+public interface CourtOrderRepository extends CrudRepository<CourtOrder, Long> {
 }
