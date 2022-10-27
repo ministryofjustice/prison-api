fd3da76f95aa22ea2060124f17f786f3ea5cfa7d
Luke Harper <luke.harper@digital.justice.gov.uk>
2022-10-27 12:04:55 +0100

    MDWFP-23 adding court events.

--- src/main/java/uk/gov/justice/hmpps/prison/service/EntityNotFoundException.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/EntityNotFoundException.java
@@ -4,9 +4,18 @@ import java.util.function.Supplier;
 
 public class EntityNotFoundException extends RuntimeException implements Supplier<EntityNotFoundException> {
     private static final String DEFAULT_MESSAGE_FOR_ID_FORMAT = "Resource with id [%s] not found.";
+    private static final String DEFAULT_MESSAGE_FOR_ID_AND_CLASS_FORMAT = "[%s] with id [%s] not found.";
 
     public static EntityNotFoundException withId(final long id) {
         return withId(String.valueOf(id));
+    }
+
+    public static EntityNotFoundException withIdAndClass(final long id, final Class<?> clazz) {
+        return withIdAndClass(String.valueOf(id), clazz);
+    }
+
+    public static EntityNotFoundException withIdAndClass(final String id, final Class<?> clazz) {
+        return new EntityNotFoundException(String.format(DEFAULT_MESSAGE_FOR_ID_AND_CLASS_FORMAT, clazz, id));
     }
 
     public static EntityNotFoundException withId(final String id) {
