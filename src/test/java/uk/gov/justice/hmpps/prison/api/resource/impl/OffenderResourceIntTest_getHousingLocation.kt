cf9cb575b11744dc6513da1cdf7658a448bf9a47
Peter Phillips <peter@greenthistle.co.uk>
2022-11-22 15:48:44 +0000

    SDI-444: ✨ Add in new endpoint to return split of housing locations

---
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/OffenderResourceIntTest_getHousingLocation.kt
@@ -0,0 +1,77 @@
+@file:Suppress("ClassName")
+
+package uk.gov.justice.hmpps.prison.api.resource.impl
+
+import net.minidev.json.JSONArray
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import uk.gov.justice.hmpps.prison.service.OffenderLocation
+import uk.gov.justice.hmpps.prison.util.builders.OffenderBookingBuilder
+import uk.gov.justice.hmpps.prison.util.builders.OffenderBuilder
+
+@DisplayName("GET /offenders/{offenderNo}/housing-location")
+class OffenderResourceIntTest_getHousingLocation : ResourceTest() {
+  @Test
+  internal fun `404 when offender not found`() {
+    val offenderNo = "Z9999ZZ"
+
+    webTestClient.get()
+      .uri("/api/offenders/{offenderNo}/housing-location", offenderNo)
+      .headers(
+        setAuthorisation(
+          listOf("ROLE_SYSTEM_USER")
+        )
+      )
+      .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isNotFound
+  }
+
+  @Test
+  internal fun `empty response when offender not in prison`() {
+    val offenderNo =
+      OffenderBuilder().withBooking(OffenderBookingBuilder(released = true)).save(testDataContext).offenderNo
+
+    val location = webTestClient.get()
+      .uri("/api/offenders/{offenderNo}/housing-location", offenderNo)
+      .headers(
+        setAuthorisation(
+          listOf("ROLE_SYSTEM_USER")
+        )
+      )
+      .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .returnResult(OffenderLocation::class.java)
+      .responseBody.blockFirst()!!
+
+    assertThat(location).isEqualTo(OffenderLocation())
+  }
+
+  @Test
+  internal fun `response with levels for offender`() {
+    val offenderNo = OffenderBuilder().withBooking(
+      OffenderBookingBuilder(cellLocation = "LEI-A-1-10", prisonId = "LEI")
+    ).save(testDataContext).offenderNo
+
+    val location = webTestClient.get()
+      .uri("/api/offenders/{offenderNo}/housing-location", offenderNo)
+      .headers(
+        setAuthorisation(
+          listOf("ROLE_SYSTEM_USER")
+        )
+      )
+      .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("levels[*].level").isEqualTo(JSONArray().also { it.addAll(listOf(1, 2, 3)) })
+      .jsonPath("levels[*].code").isEqualTo(JSONArray().also { it.addAll(listOf("A", "1", "10")) })
+      .jsonPath("levels[*].type").isEqualTo(JSONArray().also { it.addAll(listOf("WING", "LAND", "CELL")) })
+  }
+}
