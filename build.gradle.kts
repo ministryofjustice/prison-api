f6ca3593a7a2a7b8455208608cc33de837d4fc38
Peter Phillips <peter@greenthistle.co.uk>
2023-03-03 12:29:12 +0000

    SDIT-513: ⬆️  Upgrade to latest to fix owasp issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.1-beta-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.1-beta-3"
   kotlin("plugin.spring") version "1.8.10"
   kotlin("plugin.jpa") version "1.8.10"
   kotlin("plugin.lombok") version "1.8.10"
