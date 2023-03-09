e78748e76a1f8f11a1276c1fbd8f4d93c1943c01
ldlharper <luke.harper@digital.justice.gov.uk>
2023-03-09 16:13:15 +0000

    MDWFP-84 filtering out bad data with the court event charges table. (#1524)
    * MDWFP-84 Filtering out bad data in NOMIS.
    
    * MDWFP-84 formatting.
    
    * MDWFP-84 adding comment explaining query.

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/CourtEventChargeRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/CourtEventChargeRepository.java
@@ -8,13 +8,19 @@ import java.util.List;
 
 public interface CourtEventChargeRepository extends CrudRepository<CourtEventCharge, CourtEventCharge.Pk> {
 
+    /*
+        Find all court event charges for a given offender.
+        This also filters out data where there is missing court event data, caused by a
+        NOMIS bug where orphan records were left when deleting court data.
+     */
     @Query("select " +
         "cec " +
         "from CourtEventCharge cec " +
         "join cec.eventAndCharge.offenderCharge oc " +
         "join oc.offenderBooking ob " +
         "join ob.offender o " +
-        "where o.nomsId = :offenderId"
+        "where o.nomsId = :offenderId " +
+        "and exists (select id from CourtEvent where id = cec.eventAndCharge.courtEvent.id)"
     )
     List<CourtEventCharge> findByOffender(String offenderId);
 }
