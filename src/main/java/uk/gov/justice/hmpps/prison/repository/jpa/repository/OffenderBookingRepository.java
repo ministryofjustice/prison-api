64146cc6925f57f52ad43ee73efe4185e2394406
Peter Phillips <peter@greenthistle.co.uk>
2023-03-10 10:15:16 +0000

    Revert "Revert "SDIT-645: 🐛 Attempt to prevent duplicate bookings by grabbing lock on select (#1526)" (#1528)"
    This reverts commit cb60d7a24201d29bb03c3c86e035ad2e453ca054.

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderBookingRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderBookingRepository.java
@@ -1,11 +1,15 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderBooking;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +17,10 @@ public interface OffenderBookingRepository extends
     PagingAndSortingRepository<OffenderBooking, Long>,
     JpaSpecificationExecutor<OffenderBooking>,
     CrudRepository<OffenderBooking, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT ob from OffenderBooking ob where ob.offender.nomsId = :nomsId")
+    List<OffenderBooking> findAllByOffenderNomsIdForUpdate(String nomsId);
 
     Optional<OffenderBooking> findByOffenderNomsIdAndActive(String nomsId, boolean active);
     Optional<OffenderBooking> findByOffenderNomsIdAndBookingSequence(String nomsId, Integer bookingSequence);
