c321cc1e4b0ae99f6eaf8b9c83d50f54301faa3a
Peter Nikitin <peter.nikitin@digital.justice.gov.uk>
2023-04-26 11:09:05 +0100

    NN-5036 remove unused code

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OicSanctionRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OicSanctionRepository.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public interface OicSanctionRepository extends CrudRepository<OicSanction, PK> {
 
-    @Query(value = "SELECT * FROM OFFENDER_OIC_SANCTIONS WHERE OIC_HEARING_ID = :oicHearingId", nativeQuery = true)
     List<OicSanction> findByOicHearingId(Long oicHearingId);
 
     @Query(value = "SELECT NVL(MAX(SANCTION_SEQ)+1, 0) FROM OFFENDER_OIC_SANCTIONS oos WHERE OFFENDER_BOOK_ID = :offenderBookId", nativeQuery = true)
