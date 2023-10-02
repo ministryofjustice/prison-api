2d0d0c3f151ccb7b490dab09d55a317f3389c40c
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-10-02 10:22:57 +0100

    SDIT-1109 Remove default overrideRole of SYSTEM_USER

--- src/main/java/uk/gov/justice/hmpps/prison/security/AuthenticationFacade.java
+++ src/main/java/uk/gov/justice/hmpps/prison/security/AuthenticationFacade.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.hmpps.prison.web.config.AuthAwareAuthenticationToken;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -55,8 +56,7 @@ public class AuthenticationFacade {
     }
 
     public boolean isOverrideRole(final String... overrideRoles) {
-        final var roles = overrideRoles.length > 0 ? getRoles(overrideRoles) : List.of("SYSTEM_USER");
-        return hasMatchingRole(roles, getAuthentication());
+        return hasMatchingRole(getRoles(overrideRoles), getAuthentication());
     }
 
     private static boolean hasMatchingRole(final List<String> roles, final Authentication authentication) {
