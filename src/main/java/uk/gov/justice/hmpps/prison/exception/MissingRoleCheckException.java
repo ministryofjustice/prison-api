e255aa398d6a0d54e9807cd21f2b6806c9e5db35
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-10-09 15:41:33 +0100

    SDIT-1125 Use kotlin file for exception

--- src/main/java/uk/gov/justice/hmpps/prison/exception/MissingRoleCheckException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.hmpps.prison.exception;
-
-@SuppressWarnings("serial")
-public class MissingRoleCheckException extends RuntimeException {
-
-    public MissingRoleCheckException(final String s) {
-        super(s);
-    }
-}
