0cd6c888403097cad603ac1ac8b286f0230ae60d
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-10-09 10:11:19 +0100

    SDIT-1125 Upgrade VerifyAccess files to kotlin

---
+++ src/main/java/uk/gov/justice/hmpps/prison/security/VerifyOffenderAccess.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.hmpps.prison.security
+
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.FUNCTION
+import kotlin.annotation.AnnotationTarget.PROPERTY_GETTER
+import kotlin.annotation.AnnotationTarget.PROPERTY_SETTER
+
+@Target(FUNCTION, PROPERTY_GETTER, PROPERTY_SETTER)
+@Retention(RUNTIME)
+annotation class VerifyOffenderAccess(val overrideRoles: Array<String>)
