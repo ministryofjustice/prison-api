c4306bc7150139ec54c05fa5e7126a999e6c7ffc
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-10-13 13:43:33 +0100

    CRS-1500 change structure to match other endpoints

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/calculation/CalculableSentenceEnvelope.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/calculation/CalculableSentenceEnvelope.kt
@@ -1,20 +1,16 @@
 package uk.gov.justice.hmpps.prison.api.model.calculation
 
 import io.swagger.v3.oas.annotations.media.Schema
-import uk.gov.justice.hmpps.prison.api.model.BookingAdjustment
-import uk.gov.justice.hmpps.prison.api.model.FixedTermRecallDetails
-import uk.gov.justice.hmpps.prison.api.model.OffenderFinePaymentDto
-import uk.gov.justice.hmpps.prison.api.model.SentenceAdjustmentValues
-import uk.gov.justice.hmpps.prison.api.model.SentenceCalcDates
-import uk.gov.justice.hmpps.prison.api.model.SentenceSummary
+import uk.gov.justice.hmpps.prison.api.model.*
 
 @Schema(description = "The active sentence envelope is a combination of the person information, the active booking and calculable sentences at a particular establishment")
 data class CalculableSentenceEnvelope(
   @Schema(description = "The identifiers of a person necessary for a calculation")
   val person: Person,
-
-  @Schema(description = "Most recent term in prison")
-  val latestPrisonTerm: SentenceSummary.PrisonTerm? = null,
+  @Schema(description = "The booking ID")
+  val bookingId: Long,
+  @Schema(description = "Sentence and offence details  for a prisoner")
+  val sentenceAndOffences: List<OffenderSentenceAndOffences> = listOf(),
 
   @Schema(description = "Adjustments at a sentence level")
   val sentenceAdjustments: List<SentenceAdjustmentValues> = listOf(),
@@ -31,4 +27,4 @@ data class CalculableSentenceEnvelope(
   @Schema(description = "The current set of sentence dates determined by NOMIS or recorded via overrides")
   val sentenceCalcDates: SentenceCalcDates? = null,
 
-)
+  )
