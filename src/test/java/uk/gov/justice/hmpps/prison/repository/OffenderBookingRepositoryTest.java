c96c7711324f6a10856435ec09c070173cdccd27
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2023-10-20 16:58:19 +0100

    clean up imports

--- src/test/java/uk/gov/justice/hmpps/prison/repository/OffenderBookingRepositoryTest.java
+++ src/test/java/uk/gov/justice/hmpps/prison/repository/OffenderBookingRepositoryTest.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.hmpps.prison.repository;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
