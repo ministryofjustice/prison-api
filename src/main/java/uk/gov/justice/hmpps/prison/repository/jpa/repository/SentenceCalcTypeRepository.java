d8ed5c94bee4c29cb4882da0ff44fbefd69ad5ba
carloveo-moj <144013256+carloveo-moj@users.noreply.github.com>
2023-10-23 09:11:33 +0100

    CRS-1500 active calculable booking (#1783)
    * CRS-1500 made sure bookings were only retrieved when an active calculable sentence is linked
    
    * CRS-1500 corrected test setup
    
    * Update src/test/java/uk/gov/justice/hmpps/prison/repository/OffenderBookingRepositoryTest.java
    
    Co-authored-by: petergphillips <peter@greenthistle.co.uk>
    
    * clean up imports
    
    ---------
    
    Co-authored-by: petergphillips <peter@greenthistle.co.uk>

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/SentenceCalcTypeRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/SentenceCalcTypeRepository.java
@@ -1,14 +1,7 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.repository;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.SentenceCalcType;
 
-import java.util.List;
-
 public interface SentenceCalcTypeRepository extends CrudRepository<SentenceCalcType, SentenceCalcType.PK> {
-
-    @Cacheable("calculableSentenceCalculationTypes")
-    List<SentenceCalcType> findByCalculationTypeNotContainingAndCategoryIsNot(String excludeCalcTypeContains, String excludeCategory);
-
 }
