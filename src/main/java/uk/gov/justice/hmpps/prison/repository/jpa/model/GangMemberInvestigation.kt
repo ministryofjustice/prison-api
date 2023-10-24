c0cfeb7461616e6d936b49ffeee82a47a6db1c17
mjwillis <michael.willis@digital.justice.gov.uk>
2023-10-24 15:57:42 +0100

    NON-280 Add members and investigations mappings

---
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/GangMemberInvestigation.kt
@@ -0,0 +1,81 @@
+package uk.gov.justice.hmpps.prison.repository.jpa.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.IdClass
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.Hibernate
+import org.hibernate.annotations.JoinColumnOrFormula
+import org.hibernate.annotations.JoinColumnsOrFormulas
+import org.hibernate.annotations.JoinFormula
+import uk.gov.justice.hmpps.prison.repository.jpa.helper.EntityOpen
+import java.io.Serializable
+
+@Entity
+@Table(name = "OFFENDER_GANG_INVESTS")
+@EntityOpen
+@IdClass(GangMemberInvestigationId::class)
+class GangMemberInvestigation(
+
+  @Id
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "GANG_CODE", nullable = false)
+  val gang: Gang,
+
+  @Id
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "OFFENDER_BOOK_ID", nullable = false)
+  val booking: OffenderBooking,
+
+  @Id
+  @Column(name = "INVESTIGATE_SEQ", nullable = false)
+  val sequence: Int = 1,
+
+  @ManyToOne(optional = false)
+  @JoinColumnsOrFormulas(
+    value = [
+      JoinColumnOrFormula(
+        formula = JoinFormula(
+          value = "'" + MembershipStatus.DOMAIN + "'",
+          referencedColumnName = "domain",
+        ),
+      ),
+      JoinColumnOrFormula(column = JoinColumn(name = "GANG_MBR_STS", referencedColumnName = "code")),
+    ],
+  )
+  val membershipStatus: MembershipStatus,
+
+  @Column(name = "COMMENT_TEXT")
+  val commentText: String? = null,
+
+)
+
+class GangMemberInvestigationId(
+  val gang: Gang? = null,
+  val booking: OffenderBooking? = null,
+  val sequence: Int = 1,
+
+) : Serializable {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+
+    other as GangMemberInvestigationId
+
+    if (gang != other.gang) return false
+    if (booking != other.booking) return false
+    if (sequence != other.sequence) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = gang?.hashCode() ?: 0
+    result = 31 * result + (booking?.hashCode() ?: 0)
+    result = 31 * result + sequence
+    return result
+  }
+}
--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/GangNonAssociationRepository.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/GangNonAssociationRepository.kt
@@ -6,4 +6,13 @@ import uk.gov.justice.hmpps.prison.repository.jpa.model.GangNonAssociation
 import uk.gov.justice.hmpps.prison.repository.jpa.model.GangNonAssociationId
 
 @Repository
-interface GangNonAssociationRepository : CrudRepository<GangNonAssociation, GangNonAssociationId>
+interface GangNonAssociationRepository : CrudRepository<GangNonAssociation, GangNonAssociationId> {
+
+  fun findAllByPrimaryGangCodeOrSecondaryGangCode(primaryGangCode: String, secondaryGangCode: String): List<GangNonAssociation>
+
+  /**
+   * Returns all gang non-associations where the given gang code is either primary OR secondary in the non-association
+   */
+  fun findAllByGangCode(gangCode: String): List<GangNonAssociation> =
+    findAllByPrimaryGangCodeOrSecondaryGangCode(gangCode, gangCode)
+}
