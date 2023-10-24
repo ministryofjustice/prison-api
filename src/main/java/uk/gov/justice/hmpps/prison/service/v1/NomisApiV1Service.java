468b14fa96b4c2116453f6721e16820b3ad6aec1
steverendell <steve.rendell@syscon.net>
2023-10-24 15:48:35 +0100

    SDIT-1176 NomisApiV1 and OffenderFinePayment

--- src/main/java/uk/gov/justice/hmpps/prison/service/v1/NomisApiV1Service.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/v1/NomisApiV1Service.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
