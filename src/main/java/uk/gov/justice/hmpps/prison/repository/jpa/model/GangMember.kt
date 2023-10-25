de8e89e1d7f2e8db849cbecf6265c78324147440
mjwillis <michael.willis@digital.justice.gov.uk>
2023-10-25 14:38:27 +0100

    NON-280 Add service to find gangs with non-associations

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/GangMember.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/GangMember.kt
@@ -1,6 +1,13 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Id
+import jakarta.persistence.IdClass
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 import org.hibernate.Hibernate
 import uk.gov.justice.hmpps.prison.repository.jpa.helper.EntityOpen
 import java.io.Serializable
