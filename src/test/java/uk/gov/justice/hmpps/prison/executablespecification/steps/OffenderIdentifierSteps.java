f880674bdcb4a144b044d53f62738ae7b328ee40
steverendell <steve.rendell@syscon.net>
2023-10-31 12:51:14 +0000

    SDIT-1175 IdentifiersResource and IncidentsResource

---
+++ src/test/java/uk/gov/justice/hmpps/prison/executablespecification/steps/OffenderIdentifierSteps.java
@@ -0,0 +1,20 @@
+package uk.gov.justice.hmpps.prison.executablespecification.steps;
+
+import net.thucydides.core.annotations.Step;
+import uk.gov.justice.hmpps.prison.api.model.OffenderIdentifier;
+
+import java.util.List;
+
+public class OffenderIdentifierSteps extends CommonSteps {
+    private List<OffenderIdentifier> actualIdentifiers;
+
+    @Step("Verify identifier booking Ids")
+    public void verifyIdentifierBookingIds(final String bookingIds) {
+        verifyLongValues(actualIdentifiers, OffenderIdentifier::getBookingId, bookingIds);
+    }
+
+    @Step("Verify identifier offender Nos")
+    public void verifyIdentifierOffenderNos(final String offenderNos) {
+        verifyPropertyValues(actualIdentifiers, OffenderIdentifier::getOffenderNo, offenderNos);
+    }
+}
