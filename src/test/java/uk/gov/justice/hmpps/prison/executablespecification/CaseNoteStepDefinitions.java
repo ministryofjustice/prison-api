74102a32d3966b64384ba297671adfe95e62e348
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-11-02 12:42:23 +0000

    SDIT-1205 remove unused steps

--- src/test/java/uk/gov/justice/hmpps/prison/executablespecification/CaseNoteStepDefinitions.java
+++ src/test/java/uk/gov/justice/hmpps/prison/executablespecification/CaseNoteStepDefinitions.java
@@ -1,26 +1,16 @@
 package uk.gov.justice.hmpps.prison.executablespecification;
 
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import uk.gov.justice.hmpps.prison.api.model.NewCaseNote;
 import uk.gov.justice.hmpps.prison.executablespecification.steps.CaseNoteSteps;
-import uk.gov.justice.hmpps.prison.util.DateTimeConverter;
-
-import java.time.ZoneOffset;
-import java.util.List;
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * BDD step definitions for case note related Booking API endpoints:
+ * BDD step definitions for case note related Case Note API endpoints:
  * <ul>
- *     <li>/booking/{bookingId}/caseNotes</li>
- *     <li>/booking/{bookingId}/caseNotes/{caseNoteId}</li>
+ *     <li>/api/case-notes/usage</li>
+ *     <li>/api/case-notes/staff-usage</li>
  * </ul>
  * <p>
  * NB: Not all API endpoints have associated tests at this point in time.
@@ -35,39 +25,9 @@ public class CaseNoteStepDefinitions extends AbstractStepDefinitions {
         caseNote.init();
     }
 
-    @Then("^\"([^\"]*)\" case notes are returned$")
-    public void caseNotesAreReturned(final String count) throws Throwable {
-        caseNote.verifyResourceRecordsReturned(Long.valueOf(count));
-    }
-
-    @And("^pagination with limit \"([0-9]*)\" and offset \"([0-9]*)\" applied$")
-    public void paginationWithLimitAndOffsetApplied(final Long limit, final Long offset) throws Throwable {
-        caseNote.applyPagination(offset, limit);
-    }
-
-    @And("^pagination with size \"([0-9]*)\" and page number \"([0-9]*)\" applied$")
-    public void paginationWithPageNumberAndSizeApplied(final Long size, final Long pageNumber) throws Throwable {
-        caseNote.applyPageNumberAndSize(pageNumber, size);
-    }
-
-    @And("^\"([^\"]*)\" case notes are available$")
-    public void caseNotesAreAvailable(final String count) throws Throwable {
-        caseNote.verifyTotalResourceRecordsAvailable(Long.valueOf(count));
-    }
-
-    @Then("^resource not found response is received from casenotes API")
-    public void caseNotesVerifyResourceNotFound() throws Throwable {
-        caseNote.verifyResourceNotFound();
-    }
-
     @Then("^case note size is \"([^\"]*)\"$")
     public void caseNoteSizeIs(final String size) throws Throwable {
         caseNote.verifyCaseNoteUsageSize(Integer.valueOf(size));
-    }
-
-    @Then("^case note staff usage size is \"([^\"]*)\"$")
-    public void caseNoteStaffUsageSizeIs(final String size) throws Throwable {
-        caseNote.verifyCaseNoteStaffUsageSize(Integer.valueOf(size));
     }
 
     @Then("^case note usage response \"([^\"]*)\" is \"([^\"]*)\"$")
@@ -78,17 +38,6 @@ public class CaseNoteStepDefinitions extends AbstractStepDefinitions {
     @Then("^case note staff usage response \"([^\"]*)\" is \"([^\"]*)\"$")
     public void caseNoteStaffUsageResponseIs(final String propertyName, final String expectedValue) throws Throwable {
         caseNote.verifyCaseNoteStaffUsagePropertyValue(propertyName, expectedValue);
-    }
-
-
-    @Then("^bad request response, with \"([^\"]*)\" message, is received from casenotes API$")
-    public void badRequestResponseWithMessageIsReceivedFromCasenotesAPI(final String expectedUserMessage) throws Throwable {
-        caseNote.verifyBadRequest(expectedUserMessage);
-    }
-
-    @Then("^access denied response, with \"([^\"]*)\" message, is received from booking case notes API$")
-    public void accessDeniedResponseWithMessageIsReceivedFromBookingCaseNotesAPI(final String userMessage) throws Throwable {
-        caseNote.verifyAccessDenied(userMessage);
     }
 
     @When("^case note usage between \"([^\"]*)\" and \"([^\"]*)\" is requested of offender No \"([^\"]*)\" for case note type \"([^\"]*)\" and sub-type \"([^\"]*)\"$")
