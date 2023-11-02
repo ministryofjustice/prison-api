d4563bb674e11373ccecdf83417b44febf9f4462
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-11-02 13:24:25 +0000

    SDIT-1195 PrisonerStatusController and /offenders/ids (#1809)
    * SDIT-1195 PrisonerStatusController and /offenders/ids
    
    * SDIT-1195 PrisonerStatusController and /offenders/ids
    
    ---------
    
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/java/uk/gov/justice/hmpps/prison/service/PrisonerInformationService.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/PrisonerInformationService.java
@@ -1,11 +1,7 @@
 package uk.gov.justice.hmpps.prison.service;
 
-import com.google.common.collect.Lists;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.prepost.PreAuthorize;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.justice.hmpps.prison.api.model.PrisonerInformation;
@@ -13,15 +9,11 @@ import uk.gov.justice.hmpps.prison.repository.InmateRepository;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.ImprisonmentStatus;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.PrisonerStatusInformation;
 import uk.gov.justice.hmpps.prison.repository.jpa.repository.PrisonerStatusInformationRepository;
-import uk.gov.justice.hmpps.prison.security.VerifyOffenderAccess;
 import uk.gov.justice.hmpps.prison.service.support.InmatesHelper;
 import uk.gov.justice.hmpps.prison.service.support.LocationProcessor;
 
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.lang.String.format;
 
@@ -32,21 +24,17 @@ public class PrisonerInformationService {
     private final PrisonerStatusInformationRepository prisonerStatusInformationRepository;
     private final BookingService bookingService;
     private final InmateRepository repository;
-    private final int maxBatchSize;
 
     public PrisonerInformationService(
                                       final PrisonerStatusInformationRepository prisonerStatusInformationRepository,
                                       final BookingService bookingService,
-                                      final InmateRepository repository,
-                                      @Value("${batch.max.size:1000}") final int maxBatchSize) {
+                                      final InmateRepository repository) {
 
         this.prisonerStatusInformationRepository = prisonerStatusInformationRepository;
         this.bookingService = bookingService;
         this.repository = repository;
-        this.maxBatchSize = maxBatchSize;
     }
 
-    @VerifyOffenderAccess(overrideRoles = {"SYSTEM_USER", "GLOBAL_SEARCH", "VIEW_PRISONER_DATA"})
     public PrisonerInformation getPrisonerInformationById(final @NotNull String offenderNo) {
         final var entity = prisonerStatusInformationRepository.getByNomsId(offenderNo).orElseThrow(() -> new EntityNotFoundException(format("Resource with id [%s] not found.", offenderNo)));
 
