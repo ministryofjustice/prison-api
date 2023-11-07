4db8d225df972c91587465e2fe473b4fdc2e4808
steverendell <steve.rendell@syscon.net>
2023-11-07 10:23:46 +0000

    SDIT-1189 OffenceResource
    Remove unused endpoints

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenceRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenceRepository.java
@@ -4,22 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.HOCode;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.Offence;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.Offence.PK;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.Statute;
 
 public interface OffenceRepository extends CrudRepository<Offence, PK> {
-    Page<Offence> findAll(Pageable pageable);
-
-    Page<Offence> findAllByActive(final boolean active, Pageable pageable);
-
-    Page<Offence> findAllByDescriptionLike(final String description, Pageable pageable);
-
-    Page<Offence> findAllByHoCode(final HOCode hoCode, Pageable pageable);
-
-    Page<Offence> findAllByStatute(final Statute statute, Pageable pageable);
-
     @EntityGraph(value = "offence-entity-graph")
     Page<Offence> findAllByCodeStartsWithIgnoreCase(final String startsWith, Pageable pageable);
 }
