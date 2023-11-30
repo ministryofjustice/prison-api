37b73666b1bca870e90e1862ef8fda4735a3a7f4
karenmillermoj <karen.miller@digital.justice.gov.uk>
2023-11-30 09:49:34 +0000

    SDIT-1282 Remove SYSTEM_USER from /api/bookings/offenderNo/{offenderNo}/image/data

--- src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/BookingResourceIntTest_getImageData.kt
+++ src/test/java/uk/gov/justice/hmpps/prison/api/resource/impl/BookingResourceIntTest_getImageData.kt
@@ -24,14 +24,6 @@ class BookingResourceIntTest_getImageData : ResourceTest() {
   }
 
   @Test
-  fun `should return success when has SYSTEM_USER override role`() {
-    webTestClient.get().uri("/api/bookings/offenderNo/A1234AA/image/data")
-      .headers(setClientAuthorisation(listOf("ROLE_SYSTEM_USER")))
-      .exchange()
-      .expectStatus().isOk
-  }
-
-  @Test
   fun `should return success when has VIEW_PRISONER_DATA override role`() {
     webTestClient.get().uri("/api/bookings/offenderNo/A1234AA/image/data")
       .headers(setClientAuthorisation(listOf("ROLE_VIEW_PRISONER_DATA")))
