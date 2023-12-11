ca1623ed6c09483d95840028a3dacdfa5fa95704
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2023-12-11 13:49:25 +0000

    SDIT-1312 Remove unused relationships endpoints (#1937)

--- src/main/java/uk/gov/justice/hmpps/prison/repository/ContactRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/ContactRepository.java
@@ -1,27 +1,15 @@
 package uk.gov.justice.hmpps.prison.repository;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.SqlParameterValue;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.hmpps.prison.api.model.Contact;
-import uk.gov.justice.hmpps.prison.api.model.Person;
-import uk.gov.justice.hmpps.prison.api.model.PersonDto;
-import uk.gov.justice.hmpps.prison.repository.mapping.DataClassByColumnRowMapper;
 import uk.gov.justice.hmpps.prison.repository.sql.ContactRepositorySql;
 import uk.gov.justice.hmpps.prison.util.DateTimeConverter;
 
-import java.sql.Types;
-import java.time.LocalDate;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class ContactRepository extends RepositoryBase {
-
-    private static final RowMapper<PersonDto> PERSON_ROW_MAPPER = new DataClassByColumnRowMapper<>(PersonDto.class);
 
     private static final RowMapper<Contact> CONTACT_ROW_MAPPER = (rs, rowNum) -> Contact.builder()
             .relationshipId(rs.getLong("RELATIONSHIP_ID"))
@@ -46,65 +34,6 @@ public class ContactRepository extends RepositoryBase {
             .createDateTime(DateTimeConverter.toISO8601LocalDateTime(rs.getObject("CREATE_DATETIME")))
             .build();
 
-
-    public Long createPerson(final String firstName, final String lastName) {
-
-        final var sql = ContactRepositorySql.CREATE_PERSON.getSql();
-        final var generatedKeyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(
-                sql,
-                createParams("firstName", firstName, "lastName", lastName),
-                generatedKeyHolder,
-                new String[]{"PERSON_ID"});
-
-        return generatedKeyHolder.getKey().longValue();
-    }
-
-
-    public void updatePerson(final Long personId, final String firstName, final String lastName) {
-
-        final var sql = ContactRepositorySql.UPDATE_PERSON.getSql();
-        jdbcTemplate.update(
-                sql,
-                createParams("personId", personId, "firstName", firstName, "lastName", lastName));
-
-    }
-
-
-    public Optional<Person> getPersonById(final Long personId) {
-        final var sql = ContactRepositorySql.GET_PERSON_BY_ID.getSql();
-
-        PersonDto person;
-        try {
-            person = jdbcTemplate.queryForObject(sql, createParams("personId", personId), PERSON_ROW_MAPPER);
-        } catch (final EmptyResultDataAccessException e) {
-            person = null;
-        }
-        return Optional.ofNullable(person).map(PersonDto::toPerson);
-    }
-
-
-    public Optional<Person> getPersonByRef(final String externalRef, final String identifierType) {
-        final var sql = ContactRepositorySql.GET_PERSON_BY_REF.getSql();
-        final var persons = jdbcTemplate.query(sql,
-                createParams("identifierType", identifierType,
-                        "identifier", externalRef), PERSON_ROW_MAPPER);
-
-        return persons.stream().min(Comparator.comparing(PersonDto::getPersonId)).map(PersonDto::toPerson);
-    }
-
-
-    public void createExternalReference(final Long personId, final String externalRef, final String identifierType) {
-        final var sql = ContactRepositorySql.CREATE_PERSON_IDENTIFIER.getSql();
-        jdbcTemplate.update(
-                sql,
-                createParams("personId", personId,
-                        "identifierType", identifierType,
-                        "identifier", externalRef));
-    }
-
-
     public List<Contact> getOffenderRelationships(final Long bookingId, final String relationshipType) {
         final var sql = ContactRepositorySql.RELATIONSHIP_TO_OFFENDER.getSql();
 
@@ -114,48 +43,4 @@ public class ContactRepository extends RepositoryBase {
                 CONTACT_ROW_MAPPER);
     }
 
-
-    public Optional<Contact> getOffenderRelationship(final Long relationshipId) {
-        final var sql = ContactRepositorySql.RELATIONSHIP_TO_OFFENDER_BY_ID.getSql();
-
-        return Optional.ofNullable(jdbcTemplate.queryForObject(sql,
-                createParams("relationshipId", relationshipId),
-                CONTACT_ROW_MAPPER));
-    }
-
-
-    public Long createRelationship(final Long personId, final Long bookingId, final String relationshipType, final String contactType) {
-        final var sql = ContactRepositorySql.CREATE_OFFENDER_CONTACT_PERSONS.getSql();
-        final var generatedKeyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(
-                sql,
-                createParams("bookingId", bookingId,
-                        "personId", personId,
-                        "contactType", contactType,
-                        "relationshipType", relationshipType,
-                        "emergencyContactFlag", "N",  //TODO: allow these to be controlled from service in future iterations
-                        "nextOfKinFlag", "N",
-                        "activeFlag", "Y"),
-                generatedKeyHolder,
-                new String[]{"OFFENDER_CONTACT_PERSON_ID"});
-
-        return generatedKeyHolder.getKey().longValue();
-    }
-
-
-    public void updateRelationship(final Long id, final Long personId, boolean active) {
-        final var sql = ContactRepositorySql.UPDATE_OFFENDER_CONTACT_PERSONS_SAME_REL_TYPE.getSql();
-
-        jdbcTemplate.update(
-                sql,
-                createParams(
-                        "bookingContactPersonId", id,
-                        "personId", personId,
-                        "activeFlag", active ? "Y" : "N",
-                        "expiryDate", new SqlParameterValue(Types.DATE, DateTimeConverter.toDate(active ? null : LocalDate.now()))
-                )
-        );
-
-    }
 }
--- src/main/java/uk/gov/justice/hmpps/prison/service/ContactService.java
+++ src/main/java/uk/gov/justice/hmpps/prison/service/ContactService.java
@@ -1,22 +1,16 @@
 package uk.gov.justice.hmpps.prison.service;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.justice.hmpps.prison.api.model.Contact;
 import uk.gov.justice.hmpps.prison.api.model.ContactDetail;
-import uk.gov.justice.hmpps.prison.api.model.OffenderRelationship;
-import uk.gov.justice.hmpps.prison.api.model.Person;
 import uk.gov.justice.hmpps.prison.repository.ContactRepository;
 import uk.gov.justice.hmpps.prison.security.VerifyBookingAccess;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
@@ -24,21 +18,11 @@ import static java.util.stream.Collectors.toList;
 @Service
 @Transactional(readOnly = true)
 public class ContactService {
-    public static final String EXTERNAL_REL = "EXTERNAL_REL";
-
-    private static final String OFFICIAL_CONTACT_TYPE = "O";  //TODO: Allow API to be specified in future work
-
     private final ContactRepository repository;
-    private final BookingService bookingService;
-    private final ReferenceDomainService referenceDomainService;
 
     @Autowired
-    public ContactService(final ContactRepository contactRepository,
-                          final BookingService bookingService,
-                          final ReferenceDomainService referenceDomainService) {
+    public ContactService(final ContactRepository contactRepository) {
         this.repository = contactRepository;
-        this.bookingService = bookingService;
-        this.referenceDomainService = referenceDomainService;
     }
 
     @VerifyBookingAccess(overrideRoles = {"SYSTEM_USER", "GLOBAL_SEARCH", "VIEW_PRISONER_DATA"})
@@ -58,77 +42,6 @@ public class ContactService {
                 .otherContacts(activeContactsMap.get(false).stream()
                         .sorted(sortCriteria)
                         .collect(toList())).build();
-    }
-
-    @VerifyBookingAccess(overrideRoles = {"SYSTEM_USER", "GLOBAL_SEARCH", "VIEW_PRISONER_DATA"})
-    public List<Contact> getRelationships(final Long bookingId, final String relationshipType, final boolean activeOnly) {
-        return repository.getOffenderRelationships(bookingId, relationshipType)
-                .stream()
-                .filter(r -> !activeOnly || r.isActiveFlag())
-                .toList();
-    }
-
-    @PreAuthorize("hasRole('CONTACT_CREATE')")
-    @Transactional
-    public Contact createRelationship(final Long bookingId, final OffenderRelationship relationshipDetail) {
-
-        // Check relationship type exists - TODO: Move to validator
-        referenceDomainService.getReferenceCodeByDomainAndCode("RELATIONSHIP", relationshipDetail.getRelationshipType(), false);
-
-        final var person = createPersonAndRef(relationshipDetail, relationshipDetail.getPersonId());
-
-        final var activeRel = new AtomicLong();
-
-        // now check the relationship exists already and mark active and turn others inactive.
-        getRelationships(bookingId, relationshipDetail.getRelationshipType(), false)
-                .forEach(rel -> {
-                    if (rel.getPersonId().equals(person.getPersonId()) && activeRel.get() == 0) {
-                        // set the contact with same person to active.
-                        if (!rel.isActiveFlag()) {
-                            repository.updateRelationship(rel.getRelationshipId(), rel.getPersonId(), true);
-                        }
-                        activeRel.set(rel.getRelationshipId());
-                    } else {
-                        // set all the others to inactive
-                        repository.updateRelationship(rel.getRelationshipId(), rel.getPersonId(), false);
-                    }
-                });
-
-        // if we didn't find a relationship for that person, all others are now inactive, create a new record with this person/offender relationship
-        if (activeRel.get() == 0) {
-            activeRel.set(repository.createRelationship(person.getPersonId(), bookingId, relationshipDetail.getRelationshipType(), OFFICIAL_CONTACT_TYPE));
-        }
-
-        return repository.getOffenderRelationship(activeRel.get()).orElseThrow(EntityNotFoundException.withId(bookingId));
-    }
-
-
-    private Person createPersonAndRef(final OffenderRelationship relationshipDetail, final Long personId) {
-        var foundRef = false;
-        Optional<Person> person = Optional.empty();
-        // check if person ref set
-        if (StringUtils.isNotBlank(relationshipDetail.getExternalRef())) {
-            person = repository.getPersonByRef(relationshipDetail.getExternalRef(), EXTERNAL_REL);
-            foundRef = person.isPresent();
-        } else if (personId != null) {
-            person = repository.getPersonById(personId);
-        }
-
-        final Long newPersonId;
-        // for now if person is null, create it!
-        if (person.isPresent()) {
-            newPersonId = person.get().getPersonId();
-            repository.updatePerson(newPersonId, relationshipDetail.getFirstName(), relationshipDetail.getLastName());
-        } else {
-            newPersonId = repository.createPerson(relationshipDetail.getFirstName(), relationshipDetail.getLastName());
-        }
-
-        // if the external ref was not found add as identifier
-        if (StringUtils.isNotBlank(relationshipDetail.getExternalRef()) && !foundRef) {
-            repository.createExternalReference(newPersonId, relationshipDetail.getExternalRef(), EXTERNAL_REL);
-        }
-
-        return repository.getPersonById(newPersonId).orElseThrow(EntityNotFoundException.withId(newPersonId));
     }
 
 }
