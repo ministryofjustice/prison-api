13184e7dce2464ecd4ea5b878bb8dd20ce5d5258
steverendell <steve.rendell@syscon.net>
2023-12-22 11:00:23 +0000

    SDIT-1384 StaffResource
    formatting only

--- src/main/java/uk/gov/justice/hmpps/prison/api/resource/StaffResource.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/resource/StaffResource.java
@@ -42,10 +42,10 @@ public class StaffResource {
     }
 
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = StaffDetail.class))}),
-            @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
+        @ApiResponse(responseCode = "200", description = "OK", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = StaffDetail.class))}),
+        @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
     @Operation(summary = "Staff detail.", description = "Staff detail.")
     @GetMapping("/{staffId}")
     public StaffDetail getStaffDetail(@PathVariable("staffId") @Parameter(description = "The staff id of the staff member.", required = true) final Long staffId) {
@@ -53,10 +53,10 @@ public class StaffResource {
     }
 
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "The staffId supplied was not valid.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "204", description = "No email addresses were found for this staff member."),
-            @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "404", description = "The staffId supplied was not valid.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "204", description = "No email addresses were found for this staff member."),
+        @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
     @Operation(summary = "Returns a list of email addresses associated with this staff user", description = "List of email addresses for a specified staff user")
     @GetMapping("/{staffId}/emails")
     public List<String> getStaffEmailAddresses(@PathVariable("staffId") @Parameter(description = "The staff id of the staff user.", required = true) final Long staffId) {
@@ -64,10 +64,10 @@ public class StaffResource {
     }
 
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "404", description = "The staffId supplied was not valid or not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "204", description = "No caseloads were found for this staff member."),
-            @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "404", description = "The staffId supplied was not valid or not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "204", description = "No caseloads were found for this staff member."),
+        @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
     @Operation(summary = "Returns a list of caseloads associated with this staff user", description = "List of caseloads for a specified staff user")
     @GetMapping("/{staffId}/caseloads")
     public List<CaseLoad> getStaffCaseloads(@PathVariable("staffId") @Parameter(description = "The staff id of the staff user.", required = true, example = "123123") final Long staffId) {
@@ -75,17 +75,24 @@ public class StaffResource {
     }
 
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
     @Operation(summary = "Get staff members within agency who are currently assigned the specified role.", description = "Get staff members within agency who are currently assigned the specified role.")
     @GetMapping("/roles/{agencyId}/role/{role}")
     @SlowReportQuery
     public ResponseEntity<List<StaffLocationRole>> getStaffByAgencyRole(
-            @PathVariable("agencyId") @Parameter(description = "The agency (prison) id.", required = true) final String agencyId, @PathVariable("role") @Parameter(description = "The staff role.", required = true) final String role, @RequestParam(value = "nameFilter", required = false) @Parameter(description = "Filter results by first name and/or last name of staff member. Supplied filter term is matched to start of staff member's first and last name.") final String nameFilter, @RequestParam(value = "staffId", required = false) @Parameter(description = "The staff id of a staff member.") final Long staffId, @RequestParam(value = "activeOnly", required = false, defaultValue = "true") @Parameter(description = "Filters results by activeOnly staff members.") final Boolean activeOnly,
-            @RequestHeader(value = "Page-Offset", defaultValue = "0", required = false) @Parameter(description = "Requested offset of first record in returned collection of role records.") final Long pageOffset, @RequestHeader(value = "Page-Limit", defaultValue = "10", required = false) @Parameter(description = "Requested limit to number of role records returned.") final Long pageLimit, @RequestHeader(value = "Sort-Fields", required = false) @Parameter(description = "Comma separated list of one or more of the following fields - <b>firstName, lastName</b>") final String sortFields, @RequestHeader(value = "Sort-Order", defaultValue = "ASC", required = false) @Parameter(description = "Sort order (ASC or DESC) - defaults to ASC.") final Order sortOrder) {
-
+        @PathVariable("agencyId") @Parameter(description = "The agency (prison) id.", required = true) final String agencyId,
+        @PathVariable("role") @Parameter(description = "The staff role.", required = true) final String role,
+        @RequestParam(value = "nameFilter", required = false) @Parameter(description = "Filter results by first name and/or last name of staff member. Supplied filter term is matched to start of staff member's first and last name.") final String nameFilter,
+        @RequestParam(value = "staffId", required = false) @Parameter(description = "The staff id of a staff member.") final Long staffId,
+        @RequestParam(value = "activeOnly", required = false, defaultValue = "true") @Parameter(description = "Filters results by activeOnly staff members.") final Boolean activeOnly,
+        @RequestHeader(value = "Page-Offset", defaultValue = "0", required = false) @Parameter(description = "Requested offset of first record in returned collection of role records.") final Long pageOffset,
+        @RequestHeader(value = "Page-Limit", defaultValue = "10", required = false) @Parameter(description = "Requested limit to number of role records returned.") final Long pageLimit,
+        @RequestHeader(value = "Sort-Fields", required = false) @Parameter(description = "Comma separated list of one or more of the following fields - <b>firstName, lastName</b>") final String sortFields,
+        @RequestHeader(value = "Sort-Order", defaultValue = "ASC", required = false) @Parameter(description = "Sort order (ASC or DESC) - defaults to ASC.") final Order sortOrder
+    ) {
         final var defaultedActiveOnly = activeOnly != null ? activeOnly : Boolean.TRUE;
 
         final var staffRoleRequest = new GetStaffRoleRequest(agencyId, null, role, nameFilter, defaultedActiveOnly, staffId);
@@ -94,18 +101,21 @@ public class StaffResource {
         final var staffDetails = staffService.getStaffByAgencyPositionRole(staffRoleRequest, pageRequest);
 
         return ResponseEntity.ok()
-                .headers(staffDetails.getPaginationHeaders())
-                .body(staffDetails.getItems());
+            .headers(staffDetails.getPaginationHeaders())
+            .body(staffDetails.getItems());
     }
 
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK"),
-            @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
-            @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
     @Operation(summary = "List of job roles for specified staff and agency Id", description = "List of job roles for specified staff and agency Id")
     @GetMapping("/{staffId}/{agencyId}/roles")
-    public List<StaffRole>  getAllRolesForAgency(@PathVariable("staffId") @Parameter(description = "The staff id of the staff member.", required = true) final Long staffId, @PathVariable("agencyId") @Parameter(description = "Agency Id.", required = true) final String agencyId) {
+    public List<StaffRole> getAllRolesForAgency(
+        @PathVariable("staffId") @Parameter(description = "The staff id of the staff member.", required = true) final Long staffId,
+        @PathVariable("agencyId") @Parameter(description = "Agency Id.", required = true) final String agencyId
+    ) {
         return staffService.getAllRolesForAgency(staffId, agencyId);
     }
 
@@ -115,18 +125,11 @@ public class StaffResource {
         @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
     @Operation(summary = "Check if staff member has a role", description = "Check if staff member has a role, either KW or POM")
     @GetMapping("/{staffId}/{agencyId}/roles/{roleType}")
-    public boolean hasStaffRole( @PathVariable("staffId")
-                                 @Parameter(description = "The staff id of the staff member.", required = true, example = "1111111")
-                                 @NotNull final Long staffId,
-                                 @PathVariable("agencyId")
-                                 @Parameter(description = "Agency Id.", required = true, example = "MDI")
-                                 @NotNull final String agencyId,
-                                 @PathVariable("roleType")
-                                 @Parameter(description = "Type of role", required = true, example = "KW")
-                                 @NotNull final StaffJobType roleType
+    public boolean hasStaffRole(
+        @PathVariable("staffId") @Parameter(description = "The staff id of the staff member.", required = true, example = "1111111") @NotNull final Long staffId,
+        @PathVariable("agencyId") @Parameter(description = "Agency Id.", required = true, example = "MDI") @NotNull final String agencyId,
+        @PathVariable("roleType") @Parameter(description = "Type of role", required = true, example = "KW") @NotNull final StaffJobType roleType
     ) {
         return staffService.hasStaffRole(staffId, agencyId, roleType);
     }
-
-
 }
