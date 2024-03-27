005b41269b3cb547653a8e7fc6ab2fb88119cc82
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-03-27 09:09:42 +0000

    SDIT-1610: 🐛 Fix booking into prison service

--- src/test/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderRepositoryTest.kt
+++ src/test/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderRepositoryTest.kt
@@ -1,7 +1,10 @@
+@file:Suppress("ClassName")
+
 package uk.gov.justice.hmpps.prison.repository.jpa.repository
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.tuple
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
@@ -23,59 +26,93 @@ class OffenderRepositoryTest {
   @Autowired
   private lateinit var repository: OffenderRepository
 
-  @Test
-  fun findByOffenderNomsIdUnique() {
-    val offender = repository.findOffenderByNomsId("A1234AL").orElseThrow()
-    assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1012L, -1012L)
-    assertThat(offender.bookings).hasSize(2)
-    val latestBooking = offender.latestBooking.get()
-    assertThat(latestBooking.bookingId).isEqualTo(-12L)
+  @Nested
+  inner class findOffenderByNomsId {
+    @Test
+    fun findByOffenderNomsIdUnique() {
+      val offender = repository.findOffenderByNomsId("A1234AL").orElseThrow()
+      assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1012L, -1012L)
+      assertThat(offender.bookings).hasSize(2)
+      val latestBooking = offender.latestBooking.get()
+      assertThat(latestBooking.bookingId).isEqualTo(-12L)
+    }
   }
 
-  @Test
-  fun findByOffendersNomsId() {
-    val offenders = repository.findByNomsId("A1234AL")
-    assertThat(offenders).extracting(Offender::getId).containsExactly(tuple(-1012L), tuple(-1013L))
+  @Nested
+  inner class findByNomsId {
+    @Test
+    fun findByOffendersNomsId() {
+      val offenders = repository.findByNomsId("A1234AL")
+      assertThat(offenders).extracting(Offender::getId).containsExactly(tuple(-1012L), tuple(-1013L))
+    }
   }
 
-  @Test
-  fun findOffenderWithLatestBookingNoSentencesOrReleaseDetail() {
-    val offender = repository.findOffenderWithLatestBookingByNomsId("A1060AA").orElseThrow()
-    assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1060L, -1060L)
-    assertThat(offender.bookings).hasSize(1)
-    val latestBooking = offender.latestBooking.get()
-    assertThat(latestBooking.bookingId).isEqualTo(-58L)
-    assertThat(latestBooking.releaseDetail).isNull()
-    assertThat(latestBooking.sentences.size).isEqualTo(0)
+  @Nested
+  inner class findOffenderWithLatestBookingByNomsId {
+    @Test
+    fun findOffenderWithLatestBookingNoSentencesOrReleaseDetail() {
+      val offender = repository.findOffenderWithLatestBookingByNomsId("A1060AA").orElseThrow()
+      assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1060L, -1060L)
+      assertThat(offender.bookings).hasSize(1)
+      val latestBooking = offender.latestBooking.get()
+      assertThat(latestBooking.bookingId).isEqualTo(-58L)
+      assertThat(latestBooking.releaseDetail).isNull()
+      assertThat(latestBooking.sentences.size).isEqualTo(0)
+    }
+
+    @Test
+    fun findOffenderWithLatestBookingWithSentences() {
+      val offender = repository.findOffenderWithLatestBookingByNomsId("A1234AL").orElseThrow()
+      assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1012L, -1012L)
+      assertThat(offender.bookings).hasSize(1)
+      val latestBooking = offender.latestBooking.get()
+      assertThat(latestBooking.bookingId).isEqualTo(-12L)
+      assertThat(latestBooking.releaseDetail).isNull()
+      assertThat(latestBooking.sentences.size).isEqualTo(1)
+    }
+
+    @Test
+    fun findOffenderWithLatestBookingWithReleaseDetailAndSentences() {
+      val offender = repository.findOffenderWithLatestBookingByNomsId("A1234AA").orElseThrow()
+      assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1001L, -1001L)
+      assertThat(offender.bookings).hasSize(1)
+      val latestBooking = offender.latestBooking.get()
+      assertThat(latestBooking.bookingId).isEqualTo(-1L)
+      assertThat(latestBooking.releaseDetail.id).isEqualTo(-1L)
+      assertThat(latestBooking.sentences.size).isEqualTo(1)
+    }
+
+    @Test
+    fun findOffenderWithLatestBookingAliasedOffender() {
+      val offender = repository.findOffenderWithLatestBookingByNomsId("A1234AI").orElseThrow()
+      assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1009L, -1009L)
+      assertThat(offender.bookings).hasSize(1)
+      assertThat(offender.latestBooking.get().bookingId).isEqualTo(-9L)
+    }
   }
 
-  @Test
-  fun findOffenderWithLatestBookingWithSentences() {
-    val offender = repository.findOffenderWithLatestBookingByNomsId("A1234AL").orElseThrow()
-    assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1012L, -1012L)
-    assertThat(offender.bookings).hasSize(1)
-    val latestBooking = offender.latestBooking.get()
-    assertThat(latestBooking.bookingId).isEqualTo(-12L)
-    assertThat(latestBooking.releaseDetail).isNull()
-    assertThat(latestBooking.sentences.size).isEqualTo(1)
-  }
+  @Nested
+  inner class findRootOffenderByNomsIdForUpdate {
+    @Test
+    fun `test find root offender with bookings`() {
+      // this prisoner has two offender records - ensure we get the right one
+      val offender = repository.findRootOffenderByNomsIdForUpdate("A1234AL").orElseThrow()
+      assertThat(offender.id).isEqualTo(-1012L)
+      assertThat(offender.rootOffenderId).isEqualTo(-1012L)
+      assertThat(offender.rootOffender.id).isEqualTo(-1012L)
 
-  @Test
-  fun findOffenderWithLatestBookingWithReleaseDetailAndSentences() {
-    val offender = repository.findOffenderWithLatestBookingByNomsId("A1234AA").orElseThrow()
-    assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1001L, -1001L)
-    assertThat(offender.bookings).hasSize(1)
-    val latestBooking = offender.latestBooking.get()
-    assertThat(latestBooking.bookingId).isEqualTo(-1L)
-    assertThat(latestBooking.releaseDetail.id).isEqualTo(-1L)
-    assertThat(latestBooking.sentences.size).isEqualTo(1)
-  }
+      assertThat(offender.bookings).hasSizeGreaterThan(0)
+    }
 
-  @Test
-  fun findOffenderWithLatestBookingAliasedOffender() {
-    val offender = repository.findOffenderWithLatestBookingByNomsId("A1234AI").orElseThrow()
-    assertThat(offender).extracting({ it.id }, { it.rootOffender.id }).containsExactly(-1009L, -1009L)
-    assertThat(offender.bookings).hasSize(1)
-    assertThat(offender.latestBooking.get().bookingId).isEqualTo(-9L)
+    @Test
+    fun `test find root offender with no bookings`() {
+      // this prisoner has two offender records but no bookings
+      val offender = repository.findRootOffenderByNomsIdForUpdate("A1234DD").orElseThrow()
+      assertThat(offender.id).isEqualTo(-1056L)
+      assertThat(offender.rootOffenderId).isEqualTo(-1056L)
+      assertThat(offender.rootOffender.id).isEqualTo(-1056L)
+
+      assertThat(offender.bookings).hasSize(0)
+    }
   }
 }
