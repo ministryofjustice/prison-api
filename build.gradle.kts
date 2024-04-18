b72126ba2acb557529d6211e6b888634c00f94ef
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2024-04-18 13:57:36 +0100

    SDIT-1669 Ensure jcache used for caching (#2112)
    * SDIT-1669 Ensure jcache used for caching
    
    * SDIT-1669 cache-api not needed for jcache config

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-aop")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
+  implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
