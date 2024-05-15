8b486ee015198d6fb0fef66c15a18b725d4c4544
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-05-15 08:20:48 +0100

    SDIT-1745 Join on staff members to fix n+1 query

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/StaffUserAccount.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/StaffUserAccount.java
@@ -1,15 +1,5 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Builder.Default;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.ToString.Exclude;
-import org.hibernate.Hibernate;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,11 +11,20 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.ToString.Exclude;
+import org.hibernate.Hibernate;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "STAFF_USER_ACCOUNTS")
@@ -41,7 +40,7 @@ public class StaffUserAccount extends AuditableEntity {
     @Column(nullable = false)
     private String username;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(optional = false)
     @JoinColumn(name = "STAFF_ID", nullable = false)
     @Exclude
     private Staff staff;
