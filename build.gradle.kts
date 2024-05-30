d29c7c73d1b5397a86745e12535ff81c8afa7ca9
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-05-30 08:08:41 +0100

    SDIT-1788: ⬆️ Upgrade to latest spring boot etc. (#2185)
    * SDIT-1788: ⬆️ Upgrade to latest spring boot etc.
    
    * SDIT-1788: ⬆️ Upgrade to latest kotlin lib
    
    * SDIT-1788: 🔧 Add flyway hsqldb dependency
    
    * SDIT-1788: ⬆️ Upgrade to latest jsql lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
-  kotlin("plugin.spring") version "1.9.23"
-  kotlin("plugin.jpa") version "1.9.23"
-  kotlin("plugin.lombok") version "1.9.23"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.0"
+  kotlin("plugin.spring") version "2.0.0"
+  kotlin("plugin.jpa") version "2.0.0"
+  kotlin("plugin.lombok") version "2.0.0"
 }
 
 configurations {
@@ -14,15 +14,12 @@ configurations {
   }
 }
 
-// Temporarily kept at 4.6 as 4.7 not compatible with spring data jpa
-val jsqlParserVersion by extra("4.6")
-
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   annotationProcessor("org.projectlombok:lombok:1.18.32")
   testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.0")
   implementation("org.springframework.boot:spring-boot-starter-aop")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
@@ -31,7 +28,7 @@ dependencies {
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
   implementation("commons-codec:commons-codec:1.17.0")
-  implementation("com.github.jsqlparser:jsqlparser:$jsqlParserVersion")
+  implementation("com.github.jsqlparser:jsqlparser:4.9")
   implementation("org.ehcache:ehcache:3.10.8")
   implementation("com.zaxxer:HikariCP:5.1.0")
 
@@ -42,13 +39,14 @@ dependencies {
   implementation("commons-io:commons-io:2.16.1")
   implementation("com.google.guava:guava:33.2.0-jre")
   implementation("org.apache.commons:commons-text:1.12.0")
-  implementation("com.oracle.database.jdbc:ojdbc10:19.22.0.0")
+  implementation("com.oracle.database.jdbc:ojdbc10:19.23.0.0")
   implementation("org.hibernate.orm:hibernate-community-dialects")
 
   compileOnly("org.projectlombok:lombok:1.18.32")
 
+  // we run on oracle in all environments, but allow instance to be started using hsqldb too
   runtimeOnly("org.hsqldb:hsqldb:2.7.2")
-  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("org.flywaydb:flyway-database-hsqldb")
 
   testImplementation("org.springframework.boot:spring-boot-test-autoconfigure")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -56,7 +54,7 @@ dependencies {
   testImplementation("io.rest-assured:json-schema-validator:5.4.0")
   testImplementation("io.rest-assured:spring-mock-mvc:5.4.0")
   testImplementation("org.springframework.security:spring-security-test")
-  testImplementation("com.google.code.gson:gson:2.10.1")
+  testImplementation("com.google.code.gson:gson:2.11.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.powermock:powermock-api-mockito2:2.0.9")
   testImplementation("org.powermock:powermock-module-junit4:2.0.9")
@@ -64,20 +62,20 @@ dependencies {
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
 
-  testImplementation("net.serenity-bdd:serenity-core:4.1.4")
-  testImplementation("net.serenity-bdd:serenity-junit:4.1.4")
-  testImplementation("net.serenity-bdd:serenity-spring:4.1.4")
-  testImplementation("net.serenity-bdd:serenity-cucumber:4.1.4")
+  testImplementation("net.serenity-bdd:serenity-core:4.1.14")
+  testImplementation("net.serenity-bdd:serenity-junit:4.1.14")
+  testImplementation("net.serenity-bdd:serenity-spring:4.1.14")
+  testImplementation("net.serenity-bdd:serenity-cucumber:4.1.14")
   testImplementation("com.paulhammant:ngwebdriver:1.2")
-  testImplementation("org.wiremock:wiremock:3.5.4")
+  testImplementation("org.wiremock:wiremock:3.6.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22") {
     exclude(group = "io.swagger.core.v3")
   }
-  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.21")
+  testImplementation("io.swagger.core.v3:swagger-core-jakarta:2.2.22")
   testImplementation("commons-beanutils:commons-beanutils:1.9.4")
-  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.35.0")
+  testImplementation("io.opentelemetry:opentelemetry-sdk-testing:1.37.0")
 
   testCompileOnly("org.projectlombok:lombok:1.18.32")
 }
@@ -88,10 +86,8 @@ java {
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      freeCompilerArgs = listOf("-Xjvm-default=all")
-      jvmTarget = "21"
-    }
+    compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+    compilerOptions.freeCompilerArgs = listOf("-Xjvm-default=all")
   }
 
   // Exclude Serenity BDD integration and IntTest tests from "test" task so they can be controlled independently
