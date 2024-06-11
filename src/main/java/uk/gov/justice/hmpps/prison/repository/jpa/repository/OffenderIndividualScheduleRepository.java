18fa1b5ab3d227708322338bc54fb04508c82c7e
steverendell <steve.rendell@syscon.net>
2024-06-11 15:20:04 +0100

    SDIT-1839 POST /api/appointments/delete
    And also DELETE /api/appointments/{id} and update of comments

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderIndividualScheduleRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderIndividualScheduleRepository.java
@@ -1,17 +1,20 @@
 package uk.gov.justice.hmpps.prison.repository.jpa.repository;
 
+import jakarta.persistence.LockModeType;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderIndividualSchedule;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface OffenderIndividualScheduleRepository extends JpaRepository<OffenderIndividualSchedule, Long> {
-
-    Optional<OffenderIndividualSchedule> findOneByParentEventId(Long parentEventId);
+    @NotNull
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<OffenderIndividualSchedule> findWithLockById(@NotNull Long eventId);
 
     List<OffenderIndividualSchedule> findByOffenderBooking_BookingIdOrderByIdAsc(final Long bookingId);
 
     Optional<OffenderIndividualSchedule> findOneByOffenderBookingBookingIdAndParentEventId(Long bookingId, Long parentEventId);
-
 }
