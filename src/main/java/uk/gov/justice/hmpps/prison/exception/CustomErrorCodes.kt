9947c7867cb61d2a58678239aa2549fb19947695
andymarke <andy.marke@digital.justice.gov.uk>
2024-07-23 11:52:39 +0100

    SDIT-1900 Prevent new bookings being created after an escape
    Also mapped the escape flag for movements and experiment for LocalDateTime DSL for better readability

--- src/main/java/uk/gov/justice/hmpps/prison/exception/CustomErrorCodes.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/exception/CustomErrorCodes.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.hmpps.prison.exception
 object CustomErrorCodes {
   const val NO_CELL_CAPACITY = 30001
   const val PRISONER_ALREADY_EXIST = 30002
-  const val PRISONER_ESCAPED_NEW_BOOKING_NOT_ALLOWED = 30002
+  const val PRISONER_ESCAPED_NEW_BOOKING_NOT_ALLOWED = 30003
 }
