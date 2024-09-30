fbe705ad5a52719688cd688bb2a9f5b0bf426e15
Jon Brighton <brightonsbox@hotmail.com>
2024-09-30 10:20:03 +0100

    CDPS-976: Adding database row lock timeout and processing (#2278)

--- src/main/java/uk/gov/justice/hmpps/prison/service/PrisonerProfileUpdateService.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/service/PrisonerProfileUpdateService.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.hmpps.prison.service
 
+import org.slf4j.LoggerFactory
+import org.springframework.dao.CannotAcquireLockException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.hmpps.prison.exception.DatabaseRowLockedException
 import uk.gov.justice.hmpps.prison.repository.jpa.repository.OffenderRepository
 
 @Service
@@ -11,13 +14,30 @@ class PrisonerProfileUpdateService(
 
   @Transactional
   fun updateBirthPlaceOfCurrentAlias(prisonerNumber: String, birthPlace: String?) {
-    offenderRepository.findLinkedToLatestBookingForUpdate(prisonerNumber)
-      .orElseThrow(
-        EntityNotFoundException.withMessage(
-          "Prisoner with prisonerNumber %s and existing booking not found",
-          prisonerNumber,
-        ),
-      )
-      .let { it.birthPlace = birthPlace?.ifBlank { null } }
+    try {
+      offenderRepository.findLinkedToLatestBookingForUpdate(prisonerNumber)
+        .orElseThrow(
+          EntityNotFoundException.withMessage(
+            "Prisoner with prisonerNumber %s and existing booking not found",
+            prisonerNumber,
+          ),
+        )
+        .let { it.birthPlace = birthPlace?.ifBlank { null } }
+    } catch (e: CannotAcquireLockException) {
+      throw processLockError(e, prisonerNumber)
+    }
+  }
+
+  private fun processLockError(e: CannotAcquireLockException, prisonerNumber: String): Exception {
+    log.error("Error getting lock", e)
+    return if (true == e.cause?.message?.contains("ORA-30006")) {
+      DatabaseRowLockedException("Failed to get OFFENDERS lock for prisonerNumber=$prisonerNumber")
+    } else {
+      e
+    }
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
   }
 }
