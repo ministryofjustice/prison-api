24e788396d61ad94504115ae6e8af30bff49be83
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-11-26 08:35:45 +0000

    SDIT-2318: 🐛 Implement new rest template method that is called instead these days

--- src/test/java/uk/gov/justice/hmpps/prison/test/ErrorResponseErrorHandler.java
+++ src/test/java/uk/gov/justice/hmpps/prison/test/ErrorResponseErrorHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.hmpps.prison.test;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -8,6 +9,7 @@ import org.springframework.web.client.HttpMessageConverterExtractor;
 import uk.gov.justice.hmpps.prison.api.model.ErrorResponse;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,13 +21,27 @@ public class ErrorResponseErrorHandler extends DefaultResponseErrorHandler {
     @Override
     public void handleError(final ClientHttpResponse response) throws IOException {
         final var errorMessageExtractor =
-                new HttpMessageConverterExtractor<ErrorResponse>(ErrorResponse.class, getMessageConverters());
+                new HttpMessageConverterExtractor<>(ErrorResponse.class, getMessageConverters());
 
         ErrorResponse errorResponse = null;
         try {
             errorResponse = errorMessageExtractor.extractData(response);
         } catch (final Exception e) {
             super.handleError(response);
+        }
+        throw new PrisonApiClientException(errorResponse);
+    }
+
+    @Override
+    public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
+        final var errorMessageExtractor =
+                new HttpMessageConverterExtractor<>(ErrorResponse.class, getMessageConverters());
+
+        ErrorResponse errorResponse = null;
+        try {
+            errorResponse = errorMessageExtractor.extractData(response);
+        } catch (final Exception e) {
+            super.handleError(url, method, response);
         }
         throw new PrisonApiClientException(errorResponse);
     }
