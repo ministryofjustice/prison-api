825e137a58dce98a8b2e2c1303473f27e182f9ee
Jon Brighton <brightonsbox@hotmail.com>
2024-12-06 13:07:03 +0000

    CDPS-975: Allowing birth place and country to be set to null

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/UpdateBirthCountry.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/UpdateBirthCountry.kt
@@ -4,6 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Update to prisoner birth country")
 data class UpdateBirthCountry(
-  @Schema(description = "Country code", example = "GBR")
-  val countryCode: String,
+  @Schema(description = "Country code", example = "GBR", required = true, nullable = true)
+  val countryCode: String?,
 )
