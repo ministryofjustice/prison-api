f1b6bcdbe5745be588b7c4de08a878a44375b6d1
Jon Brighton <brightonsbox@hotmail.com>
2025-02-06 14:14:53 +0000

    CDPS-1186: Correcting swagger documentation

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/UpdateSmokerStatus.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/UpdateSmokerStatus.kt
@@ -2,7 +2,7 @@ package uk.gov.justice.hmpps.prison.api.model
 
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "Update to prisoner religion")
+@Schema(description = "Update to prisoner's smoker status")
 data class UpdateSmokerStatus(
   @Schema(
     description = "The smoker status code ('Y' for 'Yes', 'N' for 'No', 'V' for 'Vaper/NRT Only')",
