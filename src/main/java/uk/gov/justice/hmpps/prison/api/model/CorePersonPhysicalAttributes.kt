de157036cac5d71735049ee4a5184a42ea541d3b
David Middleton <david.middleton@digital.justice.gov.uk>
2025-03-03 10:32:13 +0000

    CDPS-1253: Support common ref data object

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/CorePersonPhysicalAttributes.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/CorePersonPhysicalAttributes.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.hmpps.prison.api.model
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.hmpps.prison.repository.jpa.model.ProfileCode
 
 @JsonInclude(NON_NULL)
 @Schema(description = "Core Person Record Physical Attributes")
@@ -13,41 +14,23 @@ data class CorePersonPhysicalAttributes(
   @Schema(description = "Weight (in kilograms)")
   val weight: Int? = null,
 
-  @Schema(description = "Code for hair type or colour. Note: uses PROFILE_TYPE of `HAIR`", example = "BROWN")
-  val hairCode: String? = null,
+  @Schema(description = "Hair type or colour. Note: uses PROFILE_TYPE of `HAIR`", example = "BROWN")
+  val hair: ProfileCode? = null,
 
-  @Schema(description = "Description for hair type or colour")
-  val hairDescription: String? = null,
+  @Schema(description = "Facial hair type. Note: uses PROFILE_TYPE of `FACIAL_HAIR`", example = "BEARDED")
+  val facialHair: ProfileCode? = null,
 
-  @Schema(description = "Code for facial hair type. Note: uses PROFILE_TYPE of `FACIAL_HAIR`", example = "BEARDED")
-  val facialHairCode: String? = null,
+  @Schema(description = "Face shape. Note: uses PROFILE_TYPE of `FACE`", example = "ROUND")
+  val face: ProfileCode? = null,
 
-  @Schema(description = "Description for facial hair type")
-  val facialHairDescription: String? = null,
+  @Schema(description = "Build. Note: uses PROFILE_TYPE of `BUILD`", example = "MEDIUM")
+  val build: ProfileCode? = null,
 
-  @Schema(description = "Code for face shape. Note: uses PROFILE_TYPE of `FACE`", example = "ROUND")
-  val faceCode: String? = null,
+  @Schema(description = "Left eye colour. Note: uses PROFILE_TYPE of `L_EYE_C`", example = "BLUE")
+  val leftEyeColour: ProfileCode? = null,
 
-  @Schema(description = "Description for face shape")
-  val faceDescription: String? = null,
-
-  @Schema(description = "Code for build. Note: uses PROFILE_TYPE of `BUILD`", example = "MEDIUM")
-  val buildCode: String? = null,
-
-  @Schema(description = "Description for build")
-  val buildDescription: String? = null,
-
-  @Schema(description = "Code for left eye colour. Note: uses PROFILE_TYPE of `L_EYE_C`", example = "BLUE")
-  val leftEyeColourCode: String? = null,
-
-  @Schema(description = "Description for left eye colour")
-  val leftEyeColourDescription: String? = null,
-
-  @Schema(description = "Code for right eye colour. Note: uses PROFILE_TYPE of `R_EYE_C`", example = "BLUE")
-  val rightEyeColourCode: String? = null,
-
-  @Schema(description = "Description for right eye colour")
-  val rightEyeColourDescription: String? = null,
+  @Schema(description = "Right eye colour. Note: uses PROFILE_TYPE of `R_EYE_C`", example = "BLUE")
+  val rightEyeColour: ProfileCode? = null,
 
   @Schema(description = "Shoe size", example = "9")
   val shoeSize: String? = null,
