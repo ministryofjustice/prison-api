4edbadfc0ee866bf6376bab251b38029186c5da5
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2025-03-14 14:28:56 +0000

    Add roles to NomisApiV1Resource (#2382)

--- src/main/java/uk/gov/justice/hmpps/prison/api/resource/NomisApiV1Resource.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/resource/NomisApiV1Resource.java
@@ -377,6 +377,7 @@ public class NomisApiV1Resource {
     @GetMapping("/prison/{prison_id}/offenders/{noms_id}/accounts")
     @Tag(name = "integration-api")
     @Tag(name = "unilink")
+    @PreAuthorize("hasAnyRole('NOMIS_API_V1', 'UNILINK', 'PRISON_API__HMPPS_INTEGRATION_API')")
     // @SlowReportQuery Temporarily go to primary to investigate cause of unilink issue
     public AccountBalance getAccountBalance(@Size(max = 3) @NotNull @PathVariable("prison_id") @Parameter(name = "prison_id", description = "Prison ID", example = "WLI", required = true) final String prisonId, @Pattern(regexp = NOMS_ID_REGEX_PATTERN) @NotNull @PathVariable("noms_id") @Parameter(name = "noms_id", description = "Offender Noms Id", example = "A1404AE", required = true) final String nomsId) {
         return service.getAccountBalances(prisonId, nomsId);
@@ -415,6 +416,7 @@ public class NomisApiV1Resource {
     @GetMapping("/prison/{prison_id}/offenders/{noms_id}/accounts/{account_code}/transactions")
     @Tag(name = "integration-api")
     @Tag(name = "unilink")
+    @PreAuthorize("hasAnyRole('NOMIS_API_V1', 'UNILINK', 'PRISON_API__HMPPS_INTEGRATION_API')")
     // @SlowReportQuery Temporarily go to primary to investigate cause of unilink issue
     public AccountTransactions getAccountTransactions(@Size(max = 3) @NotNull @PathVariable("prison_id") @Parameter(name = "prison_id", description = "Prison ID", example = "WLI", required = true) final String prisonId, @Pattern(regexp = NOMS_ID_REGEX_PATTERN) @NotNull @PathVariable("noms_id") @Parameter(name = "noms_id", description = "Offender Noms Id", example = "A1404AE", required = true) final String nomsId, @NotNull @PathVariable("account_code") @Parameter(name = "account_code", description = "Account code", example = "spends", required = true, schema = @Schema(implementation = String.class, allowableValues = {"spends","cash","savings"})) final String accountCode, @RequestParam(value = "from_date", required = false) @org.springframework.format.annotation.DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(name = "from_date", description = "Start date for transactions (defaults to today if not supplied)", example = "2019-04-01") final LocalDate fromDate, @RequestParam(value = "to_date", required = false) @org.springframework.format.annotation.DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @Parameter(name = "to_date", description = "To date for transactions (defaults to today if not supplied)", example = "2019-05-01") final LocalDate toDate) {
         final var transactions = service.getAccountTransactions(prisonId, nomsId, accountCode, fromDate, toDate);
@@ -434,6 +436,7 @@ public class NomisApiV1Resource {
     @GetMapping("/prison/{prison_id}/offenders/{noms_id}/transactions/{client_unique_ref}")
     @Tag(name = "integration-api")
     @Tag(name = "unilink")
+    @PreAuthorize("hasAnyRole('NOMIS_API_V1', 'UNILINK', 'PRISON_API__HMPPS_INTEGRATION_API')")
     // @SlowReportQuery Temporarily go to primary to investigate cause of unilink issue
     public AccountTransaction getTransactionByClientUniqueRef(@RequestHeader(value = "X-Client-Name", required = false) @Parameter(name = "X-Client-Name", description = "If present then the value is prepended to the client_unique_ref separated by a dash. When this API is invoked via the Nomis gateway this will already have been created by the gateway.") final String clientName, @Size(max = 3) @NotNull @PathVariable("prison_id") @Parameter(name = "prison_id", description = "Prison ID", example = "WLI", required = true) final String prisonId, @Pattern(regexp = NOMS_ID_REGEX_PATTERN) @NotNull @PathVariable("noms_id") @Parameter(name = "noms_id", description = "Offender Noms Id", example = "A1404AE", required = true) final String nomsId, @Pattern(regexp = CLIENT_UNIQUE_REF_PATTERN) @Size(max = 64) @PathVariable("client_unique_ref") @Parameter(name = "client_unique_ref", description = "Client unique reference", required = true) final String clientUniqueRef) {
         final var uniqueClientId = getUniqueClientId(clientName, clientUniqueRef);
