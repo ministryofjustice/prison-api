173534fba17dee3f3afcf1eb3be2977cecd01702
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-04-15 14:28:24 +0100

    MIA-277 add endpoint to retrieve multiple staff details by ids (#2401)
    Co-authored-by: David Winchurch <david.winchurch@iterativeit.com>

--- src/main/java/uk/gov/justice/hmpps/prison/api/resource/StaffResource.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/resource/StaffResource.java
@@ -2,16 +2,20 @@ package uk.gov.justice.hmpps.prison.api.resource;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +45,17 @@ public class StaffResource {
 
     public StaffResource(final StaffService staffService) {
         this.staffService = staffService;
+    }
+
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema=@Schema(implementation = StaffDetail.class)))}),
+        @ApiResponse(responseCode = "400", description = "Invalid request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "404", description = "Requested resource not found.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+        @ApiResponse(responseCode = "500", description = "Unrecoverable error occurred whilst processing request.", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))})})
+    @VerifyStaffAccess(overrideRoles = {"STAFF_SEARCH"})
+    @PostMapping
+    public List<StaffDetail> getStaffDetails(@RequestBody @NotEmpty List<Long> staffIds) {
+        return staffService.getStaffDetails(staffIds);
     }
 
     @ApiResponses({
