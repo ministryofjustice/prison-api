64ff831ce6ea1aa937969400fca668c10a53a63a
Melia Burnley <dan@danops.uk>
2025-05-27 16:39:20 +0100

    CDPS-1405: Add new global numbers endpoint

---
+++ src/main/java/uk/gov/justice/hmpps/prison/service/OffenderPhonesService.kt
@@ -0,0 +1,72 @@
+package uk.gov.justice.hmpps.prison.service
+
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+import uk.gov.justice.hmpps.prison.api.model.OffenderPhoneNumberCreateRequest
+import uk.gov.justice.hmpps.prison.api.model.Telephone
+import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderPhone
+import uk.gov.justice.hmpps.prison.repository.jpa.repository.OffenderPhoneRepository
+import uk.gov.justice.hmpps.prison.repository.jpa.repository.OffenderRepository
+import java.util.Optional
+
+@Service
+@Transactional
+class OffenderPhonesService(
+  private val offenderRepository: OffenderRepository,
+  private val offenderPhoneRepository: OffenderPhoneRepository,
+  private val referenceDomainService: ReferenceDomainService,
+) {
+  fun getPhoneNumbersByOffenderNo(prisonerNumber: String): List<Telephone> {
+    val offender = offenderRepository.findRootOffenderByNomsId(prisonerNumber)
+      .orElseThrowNotFound("Prisoner with prisonerNumber %s not found", prisonerNumber)
+    return offender.phones.map(AddressTransformer::translate)
+  }
+
+  fun addOffenderPhoneNumber(
+    prisonerNumber: String,
+    offenderPhoneNumberRequest: OffenderPhoneNumberCreateRequest,
+  ): OffenderPhone {
+    val offender = offenderRepository.findRootOffenderByNomsId(prisonerNumber)
+      .orElseThrowNotFound("Prisoner with prisonerNumber %s not found", prisonerNumber)
+
+    if (!referenceDomainService.isReferenceCodeActive("PHONE_USAGE", offenderPhoneNumberRequest.phoneNumberType)) {
+      throw BadRequestException.withMessage("Phone number type ${offenderPhoneNumberRequest.phoneNumberType} is not valid")
+    }
+
+    val phone = OffenderPhone
+      .builder()
+      .offender(offender)
+      .phoneNo(offenderPhoneNumberRequest.phoneNumber)
+      .phoneType(offenderPhoneNumberRequest.phoneNumberType).build()
+
+    offenderPhoneRepository.save(phone)
+    return phone
+  }
+
+  fun updateOffenderPhoneNumber(
+    prisonerNumber: String,
+    phoneNumberId: Long,
+    offenderPhoneNumberRequest: OffenderPhoneNumberCreateRequest,
+  ): OffenderPhone {
+    val phoneToUpdate = offenderPhoneRepository.findByRootNomsIdAndPhoneId(prisonerNumber, phoneNumberId)
+      .orElseThrow(
+        EntityNotFoundException.withMessage(
+          "Phone number with prisoner number %s and phone ID %s not found",
+          prisonerNumber,
+          phoneNumberId,
+        ),
+      )
+
+    if (!referenceDomainService.isReferenceCodeActive("PHONE_USAGE", offenderPhoneNumberRequest.phoneNumberType)) {
+      throw BadRequestException.withMessage("Phone number type ${offenderPhoneNumberRequest.phoneNumberType} is not valid")
+    }
+
+    phoneToUpdate.phoneType = offenderPhoneNumberRequest.phoneNumberType
+    phoneToUpdate.phoneNo = offenderPhoneNumberRequest.phoneNumber
+    offenderPhoneRepository.save(phoneToUpdate)
+
+    return phoneToUpdate
+  }
+
+  private inline fun <reified T> Optional<T>.orElseThrowNotFound(message: String, prisonerNumber: Any) = orElseThrow(EntityNotFoundException.withMessage(message, prisonerNumber))
+}
