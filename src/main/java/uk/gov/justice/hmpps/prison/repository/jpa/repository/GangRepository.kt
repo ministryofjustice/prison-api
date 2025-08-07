79792bb750069c1687a08b2b6803da5c99ce361e
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-08-07 10:18:47 +0100

    SDIT-2760: 🔥 Remove unused GET /api/gang/non-associations/{offenderNo} endpoint

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/GangRepository.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.hmpps.prison.repository.jpa.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import uk.gov.justice.hmpps.prison.repository.jpa.model.Gang
-
-@Repository
-interface GangRepository : JpaRepository<Gang, String>
