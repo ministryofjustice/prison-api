5e75c4762567054f52f39700e6f1b5ef4eba3361
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2025-08-07 16:55:13 +0100

    SDIT-2985 Prisoner-search image id not updated if uploaded/edited on DPS (#2487)
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/main/java/uk/gov/justice/hmpps/prison/service/ImageService.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/service/ImageService.kt
@@ -27,17 +27,21 @@ class ImageService(
   private val offenderBookingRepository: OffenderBookingRepository,
 ) {
 
-  fun findOffenderImagesFor(offenderNumber: String): List<ImageDetail> = offenderImageRepository.getImagesByOffenderNumber(offenderNumber)
+  fun findOffenderImagesFor(offenderNumber: String): List<ImageDetail> = offenderImageRepository
+    .getImagesByOffenderNumber(offenderNumber)
     .map(OffenderImage::transform)
 
-  fun findImageDetail(imageId: Long): ImageDetail = offenderImageRepository.findById(imageId)
+  fun findImageDetail(imageId: Long): ImageDetail = offenderImageRepository
+    .findById(imageId)
     .map(OffenderImage::transform)
     .orElseThrow(EntityNotFoundException.withId(imageId))
 
-  fun getImageContent(imageId: Long, fullSizeImage: Boolean): Optional<ByteArray> = offenderImageRepository.findById(imageId)
+  fun getImageContent(imageId: Long, fullSizeImage: Boolean): Optional<ByteArray> = offenderImageRepository
+    .findById(imageId)
     .map { if (fullSizeImage) it.fullSizeImage else it.thumbnailImage }
 
-  fun getImageContent(offenderNo: String, fullSizeImage: Boolean): Optional<ByteArray> = offenderImageRepository.findLatestByOffenderNumber(offenderNo)
+  fun getImageContent(offenderNo: String, fullSizeImage: Boolean): Optional<ByteArray> = offenderImageRepository
+    .findLatestByOffenderNumber(offenderNo)
     .map { if (fullSizeImage) it.fullSizeImage else it.thumbnailImage }
 
   @Transactional
@@ -50,25 +54,12 @@ class ImageService(
     val booking = offenderBookingRepository.findLatestOffenderBookingByNomsIdOrNull(offenderNumber)
       ?: throw EntityNotFoundException.withMessage("There are no bookings for $offenderNumber")
 
-    // Set the previously active facial image for this bookingId to inactive
-    val previousImage = offenderImageRepository.findLatestByBookingId(booking.bookingId)
-    if (previousImage.isPresent) {
-      val prev = previousImage.get()
-      log.info(
-        "Setting previous facial image to active=false - Id {}, bookingId {}, bookingSeq {}, offenderNo {}",
-        prev.id,
-        booking.bookingId,
-        booking.bookingSequence,
-        offenderNumber,
-      )
-      prev.isActive = false
-      offenderImageRepository.save(prev)
-    }
-    return try {
-      val imageToScale = receivedImage.readAllBytes()
-      val fullImage = scaleImage(fullWidth, fullHeight, imageToScale)
-      val thumbImage = scaleImage(thumbWidth, thumbHeight, imageToScale)
-      val newImage = OffenderImage
+    val imageToScale = receivedImage.readAllBytes()
+    val fullImage = scaleImage(fullWidth, fullHeight, imageToScale)
+    val thumbImage = scaleImage(thumbWidth, thumbHeight, imageToScale)
+
+    val image = try {
+      val imageBuilder = OffenderImage
         .builder()
         .captureDateTime(LocalDateTime.now())
         .orientationType("FRONT")
@@ -77,24 +68,59 @@ class ImageService(
         .active(true)
         .sourceCode("GEN")
         .offenderBooking(booking)
-        .thumbnailImage(thumbImage)
-        .fullSizeImage(fullImage)
-        .build()
 
-      // Suggested method - does add the image but does not flush or return the imageId for the response
-      // var savedImage = booking.addImage(newImage);
-      val savedImage = offenderImageRepository.save(newImage)
-      log.info(
-        "Saved image - Id {}, bookingId {}, bookingSeq {}, offenderNo {}",
-        savedImage.id,
-        booking.bookingId,
-        booking.bookingSequence,
-        offenderNumber,
-      )
-      savedImage.transform()
+      // Set the previously active facial image for this bookingId to inactive
+      val previousImage = offenderImageRepository.findLatestByBookingId(booking.bookingId)
+      if (previousImage.isPresent) {
+        val prev = previousImage.get()
+        log.info(
+          "Setting previous facial image to active=false - Id {}, bookingId {}, bookingSeq {}, offenderNo {}",
+          prev.id,
+          booking.bookingId,
+          booking.bookingSequence,
+          offenderNumber,
+        )
+        prev.isActive = false
+        offenderImageRepository.save(prev)
+
+        val newImage = imageBuilder
+          .thumbnailImage(thumbImage)
+          .fullSizeImage(fullImage)
+          .build()
+
+        // Suggested method - does add the image but does not flush or return the imageId for the response
+        // var savedImage = booking.addImage(newImage);
+        val savedImage = offenderImageRepository.save(newImage)
+        log.info(
+          "Saved image - Id {}, bookingId {}, bookingSeq {}, offenderNo {}",
+          savedImage.id,
+          booking.bookingId,
+          booking.bookingSequence,
+          offenderNumber,
+        )
+        savedImage.transform()
+      } else {
+        // Save full and thumbnail image data separately to emulate P-Nomis: this is to ensure the UPDATE TRIGGER fires.
+        // This is a temporary fix while there is still no INSERT trigger on OFFENDER_IMAGES
+        val newImage = imageBuilder.build()
+
+        val savedImage = offenderImageRepository.save(newImage)
+        savedImage.fullSizeImage = fullImage
+        savedImage.thumbnailImage = thumbImage
+        log.info(
+          "Saved image (no previous) - Id {}, bookingId {}, bookingSeq {}, offenderNo {}",
+          savedImage.id,
+          booking.bookingId,
+          booking.bookingSequence,
+          offenderNumber,
+        )
+        savedImage.transform()
+      }
     } catch (e: Exception) {
+      log.error("Image exception", e)
       throw BadRequestException.withMessage("Error scaling the image. Must be in JPEG format.")
     }
+    return image
   }
 
   @Throws(IOException::class, IllegalArgumentException::class, InterruptedException::class)
