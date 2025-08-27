af32894d40a0774fd70ac40e8bf95effbdbd5117
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-08-27 12:18:34 +0100

    SDIT-2996: 🔧 Switch to nvd v2 feed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.7"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
   kotlin("plugin.spring") version "2.2.10"
   kotlin("plugin.jpa") version "2.2.10"
   kotlin("plugin.lombok") version "2.2.10"
