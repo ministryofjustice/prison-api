bf37b1ce7bc107f7e2d9e5cc21f5d820a9d7c79f
Justin Gardiner <justin.gardiner@digital.justice.gov.uk>
2025-09-04 09:53:27 +0100

    CVSL-3258: Add isFixedTermRecall to recall type.

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/RecallType.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/RecallType.kt
@@ -1,9 +1,15 @@
 package uk.gov.justice.hmpps.prison.api.model
 
-enum class RecallType {
-  NONE,
-  STANDARD_RECALL,
-  STANDARD_RECALL_255,
-  FIXED_TERM_RECALL_14,
-  FIXED_TERM_RECALL_28,
+import com.fasterxml.jackson.annotation.JsonFormat
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+enum class RecallType(
+  val recallName: String,
+  val isFixedTermRecall: Boolean = false,
+) {
+  NONE("NONE"),
+  STANDARD_RECALL("STANDARD_RECALL"),
+  STANDARD_RECALL_255("STANDARD_RECALL_255"),
+  FIXED_TERM_RECALL_14("FIXED_TERM_RECALL_14", isFixedTermRecall = true),
+  FIXED_TERM_RECALL_28("FIXED_TERM_RECALL_28", isFixedTermRecall = true),
 }
