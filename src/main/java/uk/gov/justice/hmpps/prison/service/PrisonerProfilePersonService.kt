fab6a92cbd6202c208694af1f8e12815cd86c6f2
Ewan Donovan <ewan.donovan@digital.justice.gov.uk>
2025-09-12 10:15:24 +0100

    Catch the outlier exception

--- src/main/java/uk/gov/justice/hmpps/prison/service/PrisonerProfilePersonService.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/service/PrisonerProfilePersonService.kt
@@ -12,13 +12,17 @@ class PrisonerProfilePersonService(
   private val offenderMilitaryRecordService: OffenderMilitaryRecordService,
   private val distinguishingMarkService: DistinguishingMarkService,
 ) {
-  fun getPrisonerProfileSummary(prisonerNumber: String): PrisonerProfileSummaryDto = PrisonerProfileSummaryDto(
-    aliases = prisonerProfileUpdateService.getAliases(prisonerNumber),
-    addresses = offenderAddressService.getAddressesByOffenderNo(prisonerNumber),
-    phones = offenderPhonesService.getPhoneNumbersByOffenderNo(prisonerNumber),
-    emails = offenderEmailsService.getEmailsByPrisonerNumber(prisonerNumber),
-    militaryRecord = offenderMilitaryRecordService.getMilitaryRecords(prisonerNumber),
-    physicalAttributes = prisonerProfileUpdateService.getPhysicalAttributes(prisonerNumber),
-    distinguishingMarks = distinguishingMarkService.findMarksForLatestBooking(prisonerNumber),
-  )
+  fun getPrisonerProfileSummary(prisonerNumber: String): PrisonerProfileSummaryDto = try {
+    PrisonerProfileSummaryDto(
+      aliases = prisonerProfileUpdateService.getAliases(prisonerNumber),
+      addresses = offenderAddressService.getAddressesByOffenderNo(prisonerNumber),
+      phones = offenderPhonesService.getPhoneNumbersByOffenderNo(prisonerNumber),
+      emails = offenderEmailsService.getEmailsByPrisonerNumber(prisonerNumber),
+      militaryRecord = offenderMilitaryRecordService.getMilitaryRecords(prisonerNumber),
+      physicalAttributes = prisonerProfileUpdateService.getPhysicalAttributes(prisonerNumber),
+      distinguishingMarks = distinguishingMarkService.findMarksForLatestBooking(prisonerNumber),
+    )
+  } catch (ex: NoSuchElementException) {
+    throw EntityNotFoundException("Prisoner with prisonerNumber $prisonerNumber not found")
+  }
 }
