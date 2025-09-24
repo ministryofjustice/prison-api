368d3a492b12f19b4045f13d1a50076196b7af69
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-09-24 12:25:45 +0100

    SDIT-3057: 🔒️ Upgrade to hmpps plugin 9.1.1 to fix owasp builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.1"
   kotlin("plugin.spring") version "2.2.20"
   kotlin("plugin.jpa") version "2.2.20"
   kotlin("plugin.lombok") version "2.2.20"
