0b3a23d18e501bde3735b1080219e3229e55d7a0
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-10-24 16:00:11 +0100

    SDIT-3127 Try out Spring Boot 3.5.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
   kotlin("plugin.spring") version "2.2.20"
   kotlin("plugin.jpa") version "2.2.20"
   kotlin("plugin.lombok") version "2.2.20"
