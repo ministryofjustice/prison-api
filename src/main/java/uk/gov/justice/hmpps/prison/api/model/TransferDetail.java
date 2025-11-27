6c6f9a07198a493f04f99e4d59a6773108a9bd28
Luke Harper <luke.harper@digital.justice.gov.uk>
2025-11-27 10:28:40 +0000

    RCLL-605 Fix error in documents.

--- src/main/java/uk/gov/justice/hmpps/prison/api/model/TransferDetail.java
+++ src/main/java/uk/gov/justice/hmpps/prison/api/model/TransferDetail.java
@@ -12,7 +12,6 @@ import lombok.ToString;
 import java.time.LocalDateTime;
 
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 @Schema(description = "A movement that is a transfer")
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -31,6 +30,6 @@ public class TransferDetail {
     private String transferReason;
     @Schema(description = "The prison they were transferred from", example = "WWI")
     private String fromPrisonId;
-    @Schema(description = "The prison they were transferred to. Can be absent if they have not arrived at the prison yet", requiredMode = REQUIRED, example = "BXI")
+    @Schema(description = "The prison they were transferred to. Can be absent if they have not arrived at the prison yet", requiredMode = NOT_REQUIRED, example = "BXI")
     private String toPrisonId;
 }
