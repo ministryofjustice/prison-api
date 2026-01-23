94691424637437ee7c5e853bc0c080d634069cde
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-01-23 14:11:58 +0000

    SDIT-3361: ⬆️ Upgrade to spring boot 4.0.2 etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.1"
   kotlin("plugin.spring") version "2.3.0"
   kotlin("plugin.jpa") version "2.3.0"
   kotlin("plugin.lombok") version "2.3.0"
