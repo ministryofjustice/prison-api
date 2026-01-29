5661b963db2eef5730ffec73a5ef1c9705dc27ba
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-01-29 16:53:01 +0000

    SDIT-3372: ⬆️ Upgrade gradle spring boot to unpin logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.3"
   kotlin("plugin.spring") version "2.3.0"
   kotlin("plugin.jpa") version "2.3.0"
   kotlin("plugin.lombok") version "2.3.0"
