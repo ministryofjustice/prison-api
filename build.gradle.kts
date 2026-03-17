e93d77453002790a2d007f32f31e4ae86834bb68
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-03-17 19:16:52 +0000

    SDIT-3597: 🔒️ Pin swagger-ui for security vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.6"
   kotlin("plugin.spring") version "2.3.10"
   kotlin("plugin.jpa") version "2.3.10"
   kotlin("plugin.lombok") version "2.3.10"
