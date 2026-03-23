917aa9326481d9573d65287e70f2cf20174c6146
karenmillermoj <karen.miller3@justice.gov.uk>
2026-03-23 15:58:21 +0000

    SDIT-3537 Fix test

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderTrustAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.hmpps.prison.repository.jpa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderTrustAccount;
-import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderTrustAccount.Pk;
-
-public interface OffenderTrustAccountRepository extends CrudRepository<OffenderTrustAccount, Pk> {
-}
