a69538556eb3ae171e4266641ea24e0364c463a8
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-04-29 16:36:45 +0100

    SDIT-3749: 📈 Add in micrometer registry to track hikari connections (#2648)

--- src/main/java/uk/gov/justice/hmpps/prison/web/config/ApplicationInsightsConfiguration.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/web/config/ApplicationInsightsConfiguration.kt
@@ -1,6 +1,11 @@
 package uk.gov.justice.hmpps.prison.web.config
 
 import com.microsoft.applicationinsights.TelemetryClient
+import io.micrometer.azuremonitor.AzureMonitorConfig
+import io.micrometer.azuremonitor.AzureMonitorMeterRegistry
+import io.micrometer.core.instrument.Clock.SYSTEM
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -11,6 +16,16 @@ import org.springframework.context.annotation.Configuration
 class ApplicationInsightsConfiguration {
   @Bean
   fun telemetryClient(): TelemetryClient = TelemetryClient()
+
+  @Bean
+  @ConditionalOnProperty("applicationinsights.connection.string")
+  fun azureMonitorMeterRegistry(@Value($$"${applicationinsights.connection.string}") connectionString: String): AzureMonitorMeterRegistry = AzureMonitorMeterRegistry(
+    object : AzureMonitorConfig {
+      override fun get(key: String): String? = null
+      override fun connectionString(): String = connectionString
+    },
+    SYSTEM,
+  )
 }
 
 fun TelemetryClient.trackEvent(name: String, properties: Map<String, String>) = this.trackEvent(name, properties, null)
