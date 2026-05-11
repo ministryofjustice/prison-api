6a9c4e3f94e508e31e574d11fc1ee4550189f3eb
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-05-11 12:21:16 +0100

    SDIT-3789: ⬆️ Upgrade gradle plugin for dompurify suppression

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.5"
   kotlin("plugin.spring") version "2.3.21"
   kotlin("plugin.jpa") version "2.3.21"
   kotlin("plugin.lombok") version "2.3.21"
