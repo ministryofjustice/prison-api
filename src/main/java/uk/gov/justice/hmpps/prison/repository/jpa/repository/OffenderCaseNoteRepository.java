377c812eaf569a7b8c82d08536c5a5aa5ebdf72c
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2026-05-11 12:39:11 +0100

    SDIT-3684 Remove unused /api/case-notes/usage-by-types endpoint (#2656)

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderCaseNoteRepository.java
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/repository/OffenderCaseNoteRepository.java
@@ -7,14 +7,11 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.lang.Nullable;
 import uk.gov.justice.hmpps.prison.repository.jpa.model.OffenderCaseNote;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface OffenderCaseNoteRepository extends
@@ -23,11 +20,6 @@ public interface OffenderCaseNoteRepository extends
     JpaSpecificationExecutor<OffenderCaseNote> {
 
     Optional<OffenderCaseNote> findByIdAndOffenderBooking_BookingId(final Long id, final Long bookingId);
-
-    @Query(value = "select new uk.gov.justice.hmpps.prison.repository.jpa.repository.PrisonerCaseNoteTypeAndSubType(cn.bookingId, cn.typeCode, cn.subTypeCode, cn.occurrenceDateTime) from OffenderCaseNote cn where cn.bookingId in (:bookingIds) and cn.typeCode in (:types) and cn.occurrenceDate >= :cutoffDate")
-    List<PrisonerCaseNoteTypeAndSubType> findCaseNoteTypesByBookingAndDate(
-        List<Long> bookingIds, List<String> types, LocalDate cutoffDate
-    );
 
     @NotNull
     @EntityGraph(type = EntityGraphType.FETCH, value = "case-note-with-author")
