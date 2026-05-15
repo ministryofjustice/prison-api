226d5876d3ba6176e247457b51fc3e406e0d9402
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-05-15 10:32:46 +0100

    SDIT-3807: ⬆️ Upgrade gradle plugin for tomcat vulns

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.1"
   kotlin("plugin.spring") version "2.3.21"
   kotlin("plugin.jpa") version "2.3.21"
   kotlin("plugin.lombok") version "2.3.21"
