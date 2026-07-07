dd0b4a2d9657c0138dc7487f7bf57efd56ec8d24
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-07-07 15:13:27 +0100

    SDIT-4019: ⬆️ Upgrade to latest libs (#2708)

--- src/main/java/uk/gov/justice/hmpps/prison/web/config/ApplicationInsightsConfiguration.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/web/config/ApplicationInsightsConfiguration.kt
@@ -1,13 +1,6 @@
 package uk.gov.justice.hmpps.prison.web.config
 
 import com.microsoft.applicationinsights.TelemetryClient
-import io.micrometer.azuremonitor.AzureMonitorConfig
-import io.micrometer.azuremonitor.AzureMonitorMeterRegistry
-import io.micrometer.core.instrument.Clock.SYSTEM
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -15,19 +8,9 @@ import org.springframework.context.annotation.Configuration
  * TelemetryClient gets altered at runtime by the java agent and so is a no-op otherwise
  */
 @Configuration
-class ApplicationInsightsConfiguration(@Autowired private val applicationContext: ApplicationContext) {
+class ApplicationInsightsConfiguration {
   @Bean
   fun telemetryClient(): TelemetryClient = TelemetryClient()
-
-  @Bean
-  @ConditionalOnProperty("applicationinsights.connection.string")
-  fun azureMonitorMeterRegistry(@Value($$"${applicationinsights.connection.string}") connectionString: String): AzureMonitorMeterRegistry = AzureMonitorMeterRegistry(
-    object : AzureMonitorConfig {
-      override fun get(key: String): String? = null
-      override fun connectionString(): String = connectionString
-    },
-    SYSTEM,
-  )
 }
 
 fun TelemetryClient.trackEvent(name: String, properties: Map<String, String>) = this.trackEvent(name, properties, null)
