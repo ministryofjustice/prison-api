95e875a5cdb1a2fd65e89f44de51f9108ca6e011
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-09-01 14:25:18 +0100

    SDIT-4185: ⬆️ Upgrades for tomcat pinning (#2727)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.7"
   kotlin("plugin.spring") version "2.4.10"
   kotlin("plugin.jpa") version "2.4.10"
   kotlin("plugin.lombok") version "2.4.10"
