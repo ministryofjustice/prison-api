0f01760f329bf97d29ec4e112e1ce1b9e32e728b
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-09-16 09:03:37 +0100

    SDIT-4233: ⬆️  Upgrade for jackson, netty, kotlin 2.4.20 etc. (#2731)

--- src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/MembershipStatus.kt
+++ src/main/java/uk/gov/justice/hmpps/prison/repository/jpa/model/MembershipStatus.kt
@@ -2,11 +2,9 @@ package uk.gov.justice.hmpps.prison.repository.jpa.model
 
 import jakarta.persistence.DiscriminatorValue
 import jakarta.persistence.Entity
-import lombok.NoArgsConstructor
 
 @Entity
 @DiscriminatorValue(MembershipStatus.DOMAIN)
-@NoArgsConstructor
 class MembershipStatus(code: String?, description: String?) : ReferenceCode(DOMAIN, code, description) {
 
   companion object {
